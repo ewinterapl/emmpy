b164da890574c2096da7b87043bab8c74a4f77f5
Eric Winter <eric.winter@jhuapl.edu>
2021-02-11 16:24:52 -0500

    Added UnmodifiableIterator class.

---
+++ emmpy/com/google/common/collect/__init__.py
@@ -0,0 +1,11 @@
+"""collect
+
+Python port of Java classes from the com.google.common.collect hierarchy in the
+Google Guava package:
+
+https://guava.dev/releases/8.0/api/docs/com/google/common/base/package-summary.html
+
+Modules
+-------
+unmodifiableiterator
+"""
---
+++ emmpy/com/google/common/collect/tests/__init__.py
@@ -0,0 +1,11 @@
+"""tests
+
+Tests for Python port of Java classes from the com.google.common.collect
+hierarchy in the Google Guava package:
+
+https://guava.dev/releases/8.0/api/docs/com/google/common/collect/package-summary.html
+
+Modules
+-------
+unmodifiableiterator
+"""

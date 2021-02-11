9aed7ee28fea5fbf37a36e58bbb15bebc30d7d63
Eric Winter <eric.winter@jhuapl.edu>
2021-02-11 11:11:05 -0500

    Added template for class and class tester.

---
+++ emmpy/templates/classtemplate.py
@@ -0,0 +1,24 @@
+class ClassTemplate:
+
+    """ClassTemplate
+
+    A Python class template ...
+    
+    Based on the original Java class:
+    XXX.XXX.XXX
+    """
+
+    def __init__(self):
+        """Initialize a new ClassTemplate object.
+
+        Parameters
+        ----------
+        self : ClassTemplate
+            New object to initialize.
+        
+        Returns
+        -------
+        self : ClassTemplate
+            New object after initialization.
+        """
+        pass
---
+++ emmpy/templates/tests/test_ClassTemplate.py
@@ -0,0 +1,12 @@
+import unittest
+
+from emmpy.templates.classtemplate import ClassTemplate
+
+class TestBuilder(unittest.TestCase):
+
+    def test___init__(self):
+        ClassTemplate()
+    
+
+if __name__ == '__main__':
+    unittest.main()

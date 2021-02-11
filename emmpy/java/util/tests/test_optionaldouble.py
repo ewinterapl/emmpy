7762bca8af5ba186358f62f6a63131c6dbd99569
Eric Winter <eric.winter@jhuapl.edu>
2021-02-11 08:36:34 -0500

    Added docstrings and test code for OptionalDouble class.

--- emmpy/java/util/tests/test_optionaldouble.py
+++ emmpy/java/util/tests/test_optionaldouble.py
@@ -4,8 +4,11 @@ from emmpy.java.util.optionaldouble import OptionalDouble
 
 class TestOptionalDouble(unittest.TestCase):
 
+    def test___init__(self):
+        OptionalDouble()
+
     def test_empty(self):
-        pass
+        OptionalDouble.empty()
     
 
 if __name__ == '__main__':

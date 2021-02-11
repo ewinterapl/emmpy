e8750d81d2435ae8ac262ef06415d877a8e4cfc4
Eric Winter <eric.winter@jhuapl.edu>
2021-02-11 09:15:26 -0500

    Added docstrings.

--- emmpy/crucible/core/designpatterns/builder.py
+++ emmpy/crucible/core/designpatterns/builder.py
@@ -7,11 +7,44 @@ class Builder:
     crucible.core.designpatterns.Builder
 
     Since this is an interface, all methods must be overridden
-    in classes implementing this interface
+    in classes implementing this interface.
     """
 
     def __init__(self):
+        """Initialize a new Builder object.
+
+        Parameters
+        ----------
+        self : Builder
+            New object to initialize.
+        
+        Returns
+        -------
+        self : Builder
+            New object after initialization.
+
+        Raises
+        ------
+        Exception
+            If called.
+        """
         raise Exception
 
     def build(self):
+        """Build something.
+
+        Parameters
+        ----------
+        self : Builder
+            This Builder object.
+        
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        Exception
+            If called.
+        """
         raise Exception

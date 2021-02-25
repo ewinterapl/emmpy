30baf2bce487a71fc4997e3b548af3e1c35d90e9
Eric Winter <eric.winter@jhuapl.edu>
2021-02-25 17:57:30 -0500

    Updated emmpy.com.google.common __init__.py.

--- emmpy/com/google/common/__init__.py
+++ emmpy/com/google/common/__init__.py
@@ -1,11 +1,12 @@
-"""common
+"""emmpy.com.google.common
 
 Python port of Java classes from the com.google.common hierarchy in the
 Google Guava package:
 
 https://guava.dev/releases/8.0/api/docs/overview-summary.html
 
-Modules
--------
+Packages
+--------
 base
+collect
 """

7aed98b4ab5d76ee27f3caf46887a40b7f960b16
Eric Winter <eric.winter@jhuapl.edu>
2021-03-02 13:24:15 -0500

    Completed first version of internaloperations.py.

--- emmpy/crucible/core/math/vectorspace/internaloperations.py
+++ emmpy/crucible/core/math/vectorspace/internaloperations.py
@@ -1,96 +1,175 @@
+"""internaloperations.py
+"""
+
+
 from math import sqrt
 
-# Class providing various static methods that support the implementation of methods of the various
-# other classes provided in this package.
-# <p>
-# <b>Maintainer Note:</b>This class is an implementation detail of the classes and methods provided
-# by this package as a whole. Functionality present here should not be exposed outside of this
-# package. Further, as methods defined here may be invoked from any class in this package, it must
-# remain free of references to all other classes in this package. In other words, it is at the
-# absolute bottom of the package layering structure.
-# </p>
-# 
-# @author F.S.Turner
+from emmpy.crucible.core.math.vectorspace.malformedrotationexception import (
+    MalformedRotationException
+)
+
 
 class InternalOperations:
+    """Class providing various static methods that support the implementation
+    of methods of the various other classes provided in this package.
 
-    # Computes the absolute value of the largest, magnitude, component of a vector expressed in three
-    # individual components.
-    # @param i the ith component
-    # @param j the jth component
-    # @param k the kth component
-    # @return the absolute value of the largest, in magnitude, component of the vector [i,j,k].
+    <b>Maintainer Note:</b>This class is an implementation detail of the
+    classes and methods provided by this package as a whole. Functionality
+    present here should not be exposed outside of this package. Further, as
+    methods defined here may be invoked from any class in this package, it must
+    remain free of references to all other classes in this package. In other
+    words, it is at the absolute bottom of the package layering structure.
+
+    @author F.S.Turner
+    """
+
     @staticmethod
-    def absMaxComponent(i, j, k):
-        return max(abs(i), max(abs(j), abs(k)))
+    def absMaxComponent(*args) -> float:
+        """Computes the absolute value of the largest, magnitude, component of
+        a vector expressed in three individual components.
+        @param i the ith component
+        @param j the jth component
+        @param k the kth component
+        @return the absolute value of the largest, in magnitude, component of
+        the vector [i,j,k].
+        """
+        if len(args) not in (2, 3):
+            raise Exception
+        return max(abs(x) for x in args)
 
-    # Method that computes the norm of a vector expressed as three separate components. This method
-    # is marked with package access to allow other classes in the vector arithmetic toolkit to
-    # utilize it.
-    # @param i the ith component
-    # @param j the jth component
-    # @param k the kth component
-    # @return the length of the [i,j,k] vector
     @staticmethod
-    def computeNorm(i, j, k):
-        max = InternalOperations.absMaxComponent(i, j, k)
+    def computeNorm(*args) -> float:
+        """Method that computes the norm of a vector expressed as three
+        separate components. This method is marked with package access to allow
+        other classes in the vector arithmetic toolkit to utilize it.
+        @param i the ith component
+        @param j the jth component
+        @param k the kth component
+        @return the length of the [i,j,k] vector
+        """
+        if len(args) == 2:
+            (i, j) = args
+            max = InternalOperations.absMaxComponent(i, j)
+            # If max is 0, then vector is clearly the zero vector.
+            if max == 0.0:
+                return 0.0
+            i /= max
+            j /= max
+            # Since we're trying to avoid overflow in the square root:
+            return max*sqrt(i*i + j*j)
+        elif len(args) == 3:
+            (i, j, k) = args
+            max = InternalOperations.absMaxComponent(i, j, k)
+            # If max is 0, then vector is clearly the zero vector.
+            if max == 0.0:
+                return 0.0
+            i /= max
+            j /= max
+            k /= max
+            # Since we're trying to avoid overflow in the square root:
+            return max*sqrt(i*i + j*j + k*k)
+        else:
+            raise Exception
 
-        # If max is 0, then vector is clearly the zero vector.
-        if max == 0.0:
-            return 0.0
+    @staticmethod
+    def checkRotation(*args) -> None:
+        """Determine if the components of a three by three matrix constitute a
+        rotation.
 
-        i /= max
-        j /= max
-        k /= max
+        @param ii ith row, ith column element
+        @param ji jth row, ith column element
+        @param ki kth row, ith column element
+        @param ij ith row, jth column element
+        @param jj jth row, jth column element
+        @param kj kth row, jth column element
+        @param ik ith row, kth column element
+        @param jk jth row, kth column element
+        @param kk kth row, kth column element
+        @param normTolerance tolerance off of unity for the magnitude of the
+        column vectors
+        @param detTolerance tolerance off of unity for the determinant of the
+        matrix
 
-        # Since we're trying to avoid overflow in the square root:
-        return max * sqrt(i * i + j * j + k * k)
+        @throws MalformedRotationException if the supplied components do not
+        adequately describe a rotation given the supplied tolerances
+        """
+        if len(args) == 8:
+            (ii, ji, ij, jj, normTolerance, detTolerance) = args
+            testVal = InternalOperations.computeNorm(ii, ji)
+            if (testVal < 1.0 - normTolerance) or (testVal > 1.0 +
+                                                   normTolerance):
+                raise MalformedRotationException(
+                    "Matrix's ith column is not sufficiently close to unit " +
+                    "length.")
+            testVal = InternalOperations.computeNorm(ij, jj)
+            if ((testVal < 1.0 - normTolerance) or (testVal > 1.0 +
+                                                    normTolerance)):
+                raise MalformedRotationException(
+                    "Matrix's jth column is not sufficiently close to unit " +
+                    "length.")
+            testVal = InternalOperations.computeDeterminant(ii, ji, ij, jj)
+            if ((testVal < 1.0 - detTolerance) or (testVal > 1.0 +
+                                                   detTolerance)):
+                raise MalformedRotationException(
+                    "Matrix's determinant is not sufficiently close to unity.")
+        elif len(args) == 11:
+            (ii, ji, ki, ij, jj, kj, ik, jk, kk,
+             normTolerance, detTolerance) = args
+            testVal = InternalOperations.computeNorm(ii, ji, ki)
+            if ((testVal < 1.0 - normTolerance) or (testVal > 1.0 +
+                                                    normTolerance)):
+                raise MalformedRotationException(
+                    "Matrix's ith column is not sufficiently close to unit " +
+                    "length.")
+            testVal = InternalOperations.computeNorm(ij, jj, kj)
+            if ((testVal < 1.0 - normTolerance) or (testVal > 1.0 +
+                                                    normTolerance)):
+                raise MalformedRotationException(
+                    "Matrix's jth column is not sufficiently close to unit " +
+                    "length."
+                )
+            testVal = InternalOperations.computeNorm(ik, jk, kk)
+            if (testVal < 1.0 - normTolerance) or (testVal > 1.0 +
+                                                   normTolerance):
+                raise MalformedRotationException(
+                    "Matrix's kth column is not sufficiently close to unit " +
+                    "length."
+                )
+            testVal = InternalOperations.computeDeterminant(ii, ji, ki,
+                                                            ij, jj, kj,
+                                                            ik, jk, kk)
+            if ((testVal < 1.0 - detTolerance) or (testVal > 1.0 +
+                                                   detTolerance)):
+                raise MalformedRotationException(
+                    "Matrix's determinant is not sufficiently close to unity.")
 
-    #   /**
-    #    * Determine if the components of a three by three matrix constitute a rotation.
-    #    * 
-    #    * @param ii ith row, ith column element
-    #    * @param ji jth row, ith column element
-    #    * @param ki kth row, ith column element
-    #    * @param ij ith row, jth column element
-    #    * @param jj jth row, jth column element
-    #    * @param kj kth row, jth column element
-    #    * @param ik ith row, kth column element
-    #    * @param jk jth row, kth column element
-    #    * @param kk kth row, kth column element
-    #    * @param normTolerance tolerance off of unity for the magnitude of the column vectors
-    #    * @param detTolerance tolerance off of unity for the determinant of the matrix
-    #    * 
-    #    * @throws MalformedRotationException if the supplied components do not adequately describe a
-    #    *         rotation given the supplied tolerances
-    #    */
-    @classmethod
-    def checkRotation(cls, ii, ji, ki, ij, jj, kj, ik, jk, kk, normTolerance, detTolerance):
-        #       throws MalformedRotationException {
+    def computeDeterminant(*args) -> float:
+        """Computes the determinant of a three by three matrix described by the
+        supplied components.
 
-        testVal = cls.computeNorm(ii, ji, ki)
-        if (testVal < 1.0 - normTolerance) or (testVal > 1.0 + normTolerance):
-            raise Exception("Matrix's ith column is not sufficiently close to unit length.")
-            # throw new MalformedRotationException(
-                # "Matrix's ith column is not sufficiently close to unit length.");
+        @param ii ith row, ith column element
+        @param ji jth row, ith column element
+        @param ki kth row, ith column element
+        @param ij ith row, jth column element
+        @param jj jth row, jth column element
+        @param kj kth row, jth column element
+        @param ik ith row, kth column element
+        @param jk jth row, kth column element
+        @param kk kth row, kth column element
 
-        #     testVal = computeNorm(ij, jj, kj);
-        #     if ((testVal < 1.0 - normTolerance) || (testVal > 1.0 + normTolerance)) {
-        #       throw new MalformedRotationException(
-        #           "Matrix's jth column is not sufficiently close to unit length.");
-        #     }
+        @return the determinant of the matrix described by the supplied
+        components.
 
-        #     testVal = computeNorm(ik, jk, kk);
-        #     if ((testVal < 1.0 - normTolerance) || (testVal > 1.0 + normTolerance)) {
-        #       throw new MalformedRotationException(
-        #           "Matrix's kth column is not sufficiently close to unit length.");
-        #     }
-
-        #     testVal = computeDeterminant(ii, ji, ki, ij, jj, kj, ik, jk, kk);
-
-        #     if ((testVal < 1.0 - detTolerance) || (testVal > 1.0 + detTolerance)) {
-        #       throw new MalformedRotationException(
-        #           "Matrix's determinant is not sufficiently close to unity.");
-        #     }
-
-        #   }
+        TODO: Consider scaling the components by the largest component and then
+        evaluating the determinant function. This may enhance numerical
+        precision.
+        """
+        if len(args) == 4:
+            (ii, ji, ij, jj) = args
+            # TODO: Consider scaling the components by the largest component
+            # and then evaluating the determinant function. This may enhance
+            # numerical precision.
+            return ii*jj - ji*ij
+        elif len(args) == 9:
+            (ii, ji, ki, ij, jj, kj, ik, jk, kk) = args
+            return ii*(jj*kk - jk*kj) - ij*(ji*kk - jk*ki) + ik*(ji*kj - jj*ki)
--- emmpy/crucible/core/math/vectorspace/tests/test_InternalOperations.py
+++ emmpy/crucible/core/math/vectorspace/tests/test_InternalOperations.py
@@ -1,20 +1,31 @@
 from math import sqrt
 import unittest
 
-import numpy as np
+# import numpy as np
 
-from emmpy.crucible.core.math.vectorspace.internaloperations import InternalOperations
+from emmpy.crucible.core.math.vectorspace.internaloperations import (
+    InternalOperations
+)
+
 
 class TestInternalOperations(unittest.TestCase):
 
     def test_absMaxComponent(self):
-        self.assertEqual(InternalOperations.absMaxComponent(1, 2, 3), 3)
+        self.assertEqual(InternalOperations.absMaxComponent(1, 2, -3), 3)
 
     def test_computeNorm(self):
-        self.assertTrue(np.isclose(InternalOperations.computeNorm(1, 1, 1), sqrt(3)))
+        self.assertAlmostEqual(InternalOperations.computeNorm(1, 1, 1),
+                               sqrt(3))
 
     def test_checkRotation(self):
-        InternalOperations.checkRotation(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+        InternalOperations.checkRotation(1, 0, 0, 0, 1, 0, 0, 0, 1, 1e-6, 1e-6)
+
+    def test_computeDeterminant(self):
+        self.assertAlmostEqual(
+            InternalOperations.computeDeterminant(0, 0, 0, 0, 0, 0, 0, 0, 0),
+            0
+        )
+
 
 if __name__ == '__main__':
     unittest.main()

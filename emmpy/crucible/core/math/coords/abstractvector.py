857ce73f0b95476cad67c9eb32eb5c6f998b93df
Eric Winter <eric.winter@jhuapl.edu>
2021-03-02 12:03:38 -0500

    Added AbstractVector stubs.

--- emmpy/crucible/core/math/coords/abstractvector.py
+++ emmpy/crucible/core/math/coords/abstractvector.py
@@ -1,10 +1,98 @@
-"""DESCRIPTION
+"""This abstract class is meant to assist implementors of new coordinate types.
 """
 
 
-class AbstractVector():
+# # import crucible.core.math.vectorspace.VectorIJK;
+# from emmpy.crucible.core.math.vectorspace.unwritablevectorijk import (
+#     UnwritableVectorIJK
+# )
 
-    def __init__(self):
-        """DESCRIPTION
-        """
-        pass
+
+# class AbstractVector():
+#     """This abstract class is meant to assist implementors of new coordinate
+#     types.
+
+#     The unwritable coordinate should extend this guy. It is also meant to help
+#     ensure that the outlines for all coordinates are consistent. This was also
+#     done, since the Jacobian classes require interaction with the Coordinates
+#     as VectorIJKs, specifically for calling mxv on a coordinate. This makes
+#     that possible. There may be further times where a coordinate needs to be
+#     used as a {@link VectorIJK}, as long as this occurs in this package, it is
+#     possible.
+
+#     The fact that a {@link VectorIJK} is the true composition class of all
+#     coordinate systems should not leave this package. We don't want different
+#     coordinate systems to be VectorIJKs as you wont know what coordinate system
+#     it is supposed to be.
+
+#     Also, if any other meabstractcoordinateconverterthods are needed on every
+#     coordinate class, they can hopefully just be added in here.
+
+#     @author G.K.Stephens
+#     """
+
+#     def __init__(self, i: float, j: float, k: float):
+#         """Constructs a coordinate from the three basic components.
+#         """
+#         self.ijkCoordinate = UnwritableVectorIJK(i, j, k)
+
+#     # These six methods should be wrapped in new methods with the appropriate
+#     # names. The getters in the unwritable and the setters in the writable.
+#     # These six methods should never have there visibility upgraded, but should
+#     # be wrapped.
+
+#     def getI(self) -> float:
+#         """THIS METHOD SHOULD NOT BE PUBLIC."""
+#         return self.ijkCoordinate.getI()
+
+#     def getJ(self) -> float:
+#         """THIS METHOD SHOULD NOT BE PUBLIC."""
+#         return self.ijkCoordinate.getJ()
+
+#     def getK(self) -> float:
+#         """THIS METHOD SHOULD NOT BE PUBLIC."""
+#         return self.ijkCoordinate.getK()
+
+#     def getVectorIJK(self) -> UnwritableVectorIJK:
+#         """THIS METHOD SHOULD NOT BE PUBLIC."""
+#         return self.ijkCoordinate
+
+#     def hashCode(self) -> int:
+#         """THIS METHOD SHOULD NOT BE PUBLIC.
+#         (non-Javadoc)
+
+#         @see java.lang.Object#hashCode()
+#         """
+#         prime = 31
+#         result = 1
+#         result = prime*result
+#         if self.ijkCoordinate is None:
+#             pass
+#         else:
+#             # This method needs to be implemented.
+#             result += self.ijkCoordinate.hashCode()
+#         return result
+
+#     def equals(self, obj: object) -> bool:
+#         """THIS METHOD SHOULD NOT BE PUBLIC.
+#         (non-Javadoc)
+
+#         @see java.lang.Object#equals(java.lang.Object)
+#         """
+#         if self is obj:
+#             return True
+#         if obj is None:
+#             return False
+#         if not isinstance(self, obj.__class__):
+#             return False
+#         # AbstractVector other = (AbstractVector) obj;
+#         other = obj
+#         if self.ijkCoordinate is None:
+#             if other.ijkCoordinate is not None:
+#                 return False
+#         elif not self.ijkCoordinate.equals(other.ijkCoordinate):
+#             return False
+#         return True
+
+#     def toString(self):
+#         raise Exception
--- emmpy/crucible/core/math/coords/tests/test_AbstractVector.py
+++ emmpy/crucible/core/math/coords/tests/test_AbstractVector.py
@@ -1,15 +1,36 @@
-import unittest
+# import unittest
 
-from emmpy.crucible.core.math.coords.abstractvector import (
-    AbstractVector
-)
-
-
-class TestAbstractVector(unittest.TestCase):
-
-    def test___init__(self):
-        AbstractVector()
+# from emmpy.crucible.core.math.coords.abstractvector import (
+#     AbstractVector
+# )
 
 
-if __name__ == '__main__':
-    unittest.main()
+# class TestAbstractVector(unittest.TestCase):
+
+#     def test___init__(self):
+#         AbstractVector(0, 0, 0)
+
+#     def test_getI(self):
+#         av = AbstractVector(0.1, 1.2, 2.3)
+#         self.assertAlmostEqual(av.getI(), 0.1)
+
+#     def test_getJ(self):
+#         av = AbstractVector(0.1, 1.2, 2.3)
+#         self.assertAlmostEqual(av.getJ(), 1.2)
+
+#     def test_getK(self):
+#         av = AbstractVector(0.1, 1.2, 2.3)
+#         self.assertAlmostEqual(av.getK(), 2.3)
+
+#     def test_getVectorIJK(self):
+#         av = AbstractVector(0.1, 1.2, 2.3)
+#         vijk = av.getVectorIJK()
+#         self.assertAlmostEqual(vijk.getI(), 0.1)
+#         self.assertAlmostEqual(vijk.getJ(), 1.2)
+#         self.assertAlmostEqual(vijk.getK(), 2.3)
+
+#     def test_hashCode(self):
+#         av = AbstractVector(0.1, 1.2, 2.3)
+
+# if __name__ == '__main__':
+#     unittest.main()

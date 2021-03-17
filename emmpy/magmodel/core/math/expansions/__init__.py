55f432bf716fee930336edcba43a4f7787648696
Eric Winter <eric.winter@jhuapl.edu>
2021-03-17 17:54:43 -0400

    Added remaining modue names.

--- emmpy/magmodel/core/math/expansions/__init__.py
+++ emmpy/magmodel/core/math/expansions/__init__.py
@@ -2,6 +2,31 @@
 
 Python port of Java classes from the magmodel.core.math.expansions hierarchy.
 
+This package lays defines commonly used interfaces and utility classes for
+describing the mathematical expansions that are frequently used in formulating
+series solutions of boundary value problems in three dimensions, such as
+solutions to Laplace's equation.
+
+@crucible.reliability unreliable
+@crucible.volatility unstable
+@crucible.disclosure group
+
 Modules
 -------
+arraycoefficientexpansion1d.py
+arraycoefficientexpansion2d.py
+arrayexpansion1d.py
+arrayexpansion2d.py
+coefficientexpansion1d.py
+coefficientexpansion2d.py
+coefficientexpansions.py
+expansion1d.py
+expansion1ds.py
+expansion2d.py
+expansion2ds.py
+listexpansion1d.py
+
+Packages
+--------
+tests
 """
---
+++ emmpy/magmodel/core/math/expansions/tests/__init__.py
@@ -0,0 +1,20 @@
+"""emmpy.magmodel.core.math.expansions.tests
+
+Tests for Python port of Java classes from the magmodel.core.math.expansions
+hierarchy.
+
+Modules
+-------
+test_ArrayCoefficientExpansion1D.py
+test_ArrayCoefficientExpansion2D.py
+test_ArrayExpansion1D.py
+test_ArrayExpansion2D.py
+test_CoefficientExpansion1D.py
+test_CoefficientExpansion2D.py
+test_CoefficientExpansions.py
+test_Expansion1D.py
+test_Expansion1Ds.py
+test_Expansion2D.py
+test_Expansion2Ds.py
+test_ListExpansion1D.py
+"""

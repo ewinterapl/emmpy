09939aa5b9c295a2fc3dd451a6815ac58b0a3e78
Eric Winter <eric.winter@jhuapl.edu>
2021-03-17 18:40:53 -0400

    Added deepHashCode() stub.

--- emmpy/java/util/arrays.py
+++ emmpy/java/util/arrays.py
@@ -4,3 +4,8 @@ class Arrays:
     def hashCode(a):
         """THIS IS A PLACEHOLDER!!!"""
         return 0
+
+    @staticmethod
+    def deepHashCode(a):
+        """THIS IS A PLACEHOLDER!!!"""
+        return 0
--- emmpy/java/util/tests/test_Arrays.py
+++ emmpy/java/util/tests/test_Arrays.py
@@ -6,7 +6,10 @@ from emmpy.java.util.arrays import Arrays
 class TestBuilder(unittest.TestCase):
 
     def test_hashCode(self):
-        self.assertEqual(Arrays.hashCode(), 0)
+        self.assertEqual(Arrays.hashCode([]), 0)
+
+    def test_deepHashCode(self):
+        self.assertEqual(Arrays.hashCode([]), 0)
 
 
 if __name__ == '__main__':

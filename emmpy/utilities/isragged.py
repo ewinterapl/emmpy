c6bd3225b081c15b23a836ca1bf49dd1896d3e8b
Eric Winter <eric.winter@jhuapl.edu>
2021-03-23 16:44:33 -0400

    Fixed indexing error in isRagged().

--- emmpy/utilities/isragged.py
+++ emmpy/utilities/isragged.py
@@ -6,6 +6,6 @@ def isRagged(a: list) -> bool:
     """If the argument is a ragged array, return True. If not,return False."""
     n = len(a[0])
     for x in a[1:]:
-        if len(a) != n:
+        if len(x) != n:
             return True
     return False

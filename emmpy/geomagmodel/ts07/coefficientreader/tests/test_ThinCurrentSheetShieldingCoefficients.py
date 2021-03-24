26b277745342dae9990b8c62812ec6af3e1870a3
Eric Winter <eric.winter@jhuapl.edu>
2021-03-24 09:52:32 -0400

    First complete version of ThinCurrentSheetShieldingCoefficients.

--- emmpy/geomagmodel/ts07/coefficientreader/tests/test_ThinCurrentSheetShieldingCoefficients.py
+++ emmpy/geomagmodel/ts07/coefficientreader/tests/test_ThinCurrentSheetShieldingCoefficients.py
@@ -1,5 +1,11 @@
 import unittest
 
+from emmpy.magmodel.core.math.expansions.coefficientexpansion1d import (
+    CoefficientExpansion1D
+)
+from emmpy.magmodel.core.math.expansions.coefficientexpansion2d import (
+    CoefficientExpansion2D
+)
 from emmpy.geomagmodel.ts07.coefficientreader.thincurrentsheetshieldingcoefficients import (
     ThinCurrentSheetShieldingCoefficients
 )
@@ -8,7 +14,156 @@ from emmpy.geomagmodel.ts07.coefficientreader.thincurrentsheetshieldingcoefficie
 class TestThinCurrentSheetShieldingCoefficients(unittest.TestCase):
 
     def test___init__(self):
-        pass
+        na = 3
+        nr = 2
+        symTail = CoefficientExpansion2D()
+        symTailWave = CoefficientExpansion1D()
+        oddTail = CoefficientExpansion2D()
+        oddTailWave = CoefficientExpansion1D()
+        evenTail = CoefficientExpansion2D()
+        evenTailWave = CoefficientExpansion1D()
+        ThinCurrentSheetShieldingCoefficients(
+            na, nr,
+            symTail, symTailWave,
+            oddTail, oddTailWave,
+            evenTail, evenTailWave
+        )
+
+    def test_getSymmetricTailExpansion(self):
+        na = 3
+        nr = 2
+        symTail = CoefficientExpansion2D()
+        symTailWave = CoefficientExpansion1D()
+        oddTail = CoefficientExpansion2D()
+        oddTailWave = CoefficientExpansion1D()
+        evenTail = CoefficientExpansion2D()
+        evenTailWave = CoefficientExpansion1D()
+        c = ThinCurrentSheetShieldingCoefficients(
+            na, nr,
+            symTail, symTailWave,
+            oddTail, oddTailWave,
+            evenTail, evenTailWave
+        )
+        self.assertTrue(c.getSymmetricTailExpansion(), symTail)
+
+    def test_getSymmetricTailWaveExpansion(self):
+        na = 3
+        nr = 2
+        symTail = CoefficientExpansion2D()
+        symTailWave = CoefficientExpansion1D()
+        oddTail = CoefficientExpansion2D()
+        oddTailWave = CoefficientExpansion1D()
+        evenTail = CoefficientExpansion2D()
+        evenTailWave = CoefficientExpansion1D()
+        c = ThinCurrentSheetShieldingCoefficients(
+            na, nr,
+            symTail, symTailWave,
+            oddTail, oddTailWave,
+            evenTail, evenTailWave
+        )
+        self.assertTrue(c.getSymmetricTailWaveExpansion(), symTailWave)
+
+    def test_getOddTailExpansion(self):
+        na = 3
+        nr = 2
+        symTail = CoefficientExpansion2D()
+        symTailWave = CoefficientExpansion1D()
+        oddTail = CoefficientExpansion2D()
+        oddTailWave = CoefficientExpansion1D()
+        evenTail = CoefficientExpansion2D()
+        evenTailWave = CoefficientExpansion1D()
+        c = ThinCurrentSheetShieldingCoefficients(
+            na, nr,
+            symTail, symTailWave,
+            oddTail, oddTailWave,
+            evenTail, evenTailWave
+        )
+        self.assertTrue(c.getOddTailExpansion(), oddTail)
+
+    def test_getOddTailWaveExpansion(self):
+        na = 3
+        nr = 2
+        symTail = CoefficientExpansion2D()
+        symTailWave = CoefficientExpansion1D()
+        oddTail = CoefficientExpansion2D()
+        oddTailWave = CoefficientExpansion1D()
+        evenTail = CoefficientExpansion2D()
+        evenTailWave = CoefficientExpansion1D()
+        c = ThinCurrentSheetShieldingCoefficients(
+            na, nr,
+            symTail, symTailWave,
+            oddTail, oddTailWave,
+            evenTail, evenTailWave
+        )
+        self.assertTrue(c.getOddTailWaveExpansion(), oddTailWave)
+
+    def test_getEvenTailExpansion(self):
+        na = 3
+        nr = 2
+        symTail = CoefficientExpansion2D()
+        symTailWave = CoefficientExpansion1D()
+        oddTail = CoefficientExpansion2D()
+        oddTailWave = CoefficientExpansion1D()
+        evenTail = CoefficientExpansion2D()
+        evenTailWave = CoefficientExpansion1D()
+        c = ThinCurrentSheetShieldingCoefficients(
+            na, nr,
+            symTail, symTailWave,
+            oddTail, oddTailWave,
+            evenTail, evenTailWave
+        )
+        self.assertTrue(c.getEvenTailExpansion(), evenTail)
+
+    def test_getEvenTailWaveExpansion(self):
+        na = 3
+        nr = 2
+        symTail = CoefficientExpansion2D()
+        symTailWave = CoefficientExpansion1D()
+        oddTail = CoefficientExpansion2D()
+        oddTailWave = CoefficientExpansion1D()
+        evenTail = CoefficientExpansion2D()
+        evenTailWave = CoefficientExpansion1D()
+        c = ThinCurrentSheetShieldingCoefficients(
+            na, nr,
+            symTail, symTailWave,
+            oddTail, oddTailWave,
+            evenTail, evenTailWave
+        )
+        self.assertTrue(c.getEvenTailWaveExpansion(), evenTailWave)
+
+    def test_getNumRadialExpansions(self):
+        na = 3
+        nr = 2
+        symTail = CoefficientExpansion2D()
+        symTailWave = CoefficientExpansion1D()
+        oddTail = CoefficientExpansion2D()
+        oddTailWave = CoefficientExpansion1D()
+        evenTail = CoefficientExpansion2D()
+        evenTailWave = CoefficientExpansion1D()
+        c = ThinCurrentSheetShieldingCoefficients(
+            na, nr,
+            symTail, symTailWave,
+            oddTail, oddTailWave,
+            evenTail, evenTailWave
+        )
+        self.assertEqual(c.getNumRadialExpansions(), nr)
+
+    def test_getNumAzimuthalExpansions(self):
+        na = 3
+        nr = 2
+        symTail = CoefficientExpansion2D()
+        symTailWave = CoefficientExpansion1D()
+        oddTail = CoefficientExpansion2D()
+        oddTailWave = CoefficientExpansion1D()
+        evenTail = CoefficientExpansion2D()
+        evenTailWave = CoefficientExpansion1D()
+        c = ThinCurrentSheetShieldingCoefficients(
+            na, nr,
+            symTail, symTailWave,
+            oddTail, oddTailWave,
+            evenTail, evenTailWave
+        )
+        self.assertEqual(c.getNumAzimuthalExpansions(), na)
 
 
 if __name__ == '__main__':
--- emmpy/geomagmodel/ts07/coefficientreader/thincurrentsheetshieldingcoefficients.py
+++ emmpy/geomagmodel/ts07/coefficientreader/thincurrentsheetshieldingcoefficients.py
@@ -1,103 +1,101 @@
 """emmpy.geomagmodel.ts07.coefficientreader.thincurrentsheetshieldingcoefficients.py"""
 
-# package geomagmodel.ts07.coefficientreader;
 
-# import static com.google.common.base.Preconditions.checkNotNull;
 
+from emmpy.com.google.common.base.preconditions import Preconditions
 # import magmodel.core.math.expansions.CoefficientExpansion1D;
 # import magmodel.core.math.expansions.CoefficientExpansion2D;
 # import magmodel.core.math.expansions.Expansion1D;
 # import magmodel.core.math.expansions.Expansion2D;
 
+
 class ThinCurrentSheetShieldingCoefficients:
-    pass
+    """This class stores the static coefficients for the TS07D model. These
+    are available on the model webpage at
+    http://geomag_field.jhuapl.edu/model/TAIL_PAR.zip.
 
-    # /**
-    #  * This class stores the static coefficients for the TS07D model. These are available on the model
-    #  * webpage at http://geomag_field.jhuapl.edu/model/TAIL_PAR.zip.
-    #  * <p>
-    #  * To construct this class, use the {@link TS07DStaticCoefficientsFactory} class.
-    #  * <p>
-    #  * 
-    #  * @author Nicholas Sharp
-    #  * @author G.K.Stephens
-    #  * 
-    #  */
-    # public class ThinCurrentSheetShieldingCoefficients {
+    To construct this class, use the {@link TS07DStaticCoefficientsFactory}
+    class.
 
-    #   private final int numAzimuthalExpansions;
-    #   private final int numRadialExpansions;
+    @author Nicholas Sharp
+    @author G.K.Stephens
+    """
 
-    #   private final Expansion1D<CoefficientExpansion2D> symmetricTailExpansion;
-    #   private final Expansion1D<CoefficientExpansion1D> symmetricTailWaveExpansion;
+    # private final int numAzimuthalExpansions;
+    # private final int numRadialExpansions;
+    # private final Expansion1D<CoefficientExpansion2D> symmetricTailExpansion;
+    # private final Expansion1D<CoefficientExpansion1D> symmetricTailWaveExpansion;
+    # private final Expansion2D<CoefficientExpansion2D> oddTailExpansion;
+    # private final Expansion2D<CoefficientExpansion1D> oddTailWaveExpansion;
+    # private final Expansion2D<CoefficientExpansion2D> evenTailExpansion;
+    # private final Expansion2D<CoefficientExpansion1D> evenTailWaveExpansion;
 
-    #   private final Expansion2D<CoefficientExpansion2D> oddTailExpansion;
-    #   private final Expansion2D<CoefficientExpansion1D> oddTailWaveExpansion;
+    def __init__(
+        self, numAzimuthalExpansions, numRadialExpansions,
+        symmetricTailExpansion, symmetricTailWaveExpansion,
+        oddTailExpansion, oddTailWaveExpansion,
+        evenTailExpansion, evenTailWaveExpansion):
+        """Constructor is package private, should be constructed using the
+        {@link TS07DStaticCoefficientsFactory} class.
 
-    #   private final Expansion2D<CoefficientExpansion2D> evenTailExpansion;
-    #   private final Expansion2D<CoefficientExpansion1D> evenTailWaveExpansion;
+        @param numAzimuthalExpansions
+        @param numRadialExpansions
+        @param symmetricTailExpansion
+        @param symmetricTailWaveExpansion
+        @param oddTailExpansion
+        @param oddTailWaveExpansion
+        @param evenTailExpansion
+        @param evenTailWaveExpansion
+        """
+        # ThinCurrentSheetShieldingCoefficients(int numAzimuthalExpansions, int numRadialExpansions,
+        #     Expansion1D<CoefficientExpansion2D> symmetricTailExpansion,
+        #     Expansion1D<CoefficientExpansion1D> symmetricTailWaveExpansion,
+        #     Expansion2D<CoefficientExpansion2D> oddTailExpansion,
+        #     Expansion2D<CoefficientExpansion1D> oddTailWaveExpansion,
+        #     Expansion2D<CoefficientExpansion2D> evenTailExpansion,
+        #     Expansion2D<CoefficientExpansion1D> evenTailWaveExpansion) {
 
-    #   /**
-    #    * Constructor is package private, should be constructed using the
-    #    * {@link TS07DStaticCoefficientsFactory} class.
-    #    * 
-    #    * @param numAzimuthalExpansions
-    #    * @param numRadialExpansions
-    #    * @param symmetricTailExpansion
-    #    * @param symmetricTailWaveExpansion
-    #    * @param oddTailExpansion
-    #    * @param oddTailWaveExpansion
-    #    * @param evenTailExpansion
-    #    * @param evenTailWaveExpansion
-    #    */
-    #   ThinCurrentSheetShieldingCoefficients(int numAzimuthalExpansions, int numRadialExpansions,
-    #       Expansion1D<CoefficientExpansion2D> symmetricTailExpansion,
-    #       Expansion1D<CoefficientExpansion1D> symmetricTailWaveExpansion,
-    #       Expansion2D<CoefficientExpansion2D> oddTailExpansion,
-    #       Expansion2D<CoefficientExpansion1D> oddTailWaveExpansion,
-    #       Expansion2D<CoefficientExpansion2D> evenTailExpansion,
-    #       Expansion2D<CoefficientExpansion1D> evenTailWaveExpansion) {
-    #     super();
-    #     this.numRadialExpansions = checkNotNull(numRadialExpansions);
-    #     this.numAzimuthalExpansions = checkNotNull(numAzimuthalExpansions);
-    #     this.symmetricTailExpansion = checkNotNull(symmetricTailExpansion);
-    #     this.symmetricTailWaveExpansion = checkNotNull(symmetricTailWaveExpansion);
-    #     this.oddTailExpansion = checkNotNull(oddTailExpansion);
-    #     this.oddTailWaveExpansion = checkNotNull(oddTailWaveExpansion);
-    #     this.evenTailExpansion = checkNotNull(evenTailExpansion);
-    #     this.evenTailWaveExpansion = checkNotNull(evenTailWaveExpansion);
-    #   }
+        self.numRadialExpansions = (
+            Preconditions.checkNotNull(numRadialExpansions)
+        )
+        self.numAzimuthalExpansions = (
+            Preconditions.checkNotNull(numAzimuthalExpansions)
+        )
+        self.symmetricTailExpansion = (
+            Preconditions.checkNotNull(symmetricTailExpansion)
+        )
+        self.symmetricTailWaveExpansion = (
+            Preconditions.checkNotNull(symmetricTailWaveExpansion)
+        )
+        self.oddTailExpansion = Preconditions.checkNotNull(oddTailExpansion)
+        self.oddTailWaveExpansion = (
+            Preconditions.checkNotNull(oddTailWaveExpansion)
+        )
+        self.evenTailExpansion = Preconditions.checkNotNull(evenTailExpansion)
+        self.evenTailWaveExpansion = (
+            Preconditions.checkNotNull(evenTailWaveExpansion)
+        )
 
-    #   public Expansion1D<CoefficientExpansion2D> getSymmetricTailExpansion() {
-    #     return symmetricTailExpansion;
-    #   }
+    def getSymmetricTailExpansion(self):
+        return self.symmetricTailExpansion
 
-    #   public Expansion1D<CoefficientExpansion1D> getSymmetricTailWaveExpansion() {
-    #     return symmetricTailWaveExpansion;
-    #   }
+    def getSymmetricTailWaveExpansion(self):
+        return self.symmetricTailWaveExpansion
 
-    #   public Expansion2D<CoefficientExpansion2D> getOddTailExpansion() {
-    #     return oddTailExpansion;
-    #   }
+    def getOddTailExpansion(self):
+        return self.oddTailExpansion
 
-    #   public Expansion2D<CoefficientExpansion1D> getOddTailWaveExpansion() {
-    #     return oddTailWaveExpansion;
-    #   }
+    def getOddTailWaveExpansion(self):
+        return self.oddTailWaveExpansion
 
-    #   public Expansion2D<CoefficientExpansion2D> getEvenTailExpansion() {
-    #     return evenTailExpansion;
-    #   }
+    def getEvenTailExpansion(self):
+        return self.evenTailExpansion
 
-    #   public Expansion2D<CoefficientExpansion1D> getEvenTailWaveExpansion() {
-    #     return evenTailWaveExpansion;
-    #   }
+    def getEvenTailWaveExpansion(self):
+        return self.evenTailWaveExpansion
 
-    #   public int getNumRadialExpansions() {
-    #     return numRadialExpansions;
-    #   }
+    def getNumRadialExpansions(self):
+        return self.numRadialExpansions
 
-    #   public int getNumAzimuthalExpansions() {
-    #     return numAzimuthalExpansions;
-    #   }
-
-    # }
+    def getNumAzimuthalExpansions(self):
+        return self.numAzimuthalExpansions

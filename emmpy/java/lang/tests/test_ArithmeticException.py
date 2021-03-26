2ac5470751bc1d57cc0ec0667843588320bbadce
Eric Winter <eric.winter@jhuapl.edu>
2021-03-26 15:01:05 -0400

    First complete version of arithmeticException.

---
+++ emmpy/java/lang/tests/test_ArithmeticException.py
@@ -0,0 +1,13 @@
+import unittest
+
+from emmpy.java.lang.arithmeticexception import ArithmeticException
+
+
+class TestBuilder(unittest.TestCase):
+
+    def test___init__(self):
+        ArithmeticException()
+
+
+if __name__ == '__main__':
+    unittest.main()

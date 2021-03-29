17942b3a891578bedddf10e545bdf1f9e7635bc6
Eric Winter <eric.winter@jhuapl.edu>
2021-03-29 12:19:06 -0400

    First complete ColtBesselFunctionEvaluator.

--- emmpy/magmodel/core/math/bessel/coltbesselfunctionevaluator.py
+++ emmpy/magmodel/core/math/bessel/coltbesselfunctionevaluator.py
@@ -1,54 +1,44 @@
 """emmpy.magmodel.core.math.bessel.coltbesselfunctionevaluator"""
 
-# package magmodel.core.math.bessel;
+from emmpy.magmodel.core.math.bessel.besselfunctionevaluator import (
+    BesselFunctionEvaluator
+)
+from emmpy.magmodel.core.math.bessel.coltbessel import ColtBessel
 
-# import static crucible.core.math.CrucibleMath.abs;
 
-class ColtBesselFunctionEvaluator:
-    pass
+class ColtBesselFunctionEvaluator(BesselFunctionEvaluator):
+    """An implementation of a {@link BesselFunctionEvaluator} using the Colt
+    library.
 
-# /**
-#  * An implementation of a {@link BesselFunctionEvaluator} using the Colt library.
-#  * 
-#  * @author G.K.Stephens
-#  *
-#  */
-# public class ColtBesselFunctionEvaluator implements BesselFunctionEvaluator {
+    @author G.K.Stephens
+    """
 
-#   @Override
-#   public double besselj0(double x) {
-#     return ColtBessel.j0(x);
-#   }
+    @staticmethod
+    def besselj0(x):
+        return ColtBessel.j0(x)
 
-#   @Override
-#   public double besselj1(double x) {
-#     return ColtBessel.j1(x);
-#   }
+    @staticmethod
+    def besselj1(x):
+        return ColtBessel.j1(x)
 
-#   @Override
-#   public double besseljn(int n, double x) {
-#     /*
-#      * TODO Ugh, when x is small but non-zero, the Colt evaluator will often return NaNs. This is a
-#      * bug in their algorithm, that looks like the recursive algorithm gets so small that it is
-#      * smaller than the smallest double. Tsyganenko's code has the same problem, so Sasha fixed it
-#      * with the following change. This is a hack though, the algorithm should be fixed.
-#      */
-#     if (abs(n) > 1 && abs(x) < 1.0E-12) {
-#       return 0.0;
-#     }
-#     return ColtBessel.jn(n, x);
-#   }
+    @staticmethod
+    def besseljn(n, x):
+        """besseljn
 
-#   @Override
-#   public double[] besselj0jn(int n, double x) {
+        TODO Ugh, when x is small but non-zero, the Colt evaluator will often
+        return NaNs. This is a bug in their algorithm, that looks like the
+        recursive algorithm gets so small that it is smaller than the smallest
+        double. Tsyganenko's code has the same problem, so Sasha fixed it with
+        the following change. This is a hack though, the algorithm should be
+        fixed.
+        """
+        if abs(n) > 1 and abs(x) < 1.0E-12:
+            return 0.0
+        return ColtBessel.jn(n, x)
 
-#     double[] bessels = new double[n + 1];
-
-#     for (int nn = 0; nn <= n; nn++) {
-#       bessels[nn] = besseljn(nn, x);
-#     }
-
-#     return bessels;
-#   }
-
-# }
+    @staticmethod
+    def besselj0jn(n, x):
+        bessels = [None]*(n + 1)
+        for nn in range(n + 1):
+            bessels[nn] = ColtBesselFunctionEvaluator.besseljn(nn, x)
+        return bessels
--- emmpy/magmodel/core/math/bessel/tests/test_ColtBesselFunctionEvaluator.py
+++ emmpy/magmodel/core/math/bessel/tests/test_ColtBesselFunctionEvaluator.py
@@ -1,5 +1,7 @@
 import unittest
 
+import scipy.special as ss
+
 from emmpy.magmodel.core.math.bessel.coltbesselfunctionevaluator import (
     ColtBesselFunctionEvaluator
 )
@@ -7,8 +9,31 @@ from emmpy.magmodel.core.math.bessel.coltbesselfunctionevaluator import (
 
 class TestTestColtBesselFunctionEvaluatorr(unittest.TestCase):
 
-    def test___init__(self):
-        pass
+    def test_besselj0(self):
+        for x in range(-10, 11):
+            self.assertAlmostEqual(ColtBesselFunctionEvaluator.besselj0(x),
+                                   ss.jv(0, x))
+
+    def test_besselj1(self):
+        for x in range(-10, 11):
+            self.assertAlmostEqual(ColtBesselFunctionEvaluator.besselj1(x),
+                                   ss.jv(1, x))
+
+    def test_besseljn(self):
+        for n in range(10):
+            for x in range(-10, 11):
+                self.assertAlmostEqual(
+                    ColtBesselFunctionEvaluator.besseljn(n, x),
+                    ss.jv(n, x), places=6)
+
+    def test_besselj0jn(self):
+        n = 5
+        orders = list(range(n + 1))
+        for x in range(-10, 11):
+            xx = ColtBesselFunctionEvaluator.besselj0jn(n, x)
+            yy = ss.jv(orders, x)
+            for i in range(n + 1):
+                self.assertAlmostEqual(xx[i], yy[i], places=6)
 
 
 if __name__ == '__main__':

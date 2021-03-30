46b9b16f1da067f7242f74ec0fe9976acbce9d15
Eric Winter <eric.winter@jhuapl.edu>
2021-03-30 15:17:59 -0400

    First complete version of BasisVectorField.

---
+++ emmpy/magmodel/core/math/vectorfields/basisvectorfield.py
@@ -0,0 +1,51 @@
+"""emmpy.magmodel.core.math.vectorfields.basisvectorfield
+
+N.B. This class was created from a Java interface, and therefore most of these
+methods will raise exceptions if invoked.
+"""
+
+from emmpy.crucible.core.math.vectorfields.vectorfield import VectorField
+
+
+class BasisVectorField(VectorField):
+    """An interface that represents a VectorField that can decomposed into a
+    linear expansion of vector fields.
+
+    returns the results of the vector field expansion in an ImmutableList.
+
+    The coefficients a<sub>i</sub> are considered an implementation detail.
+
+    @author G.K.Stephens
+    """
+
+    def __init__(self):
+        """Interface - do not instantiate."""
+        raise Exception
+
+    def evaluate(self, location, buffer):
+        """evaluate"""
+        basisVectors = self.evaluateExpansion(location)
+        fx = 0.0
+        fy = 0.0
+        fz = 0.0
+        for basisVector in basisVectors:
+            fx += basisVector.getI()
+            fy += basisVector.getJ()
+            fz += basisVector.getK()
+        return buffer.setTo(fx, fy, fz)
+
+    def evaluateExpansion(self, location):
+        """Evaluate the field expansion at the given position, and returns an
+        ImmutableList of the results of each individual field in the expansion
+
+        param location VectorIJK, often location
+        return the result of evaluating each of the Basis functions VectorField
+        for the given location as an ImmutableList
+        throws FunctionEvaluationException if the function cannot perform the
+        evaluation
+        """
+        raise Exception
+
+    def getNumberOfBasisFunctions(self):
+        """return the number of individual vector fields in the expansion"""
+        raise Exception

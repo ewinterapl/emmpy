de73041533bad9121a38aed68c169947306d98e9
Eric Winter <eric.winter@jhuapl.edu>
2021-03-30 11:36:49 -0400

    First complete version of ScalarField2D.

--- emmpy/crucible/core/math/vectorfields/scalarfield2d.py
+++ emmpy/crucible/core/math/vectorfields/scalarfield2d.py
@@ -1,32 +1,26 @@
+"""emmpy.crucible.core.math.vectorfields.scalarfield2d"""
+
+
 class ScalarField2D:
+    """Represents a 2 dimensional scalar field, which maps a VectorIJ to a
+    scalar (double).
 
-    """ScalarField2D
-
-    A Python interface template ...
-    
-    Based on the original Java interface:
-    crucible.core.math.vectorfields.ScalarField2D
-
-    Since this is an interface, all methods MUST be overridden
-    in classes implementing this interface.
+    @author G.K.Stephens
     """
 
-    def __init__(self):
-        """Initialize a new ScalarField2D object.
+    def __init__(self, location):
+        """Interface - do not instantiate."""
+        raise Exception
 
-        Parameters
-        ----------
-        self : ScalarField2D
-            New object to initialize.
-        
-        Returns
-        -------
-        self : ScalarField2D
-            New object after initialization.
+    def evaluate(self, location):
+        """Evaluate the scalar at the given position, return the double.
 
-        Raises
-        ------
-        Exception
-            If called.
+        units and such are up to the implementors
+
+        @param location {@link VectorIJ}, often location
+        @return the scalar
+        @throws FunctionEvaluationException if the function cannot perform the
+        evaluation
         """
+        # Interface - do not invoke.
         raise Exception
--- emmpy/crucible/core/math/vectorfields/tests/test_ScalarField2D.py
+++ emmpy/crucible/core/math/vectorfields/tests/test_ScalarField2D.py
@@ -1,13 +1,18 @@
 import unittest
 
-from emmpy.templates.scalarfield2d import ScalarField2D
+from emmpy.crucible.core.math.vectorfields.scalarfield2d import ScalarField2D
+
 
 class TestBuilder(unittest.TestCase):
 
     def test___init__(self):
         with self.assertRaises(Exception):
             ScalarField2D()
-    
+
+    def test_evaluate(self):
+        with self.assertRaises(Exception):
+            ScalarField2D.evaluate(None, None)
+
 
 if __name__ == '__main__':
     unittest.main()

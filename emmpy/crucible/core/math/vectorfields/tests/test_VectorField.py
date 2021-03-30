cb76d9430497aec068ce40c9175155709157c11a
Eric Winter <eric.winter@jhuapl.edu>
2021-03-30 14:33:25 -0400

    Uodated to improve interface documentation.

--- emmpy/crucible/core/math/vectorfields/tests/test_VectorField.py
+++ emmpy/crucible/core/math/vectorfields/tests/test_VectorField.py
@@ -6,13 +6,14 @@ from emmpy.crucible.core.math.vectorspace.vectorijk import VectorIJK
 
 class TestBuilder(unittest.TestCase):
 
-    # def test___init__(self):
-    #     with self.assertRaises(Exception):
-    #         VectorField()
+    def test___init__(self):
+        with self.assertRaises(Exception):
+            VectorField()
 
     def test_evaluate(self):
         with self.assertRaises(Exception):
             VectorField().evaluate(VectorIJK())
+
 
 if __name__ == '__main__':
     unittest.main()
--- emmpy/crucible/core/math/vectorfields/vectorfield.py
+++ emmpy/crucible/core/math/vectorfields/vectorfield.py
@@ -1,40 +1,46 @@
-"""vectorfield"""
+"""emmpy.crucible.core.math.vectorfields.vectorfield
 
-# package crucible.core.math.vectorfields;
+N.B. This class was created from a Java interface, and therefore most of these
+methods will raise exceptions if invoked.
+"""
 
-# import crucible.core.exceptions.FunctionEvaluationException;
-# import crucible.core.math.vectorspace.UnwritableVectorIJK;
-# import crucible.core.math.vectorspace.VectorIJK;
+
+from emmpy.crucible.core.math.vectorspace.vectorijk import VectorIJK
+
 
 class VectorField:
-    """Represents a 3 dimensional vector field, which maps a {@link VectorIJK}
-    to another {@link VectorIJK}.
+    """Represents a 3 dimensional vector field, which maps a VectorIJK to
+    another VectorIJK.
 
-    @author vandejd1
-    @author G.K.Stephens
+    author vandejd1
+    author G.K.Stephens
     """
 
-    # def __init__(self):
-    #     """Interface - do not instantiate"""
-    #     raise Exception
+    def __init__(self):
+        """Interface - do not instantiate"""
+        raise Exception
 
     def evaluate(self, *args):
         """Evaluate the field at the given position
 
         units and such are up to the implementors
 
-        @throws FunctionEvaluationException if the function cannot perform the
+        throws FunctionEvaluationException if the function cannot perform the
         evaluation
         """
         if len(args) == 1:
-            # @param location {@link VectorIJK}, often location
-            # @return the resultant {@link VectorIJK}
+            # param location VectorIJK, often location
+            # return the resultant VectorIJK
             (location,) = args
             return self.evaluate(location, VectorIJK())
         elif len(args) == 2:
             # Evaluate the field at the given position, put the result into the
             # buffer and return the buffer
-            # @param location {@link VectorIJK}, often location
-            # @param buffer a {@link VectorIJK} buffer that will hold the result of the evaluation
-            # @return the buffer
+            # param location VectorIJK, often location
+            # param buffer a VectorIJK buffer that will hold the result of the
+            # evaluation
+            # return the buffer
+            (location, buffer) = args
+            raise Exception
+        else:
             raise Exception

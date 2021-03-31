d516541387fc1cbf3aba47e98ddcf4210306d0de
Eric Winter <eric.winter@jhuapl.edu>
2021-03-31 17:52:13 -0400

    First complete version of AbstractVectorFieldValue. Need more complete tests.

--- emmpy/crucible/core/math/coords/abstractvectorfieldvalue.py
+++ emmpy/crucible/core/math/coords/abstractvectorfieldvalue.py
@@ -1,10 +1,75 @@
-"""DESCRIPTION
-"""
+"""emmpy.crucible.core.math.coords.abstractvectorfieldvalue"""
 
 
-class AbstractVectorFieldValue():
+# import static com.google.common.base.Preconditions.checkNotNull;
 
-    def __init__(self):
-        """DESCRIPTION
+from emmpy.com.google.common.base.preconditions import Preconditions
+from emmpy.crucible.core.math.coords.vectorfieldvalue import VectorFieldValue
+
+
+class AbstractVectorFieldValue(VectorFieldValue):
+    """AbstractVectorFieldValue"""
+
+    #    * The field containing the buffer that holds the position component of the state.
+    #   private final C position;
+
+    #    * The field containing the buffer that holds the value component of the state.
+    #   private final C value;
+
+    def __init__(self, position, value):
+        """Creates a state.
+
+        @param position the position of one object relative to another.
+        @param value the time derivative of the supplied position.
         """
-        pass
+        #   public AbstractVectorFieldValue(C position, C value) {
+        self.position = Preconditions.checkNotNull(position)
+        self.value = Preconditions.checkNotNull(value)
+
+    def getPosition(self):
+        return self.position
+
+    def getValue(self):
+        return self.value
+
+    def toString(self):
+        return "[position=%s, value=%s]" % (self.position, self.value)
+
+    def hashCode(self):
+        """@see java.lang.Object#hashCode()
+
+        (non-Javadoc)
+        """
+        prime = 31
+        result = 1
+        result = prime*result
+        if self.position is not None:
+            result += self.position.hashCode()
+        result = prime*result
+        if self.value is not None:
+            result += self.value.hashCode()
+        return result
+
+    def equals(self, obj):
+        """@see java.lang.Object#equals(java.lang.Object)
+
+        (non-Javadoc)
+        """
+        if self is obj:
+            return True
+        if obj is None:
+            return False
+        if self.__class__ != obj.__class__:
+            return False
+        other = obj
+        if self.position is None:
+            if other.position is not None:
+                return False
+        elif not self.position.equals(other.position):
+            return False
+        if self.value is None:
+            if other.value is not None:
+                return False
+        elif not self.value.equals(other.value):
+            return False
+        return True
--- emmpy/crucible/core/math/coords/tests/test_AbstractVectorFieldValue.py
+++ emmpy/crucible/core/math/coords/tests/test_AbstractVectorFieldValue.py
@@ -8,8 +8,33 @@ from emmpy.crucible.core.math.coords.abstractvectorfieldvalue import (
 class TestAbstractVectorFieldValue(unittest.TestCase):
 
     def test___init__(self):
-        AbstractVectorFieldValue()
+        avfv = AbstractVectorFieldValue([0], [0])
+        self.assertIsNotNone(avfv)
+        self.assertAlmostEqual(avfv.position, [0])
+        self.assertAlmostEqual(avfv.value, [0])
 
+    def test_getPosition(self):
+        avfv = AbstractVectorFieldValue([0], [0])
+        self.assertAlmostEqual(avfv.getPosition(), [0])
+
+    def test_getValue(self):
+        avfv = AbstractVectorFieldValue([0], [0])
+        self.assertAlmostEqual(avfv.getValue(), [0])
+
+    def test_toString(self):
+        avfv = AbstractVectorFieldValue([0], [0])
+        self.assertEqual(avfv.toString(), "[position=[0], value=[0]]")
+
+    def test_hashCode(self):
+        # FIX THIS TEST.
+        avfv = AbstractVectorFieldValue([0], [0])
+        self.assertEqual(avfv.hashCode, 0)
+
+    def test_equals(self):
+        avfv1 = AbstractVectorFieldValue([0], [0])
+        avfv2 = AbstractVectorFieldValue([0], [0])
+        avfv3 = AbstractVectorFieldValue([1], [0])
+        self.assertTrue(avfv1.equals(avfv2))
 
 if __name__ == '__main__':
     unittest.main()

b7cc2679956f37ee049269f004d297a8aa4e79f7
Eric Winter <eric.winter@jhuapl.edu>
2021-03-31 14:22:42 -0400

    Completed CylindricalVector, other than ZERO.

--- emmpy/crucible/core/math/coords/cylindricalvector.py
+++ emmpy/crucible/core/math/coords/cylindricalvector.py
@@ -1,10 +1,36 @@
-"""DESCRIPTION
-"""
+"""emmpy.crucible.core.math.coords.cylindricalvector"""
 
 
-class CylindricalVector():
+from emmpy.crucible.core.math.coords.abstractvector import AbstractVector
 
-    def __init__(self):
-        """DESCRIPTION
-        """
-        pass
+
+class CylindricalVector(AbstractVector):
+    """A class representing a vector in the cylindrical coordinate system.
+
+    @author G.K.Stephens
+    """
+
+    # The ZERO vector.
+    # ZERO = CylindricalVector(0, 0, 0)
+
+    def __init__(self, cylindricalRadius, longInRadians, height):
+        """Constructor"""
+        AbstractVector.__init__(self, cylindricalRadius, longInRadians, height)
+
+    def getCylindricalRadius(self):
+        """return the cylindrical radius (often denoted as r)"""
+        return AbstractVector.getI(self)
+
+    def getLongitude(self):
+        """return the longitude"""
+        return AbstractVector.getJ(self)
+
+    def getHeight(self):
+        """return the height (often denoted as z)"""
+        return AbstractVector.getK(self)
+
+    def toString(self):
+        return (
+            "CylindricalVector [cylindricalRadius: %s, longitude: %s, height: %s]"
+            % (self.getCylindricalRadius(), self.getLongitude(), self.getHeight())
+        )
--- emmpy/crucible/core/math/coords/tests/test_CylindricalVector.py
+++ emmpy/crucible/core/math/coords/tests/test_CylindricalVector.py
@@ -6,7 +6,26 @@ from emmpy.crucible.core.math.coords.cylindricalvector import CylindricalVector
 class TestCylindricalVector(unittest.TestCase):
 
     def test___init__(self):
-        CylindricalVector()
+        CylindricalVector(0, 0, 0)
+
+    def test_getCylindricalRadius(self):
+        cv = CylindricalVector(1.1, 2.2, 3.3)
+        self.assertAlmostEqual(cv.getCylindricalRadius(), 1.1)
+
+    def test_getLongitude(self):
+        cv = CylindricalVector(1.1, 2.2, 3.3)
+        self.assertAlmostEqual(cv.getLongitude(), 2.2)
+
+    def test_getHeight(self):
+        cv = CylindricalVector(1.1, 2.2, 3.3)
+        self.assertAlmostEqual(cv.getHeight(), 3.3)
+
+    def test_toString(self):
+        cv = CylindricalVector(1.1, 2.2, 3.3)
+        self.assertEqual(
+            cv.toString(),
+            "CylindricalVector [cylindricalRadius: 1.1, longitude: 2.2, "
+            "height: 3.3]")
 
 
 if __name__ == '__main__':

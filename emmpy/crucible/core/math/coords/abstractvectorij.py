6cddb81f4c8a2a5cc4f92c4cbbdecbb18e463b9f
Eric Winter <eric.winter@jhuapl.edu>
2021-04-02 11:50:16 -0400

    First complete version of AbstractVectorIJ.

--- emmpy/crucible/core/math/coords/abstractvectorij.py
+++ emmpy/crucible/core/math/coords/abstractvectorij.py
@@ -1,10 +1,89 @@
-"""DESCRIPTION
-"""
+"""emmpy.crucible.core.math.coords.abstractvectorij"""
 
 
-class AbstractVectorIJ():
+from emmpy.crucible.core.math.vectorspace.unwritablevectorij import (
+    UnwritableVectorIJ
+)
+# import crucible.core.math.vectorspace.VectorIJ;
 
-    def __init__(self):
-        """DESCRIPTION
+class AbstractVectorIJ:
+    """AbstractVectorIJ
+
+    This abstract class is meant to assist implementors of new coordinate
+    types. The unwritable coordinate should extend this guy. It is also meant
+    to help ensure that the outlines for all coordinates are consistent. This
+    was also done, since the Jacobian classes require interaction with the
+    Coordinates as VectorIJs, specifically for calling mxv on a coordinate.
+    This makes that possible. There may be further times where a coordinate
+    needs to be used as a {@link VectorIJ}, as long as this occurs in this
+    package, it is possible.
+
+    The fact that a {@link VectorIJ} is the true composition class of all
+    coordinate systems should not leave this package. We don't want different
+    coordinate systems to be VectorIJs as you wont know what coordinate system
+    it is supposed to be.
+
+    Also, if any other methods are needed on every coordinate class, they can
+    hopefully just be added in here.
+ 
+    @author G.K.Stephens
+    """
+
+    def __init__(self, i, j):
+        """Constructs a coordinate from the three basic components."""
+        self.ijCoordinate = UnwritableVectorIJ(i, j)
+
+    # These six methods should be wrapped in new methods with the appropriate
+    # names. The getters in the unwritable and the setters in the writable.
+    # These six methods should never have there visibility upgraded, but should
+    # be wrapped.
+
+    def getI(self):
+        """THIS METHOD SHOULD NOT BE PUBLIC"""
+        return self.ijCoordinate.getI()
+
+    def getJ(self):
+        """THIS METHOD SHOULD NOT BE PUBLIC"""
+        return self.ijCoordinate.getJ()
+
+    def getVectorIJ(self):
+        """THIS METHOD SHOULD NOT BE PUBLIC
+
+        This method should never have its visibility upgraded.
         """
-        pass
+        return self.ijCoordinate
+
+    def hashCode(self):
+        """(non-Javadoc)
+
+        @see java.lang.Object#hashCode()
+        """
+        prime = 31
+        result = 1
+        result = prime*result
+        if self.ijCoordinate:
+            result += self.ijCoordinate.hashCode()
+        return result
+
+    def equals(self, obj):
+        """(non-Javadoc)
+
+        @see java.lang.Object#equals(java.lang.Object)
+        """
+        if self is obj:
+            return True
+        if obj is None:
+            return False
+        if self.__class__ != obj.__class__:
+            return False
+        other = obj
+        if self.ijCoordinate is None:
+            if other.ijCoordinate is not None:
+                return False
+        elif not self.ijCoordinate.equals(other.ijCoordinate):
+            return False
+        return True
+
+        def toString(self):
+            raise Exception
+
--- emmpy/crucible/core/math/coords/tests/test_AbstractVectorIJ.py
+++ emmpy/crucible/core/math/coords/tests/test_AbstractVectorIJ.py
@@ -8,7 +8,37 @@ from emmpy.crucible.core.math.coords.abstractvectorij import (
 class TestAbstractVectorIJ(unittest.TestCase):
 
     def test___init__(self):
-        AbstractVectorIJ()
+        avij = AbstractVectorIJ(0.0, 0.1)
+        self.assertIsNotNone(avij)
+
+    def test_getI(self):
+        avij = AbstractVectorIJ(0.0, 0.1)
+        self.assertAlmostEqual(avij.getI(), 0.0)
+
+    def test_getJ(self):
+        avij = AbstractVectorIJ(0.0, 0.1)
+        self.assertAlmostEqual(avij.getJ(), 0.1)
+
+    def test_getVectorIJ(self):
+        avij = AbstractVectorIJ(0.0, 0.1)
+        uvij = avij.getVectorIJ()
+        self.assertAlmostEqual(uvij.i, 0.0)
+        self.assertAlmostEqual(uvij.j, 0.1)
+
+    def test_hashCode(self):
+        avij = AbstractVectorIJ(0.0, 0.1)
+        self.assertEqual(avij.hashCode(), 1036832941)
+
+    def test_equals(self):
+        avij1 = AbstractVectorIJ(0.0, 0.1)
+        avij2 = AbstractVectorIJ(0.0, 0.1)
+        avij3 = AbstractVectorIJ(0.0, 0.2)
+        self.assertTrue(avij1.equals(avij2))
+        self.assertFalse(avij1.equals(avij3))
+
+    def test_toString(self):
+        with self.assertRaises(Exception):
+            AbstractVectorIJ.toString(None)
 
 
 if __name__ == '__main__':

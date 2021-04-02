d3c84729ff1ce330f74bf7214ea4f4c19c484ce4
Eric Winter <eric.winter@jhuapl.edu>
2021-04-02 12:20:00 -0400

    First complete version of SphericalVector.

--- emmpy/crucible/core/math/coords/sphericalvector.py
+++ emmpy/crucible/core/math/coords/sphericalvector.py
@@ -1,10 +1,36 @@
-"""DESCRIPTION
-"""
+"""emmpy.crucible.core.math.coords.sphericalvector"""
 
 
-class SphericalVector():
+from emmpy.crucible.core.math.coords.abstractvector import AbstractVector
 
-    def __init__(self):
-        """DESCRIPTION
-        """
-        pass
+
+class SphericalVector(AbstractVector):
+    """A class representing a vector in the spherical coordinate system.
+
+    @author G.K.Stephens
+    """
+
+    # The ZERO vector.
+    # ZERO = SphericalVector(0, 0, 0)
+
+    def __init__(self, radius, colatInRadians, longInRadians):
+        """Constructor"""
+        AbstractVector.__init__(self, radius, colatInRadians, longInRadians)
+
+    def getRadius(self):
+        """@return the radius"""
+        return AbstractVector.getI(self)
+
+    def getColatitude(self):
+        """@return the colatitude"""
+        return AbstractVector.getJ(self)
+
+    def getLongitude(self):
+        """@return the longitude"""
+        return AbstractVector.getK(self)
+
+    def toString(self):
+        return (
+            "SphericalVector [radius: %s, colatitude: %s, longitude: %s]" %
+            (self.getRadius(), self.getColatitude(), self.getLongitude())
+        )
--- emmpy/crucible/core/math/coords/tests/test_SphericalVector.py
+++ emmpy/crucible/core/math/coords/tests/test_SphericalVector.py
@@ -6,7 +6,27 @@ from emmpy.crucible.core.math.coords.sphericalvector import SphericalVector
 class TestSphericalVector(unittest.TestCase):
 
     def test___init__(self):
-        SphericalVector()
+        sv = SphericalVector(0.1, 0.2, 0.3)
+        self.assertIsNotNone(sv)
+
+    def test_getRadius(self):
+        sv = SphericalVector(0.1, 0.2, 0.3)
+        self.assertAlmostEqual(sv.getRadius(), 0.1)
+
+    def test_getColatitude(self):
+        sv = SphericalVector(0.1, 0.2, 0.3)
+        self.assertAlmostEqual(sv.getColatitude(), 0.2)
+
+    def test_getLongitude(self):
+        sv = SphericalVector(0.1, 0.2, 0.3)
+        self.assertAlmostEqual(sv.getLongitude(), 0.3)
+
+    def test_toString(self):
+        sv = SphericalVector(0.1, 0.2, 0.3)
+        self.assertEqual(
+            sv.toString(),
+            "SphericalVector [radius: 0.1, colatitude: 0.2, longitude: 0.3]"
+        )
 
 
 if __name__ == '__main__':

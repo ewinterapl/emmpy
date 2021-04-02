e266ac38cd85a56774f2728806921d002db9d937
Eric Winter <eric.winter@jhuapl.edu>
2021-04-02 12:08:46 -0400

    First complete version of RaDecVector.

--- emmpy/crucible/core/math/coords/radecvector.py
+++ emmpy/crucible/core/math/coords/radecvector.py
@@ -1,10 +1,36 @@
-"""DESCRIPTION
-"""
+"""emmpy.package crucible.core.math.coords.radecvector"""
 
 
-class RaDecVector():
+from emmpy.crucible.core.math.coords.abstractvector import AbstractVector
 
-    def __init__(self):
-        """DESCRIPTION
-        """
-        pass
+
+class RaDecVector(AbstractVector):
+    """A class representing a vector in the Celestial Coordinate system.
+
+    @author G.K.Stephens
+    """
+
+    # The ZERO vector.
+    # ZERO = RaDecVector(0, 0, 0)
+
+    def __init__(self, radius, raRadians, decRadians):
+        """Constructor"""
+        AbstractVector.__init__(self, radius, raRadians, decRadians)
+
+    def getRadius(self):
+        """@return the radius"""
+        return AbstractVector.getI(self)
+
+    def getRightAscension(self):
+        """@return the right ascension"""
+        return AbstractVector.getJ(self)
+
+    def getDeclination(self):
+        """@return the declination"""
+        return AbstractVector.getK(self)
+
+    def toString(self):
+        return (
+            "RaDecVector [radius: %s, rightAscension: %s, declination: %s]" %
+            (self.getRadius(), self.getRightAscension(), self.getDeclination())
+        )
--- emmpy/crucible/core/math/coords/tests/test_RaDecVector.py
+++ emmpy/crucible/core/math/coords/tests/test_RaDecVector.py
@@ -3,10 +3,30 @@ import unittest
 from emmpy.crucible.core.math.coords.radecvector import RaDecVector
 
 
-class TestRaDecVector(unittest.TestCase):
+class TestBuilder(unittest.TestCase):
 
     def test___init__(self):
-        RaDecVector()
+        rdv = RaDecVector(0.1, 0.2, 0.3)
+        self.assertIsNotNone(rdv)
+
+    def test_getRadius(self):
+        rdv = RaDecVector(0.1, 0.2, 0.3)
+        self.assertAlmostEqual(rdv.getRadius(), 0.1)
+
+    def test_getRightAscension(self):
+        rdv = RaDecVector(0.1, 0.2, 0.3)
+        self.assertAlmostEqual(rdv.getRightAscension(), 0.2)
+
+    def test_getDeclination(self):
+        rdv = RaDecVector(0.1, 0.2, 0.3)
+        self.assertAlmostEqual(rdv.getDeclination(), 0.3)
+
+    def test_toString(self):
+        rdv = RaDecVector(0.1, 0.2, 0.3)
+        self.assertEqual(
+            rdv.toString(),
+            "RaDecVector [radius: 0.1, rightAscension: 0.2, declination: 0.3]"
+        )
 
 
 if __name__ == '__main__':

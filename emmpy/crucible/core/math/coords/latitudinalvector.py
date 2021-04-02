357991ddad695dfcc0b78ac108198dc88942ac51
Eric Winter <eric.winter@jhuapl.edu>
2021-04-02 10:45:16 -0400

    First complete version of LatitudinalVector.

--- emmpy/crucible/core/math/coords/latitudinalvector.py
+++ emmpy/crucible/core/math/coords/latitudinalvector.py
@@ -1,10 +1,38 @@
-"""DESCRIPTION
-"""
+"""emmpy.crucible.core.math.coords.latitudinalvector"""
 
 
-class LatitudinalVetor():
+from emmpy.crucible.core.math.coords.abstractvector import AbstractVector
 
-    def __init__(self):
-        """DESCRIPTION
-        """
-        pass
+
+class LatitudinalVector(AbstractVector):
+    """A class representing a vector in the latitudinal coordinate system.
+
+    @author G.K.Stephens
+    """
+    # public final class LatitudinalVector extends AbstractVector {
+
+    # The ZERO vector.
+    # ZERO = LatitudinalVector(0, 0, 0)
+
+    def __init__(self, *args):
+        """Constructor"""
+        (radius, latInRadians, longInRadians) = args
+        AbstractVector.__init__(self, radius, latInRadians, longInRadians)
+
+    def getRadius(self):
+        """@return the radius"""
+        return AbstractVector.getI(self)
+
+    def getLatitude(self):
+        """@return the latitude"""
+        return AbstractVector.getJ(self)
+
+    def getLongitude(self):
+        """@return the longitude"""
+        return AbstractVector.getK(self)
+
+    def toString(self):
+        return (
+            "LatitudinalVector [radius: %s, latitude: %s, longitude: %s]" %
+            (self.getRadius(), self.getLatitude(), self.getLongitude())
+        )
--- emmpy/crucible/core/math/coords/tests/test_LatitudinalVector.py
+++ emmpy/crucible/core/math/coords/tests/test_LatitudinalVector.py
@@ -1,14 +1,34 @@
 import unittest
 
 from emmpy.crucible.core.math.coords.latitudinalvector import (
-    LatitudinalVetor
+    LatitudinalVector
 )
 
 
-class TestLatitudinalVetor(unittest.TestCase):
+class TestBuilder(unittest.TestCase):
 
     def test___init__(self):
-        LatitudinalVetor()
+        lv = LatitudinalVector(0.0, 0.1, 0.2)
+        self.assertIsNotNone(lv)
+
+    def test_getRadius(self):
+        lv = LatitudinalVector(0.0, 0.1, 0.2)
+        self.assertAlmostEqual(lv.getRadius(), 0.0)
+
+    def test_getLatitude(self):
+        lv = LatitudinalVector(0.0, 0.1, 0.2)
+        self.assertAlmostEqual(lv.getLatitude(), 0.1)
+
+    def test_getLongitude(self):
+        lv = LatitudinalVector(0.0, 0.1, 0.2)
+        self.assertAlmostEqual(lv.getLongitude(), 0.2)
+
+    def test_toString(self):
+        lv = LatitudinalVector(0.0, 0.1, 0.2)
+        self.assertEqual(
+            lv.toString(),
+            "LatitudinalVector [radius: 0.0, latitude: 0.1, longitude: 0.2]"
+        )
 
 
 if __name__ == '__main__':

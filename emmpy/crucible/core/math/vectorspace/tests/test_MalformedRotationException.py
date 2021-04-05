41c8cba097e8293379d7368463453377b74710bf
Eric Winter <eric.winter@jhuapl.edu>
2021-04-05 09:39:00 -0400

    Renamed test class.

--- emmpy/crucible/core/math/vectorspace/tests/test_MalformedRotationException.py
+++ emmpy/crucible/core/math/vectorspace/tests/test_MalformedRotationException.py
@@ -5,7 +5,7 @@ from emmpy.crucible.core.math.vectorspace.malformedrotationexception import (
 )
 
 
-class TestMalformedRotationException(unittest.TestCase):
+class TestBuilder(unittest.TestCase):
 
     def test___init__(self):
         MalformedRotationException()

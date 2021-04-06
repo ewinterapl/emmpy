a1f44e3a69794c4c143d714f3b65660665b7d708
Eric Winter <eric.winter@jhuapl.edu>
2021-04-06 18:37:21 -0400

    Linting

--- emmpy/crucible/core/math/coords/coordconverter.py
+++ emmpy/crucible/core/math/coords/coordconverter.py
@@ -8,7 +8,7 @@ methods will raise exceptions if invoked.
 class CoordConverter:
     """An interface which allows for the conversion between different
     coordinate systems to Cartesian.
-    
+
     The four methods allow you to convert positions and states from Cartesian
     to the coordinate system and back. It is templated on the Unwritable and
     Writable version of the coordinate class.
@@ -28,8 +28,10 @@ class CoordConverter:
 
         Converts a Cartesian position to another coordinate system position.
 
-        @param cartesian A {@link UnwritableVectorIJK} holding the Cartesian position.
-        @param coordinateBuffer A coordinate buffer holding the position in this coordinate system.
+        @param cartesian A {@link UnwritableVectorIJK} holding the Cartesian
+        position.
+        @param coordinateBuffer A coordinate buffer holding the position in
+        this coordinate system.
         @return a reference to buffer for convenience.
         """
         raise Exception
@@ -39,8 +41,10 @@ class CoordConverter:
 
         Converts a coordinate system position to a Cartesian position.
 
-        @param coordinate A coordinate holding the position in this coordinate system.
-        @param cartesianBuffer A {@link UnwritableVectorIJK} buffer holding the Cartesian position.
+        @param coordinate A coordinate holding the position in this coordinate
+        system.
+        @param cartesianBuffer A {@link UnwritableVectorIJK} buffer holding the
+        Cartesian position.
         @return a reference to buffer for convenience.
         """
         raise Exception

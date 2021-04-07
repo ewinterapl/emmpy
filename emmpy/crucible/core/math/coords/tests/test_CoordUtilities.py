e880d84a74a2c875b7a1a08e2b0eb247def7aa0e
Eric Winter <eric.winter@jhuapl.edu>
2021-04-07 10:15:11 -0400

    First complete version of CoordUtilities (removed class).

--- emmpy/crucible/core/math/coords/tests/test_CoordUtilities.py
+++ emmpy/crucible/core/math/coords/tests/test_CoordUtilities.py
@@ -1,12 +1,31 @@
+from math import pi
 import unittest
 
-from emmpy.crucible.core.math.coords.coordutilities import CoordUtilities
+from emmpy.crucible.core.math.coords.coordutilities import (
+    toLatitude,
+    toColatitude
+)
 
 
-class TestCoordUtilities(unittest.TestCase):
+class TestBuilder(unittest.TestCase):
 
-    def test___init__(self):
-        CoordUtilities()
+    def test_toLatitude(self):
+        self.assertAlmostEqual(toLatitude(-1), pi/2 + 1)
+        self.assertAlmostEqual(toLatitude(0), pi/2)
+        self.assertAlmostEqual(toLatitude(pi/4), pi/4)
+        self.assertAlmostEqual(toLatitude(pi/3), pi/6)
+        self.assertAlmostEqual(toLatitude(1), pi/2 - 1)
+        self.assertAlmostEqual(toLatitude(pi/2), 0)
+        self.assertAlmostEqual(toLatitude(3), pi/2 - 3)
+
+    def test_toColatitude(self):
+        self.assertAlmostEqual(toColatitude(-1), pi/2 + 1)
+        self.assertAlmostEqual(toColatitude(0), pi/2)
+        self.assertAlmostEqual(toColatitude(pi/4), pi/4)
+        self.assertAlmostEqual(toColatitude(pi/3), pi/6)
+        self.assertAlmostEqual(toColatitude(1), pi/2 - 1)
+        self.assertAlmostEqual(toColatitude(pi/2), 0)
+        self.assertAlmostEqual(toColatitude(3), pi/2 - 3)
 
 
 if __name__ == '__main__':

3dc76fa88e636c90b8b8d024aa9c2d930b2f116d
Eric Winter <eric.winter@jhuapl.edu>
2021-04-13 11:04:56 -0400

    First complete version of LatitudinalToCartesianJacobian.

--- emmpy/crucible/core/math/coords/latitudinaltocartesianjacobian.py
+++ emmpy/crucible/core/math/coords/latitudinaltocartesianjacobian.py
@@ -1,10 +1,73 @@
-"""DESCRIPTION
-"""
+"""emmpy.crucible.core.math.coords.latitudinaltocartesianjacobian"""
 
 
-class LatitudinalToCartesianJacobian():
+from math import cos, sin
+
+from emmpy.crucible.core.math.coords.latitudinalvector import LatitudinalVector
+from emmpy.crucible.core.math.coords.pointonaxisexception import (
+    PointOnAxisException
+)
+from emmpy.crucible.core.math.coords.transformation import Transformation
+from emmpy.crucible.core.math.vectorspace.matrixijk import MatrixIJK
+from emmpy.java.lang.unsupportedoperationexception import (
+    UnsupportedOperationException
+)
+
+
+class LatitudinalToCartesianJacobian(Transformation):
+    """LatitudinalToCartesianJacobian"""
 
     def __init__(self):
-        """DESCRIPTION
-        """
+        """Constructor"""
         pass
+
+    def getTransformation(self, coordPosition, buffer):
+        """from SPICE's routine in drdlat.f:
+
+        JACOBI (DX,DR) = DCOS( LONG ) * DCOS( LAT )
+        JACOBI (DY,DR) = DSIN( LONG ) * DCOS( LAT )
+        JACOBI (DZ,DR) = DSIN( LAT )
+        JACOBI (DX,DLON) = -R * DSIN( LONG ) * DCOS( LAT )
+        JACOBI (DY,DLON) = R * DCOS( LONG ) * DCOS( LAT )
+        JACOBI (DZ,DLON) = 0.0D0
+        JACOBI (DX,DLAT) = -R * DCOS( LONG ) * DSIN( LAT )
+        JACOBI (DY,DLAT) = -R * DSIN( LONG ) * DSIN( LAT )
+        JACOBI (DZ,DLAT) = R * DCOS( LAT )
+        """
+        r = coordPosition.getRadius()
+        lat = coordPosition.getLatitude()
+        lon = coordPosition.getLongitude()
+        cosLat = cos(lat)
+        sinLat = sin(lat)
+        cosLong = cos(lon)
+        sinLong = sin(lon)
+        xByR = cosLong*cosLat
+        yByR = sinLong*cosLat
+        zByR = sinLat
+        xByLat = -r*cosLong*sinLat
+        yByLat = -r*sinLong*sinLat
+        zByLat = r*cosLat
+        xByLong = -r*sinLong*cosLat
+        yByLong = r*cosLong*cosLat
+        zByLong = 0
+        return buffer.setTo(
+            xByR, yByR, zByR,
+            xByLat, yByLat, zByLat,
+            xByLong, yByLong, zByLong
+        )
+
+    def getInverseTransformation(self, coordPosition, buffer):
+        try:
+            return self.getTransformation(coordPosition, buffer).invort()
+        except UnsupportedOperationException as e:
+            raise PointOnAxisException(e)
+
+    def mxv(self, *args):
+        if isinstance(args[1], LatitudinalVector):
+            (jacobian, coordVelocity) = args
+            return MatrixIJK.mxv(jacobian, coordVelocity.getVectorIJK())
+        else:
+            print("case 2")
+            (inverseJacobian, cartVelocity) = args
+            vect = MatrixIJK.mxv(inverseJacobian, cartVelocity)
+            return LatitudinalVector(vect.getI(), vect.getJ(), vect.getK())
--- emmpy/crucible/core/math/coords/tests/test_LatitudinalToCartesianJacobian.py
+++ emmpy/crucible/core/math/coords/tests/test_LatitudinalToCartesianJacobian.py
@@ -3,12 +3,66 @@ import unittest
 from emmpy.crucible.core.math.coords.latitudinaltocartesianjacobian import (
     LatitudinalToCartesianJacobian
 )
+from emmpy.crucible.core.math.coords.latitudinalvector import LatitudinalVector
+from emmpy.crucible.core.math.vectorspace.matrixijk import MatrixIJK
 
 
-class TestLatitudinalToCartesianJacobian(unittest.TestCase):
+class TestBuilder(unittest.TestCase):
 
     def test___init__(self):
-        LatitudinalToCartesianJacobian()
+        l2cj = LatitudinalToCartesianJacobian()
+        self.assertIsNotNone(l2cj)
+
+    def test_getTransformation(self):
+        l2cj = LatitudinalToCartesianJacobian()
+        coordPosition = LatitudinalVector(1, 2, 3)
+        buffer = MatrixIJK()
+        b = l2cj.getTransformation(coordPosition, buffer)
+        self.assertIs(b, buffer)
+        self.assertAlmostEqual(b.getII(), 0.411982245665683)
+        self.assertAlmostEqual(b.getJI(), -0.05872664492762098)
+        self.assertAlmostEqual(b.getKI(), 0.9092974268256817)
+        self.assertAlmostEqual(b.getIJ(), 0.9001976297355174)
+        self.assertAlmostEqual(b.getJJ(), -0.12832006020245673)
+        self.assertAlmostEqual(b.getKJ(), -0.4161468365471424)
+        self.assertAlmostEqual(b.getIK(), 0.05872664492762098)
+        self.assertAlmostEqual(b.getJK(), 0.411982245665683)
+        self.assertAlmostEqual(b.getKK(), 0)
+
+    def test_getInverseTransformation(self):
+        l2cj = LatitudinalToCartesianJacobian()
+        coordPosition = LatitudinalVector(1, 2, 3)
+        buffer = MatrixIJK()
+        b = l2cj.getInverseTransformation(coordPosition, buffer)
+        self.assertIs(b, buffer)
+        self.assertAlmostEqual(b.getII(), 0.411982245665683)
+        self.assertAlmostEqual(b.getJI(), 0.9001976297355174)
+        self.assertAlmostEqual(b.getKI(), 0.339111091726107)
+        self.assertAlmostEqual(b.getIJ(), -0.05872664492762098)
+        self.assertAlmostEqual(b.getJJ(), -0.12832006020245673)
+        self.assertAlmostEqual(b.getKJ(), 2.378949951451322)
+        self.assertAlmostEqual(b.getIK(), 0.9092974268256817)
+        self.assertAlmostEqual(b.getJK(), -0.4161468365471424)
+        self.assertAlmostEqual(b.getKK(), 0)
+
+    def test_mxv(self):
+        l2cj = LatitudinalToCartesianJacobian()
+        r = 1.0
+        lat = 2.0
+        lon = 3.0
+        lv = LatitudinalVector(r, lat, lon)
+        mijk = MatrixIJK()
+        l2cj.getTransformation(lv, mijk)
+        latitudinalVelocity = LatitudinalVector(1, 1, 1)
+        cartesianVelocity = l2cj.mxv(mijk, latitudinalVelocity)
+        self.assertAlmostEqual(cartesianVelocity.getI(), 1.3709065203288213)
+        self.assertAlmostEqual(cartesianVelocity.getJ(), 0.22493554053560527)
+        self.assertAlmostEqual(cartesianVelocity.getK(), 0.4931505902785393)
+        l2cj.getInverseTransformation(lv, mijk)
+        latitudinalalVelocity = l2cj.mxv(mijk, cartesianVelocity)
+        self.assertAlmostEqual(latitudinalalVelocity.getI(), 1)
+        self.assertAlmostEqual(latitudinalalVelocity.getJ(), 1)
+        self.assertAlmostEqual(latitudinalalVelocity.getK(), 1)
 
 
 if __name__ == '__main__':

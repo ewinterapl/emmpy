a057544a0d8331d7039ccacd564ead52d31e732f
Eric Winter <eric.winter@jhuapl.edu>
2021-04-14 13:35:58 -0400

    Updated docstrings.

--- emmpy/crucible/crust/vectorfieldsij/scalarfieldspatialderivative.py
+++ emmpy/crucible/crust/vectorfieldsij/scalarfieldspatialderivative.py
@@ -1,17 +1,21 @@
-"""emmpy.crucible.crust.vectorfieldsij.scalarfieldspatialderivative"""
+"""emmpy.crucible.crust.vectorfieldsij.scalarfieldspatialderivative
+
+N.B. This class was created from a Java interface, and therefore most of these
+methods will raise exceptions if invoked.
+"""
 
 
 class ScalarFieldIJSpatialDerivative:
     """ScalarFieldIJSpatialDerivative"""
 
     def __init__(self, location):
-        """Interface - do not instantiate."""
+        """INTERFACE - DO NOT INSTANTIATE"""
         raise Exception
 
     def differentiateFDi(self, location):
-        # Interface - do not invoke.
+        """INTERFACE - DO NOT INVOKE"""
         raise Exception
 
     def differentiateFDj(self, location):
-        # Interface - do not invoke.
+        """INTERFACE - DO NOT INVOKE"""
         raise Exception

41117acbcea316c0f55512e8a844696b7c0e95f6
Eric Winter <eric.winter@jhuapl.edu>
2021-04-14 13:32:14 -0400

    Updated docstrings.

--- emmpy/crucible/crust/vectorfieldsij/differentiablescalarfieldij.py
+++ emmpy/crucible/crust/vectorfieldsij/differentiablescalarfieldij.py
@@ -1,4 +1,9 @@
-"""emmpy.crucible.crust.vectorfieldsij.differentiablescalarfieldij"""
+"""emmpy.crucible.crust.vectorfieldsij.differentiablescalarfieldij
+
+N.B. This class was created from a Java interface, and therefore most of these
+methods will raise exceptions if invoked.
+"""
+
 
 from emmpy.crucible.core.math.vectorfields.scalarfield2d import ScalarField2D
 from emmpy.crucible.crust.vectorfieldsij.scalarfieldspatialderivative import (
@@ -15,5 +20,5 @@ class DifferentiableScalarFieldIJ(ScalarField2D, ScalarFieldIJSpatialDerivative)
     """
 
     def __init__(self):
-        """Interface - do not instantiate."""
+        """INTERFACE - DO NOT INSTANTIATE"""
         raise Exception

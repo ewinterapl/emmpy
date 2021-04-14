52bf7eb4bb153dc00304a66b3a7e242e9f54971a
Eric Winter <eric.winter@jhuapl.edu>
2021-04-14 13:51:21 -0400

    Updated docstrings.

--- emmpy/magmodel/core/math/bessel/besselfunctionevaluator.py
+++ emmpy/magmodel/core/math/bessel/besselfunctionevaluator.py
@@ -1,16 +1,21 @@
 """emmpy.magmodel.core.math.bessel.besselfunctionevaluator
 
+N.B. This class was created from a Java interface, and therefore most of these
+methods will raise exceptions if invoked.
+
 This interface is for evaluating commonly used forms of Bessel functions of
 the first kind.
 
 @author G.K.Stephens
 """
 
+
 class BesselFunctionEvaluator:
 
     def besselj0(self, x):
-        """Evaluates the Bessel function of the first kind to the order of
-        n = 0.
+        """INTERFACE - DO NOT INSTANTIATE
+
+        Evaluates the Bessel function of the first kind to the order of n = 0.
 
         @param n the order of the Bessel function.
         @param x the value to compute the Bessel function of.
@@ -18,8 +23,9 @@ class BesselFunctionEvaluator:
         raise Exception
 
     def besselj1(self, x):
-        """Evaluates the Bessel function of the first kind to the order of
-        n = 1.
+        """INTERFACE - DO NOT INVOKE
+
+        Evaluates the Bessel function of the first kind to the order of n = 1.
 
         @param n the order of the Bessel function.
         @param x the value to compute the Bessel function of.
@@ -27,7 +33,9 @@ class BesselFunctionEvaluator:
         raise Exception
 
     def besseljn(self, n, x):
-        """Evaluates the Bessel function of the first kind to the order n.
+        """INTERFACE - DO NOT INVOKE
+
+        Evaluates the Bessel function of the first kind to the order n.
 
         @param n the order of the Bessel function.
         @param x the value to compute the Bessel function of.
@@ -35,7 +43,9 @@ class BesselFunctionEvaluator:
         raise Exception
 
     def besselj0jn(self, n, x):
-        """Evaluates the Bessel function of the first kind from order 0 to
+        """INTERFACE - DO NOT INVOKE
+
+        Evaluates the Bessel function of the first kind from order 0 to
         order n.
 
         @param n the order of the Bessel function.

9934f234a78dee089603e2b4ebabc11d61c43187
Eric Winter <eric.winter@jhuapl.edu>
2021-04-14 13:55:29 -0400

    Updated docstrings.

--- emmpy/magmodel/core/math/bessel/albertbesselfunctionevaluator.py
+++ emmpy/magmodel/core/math/bessel/albertbesselfunctionevaluator.py
@@ -1,49 +1,9 @@
-""" Template for a Python module implementing a port of a Java class.
-
-From PEP 257:
-https://www.python.org/dev/peps/pep-0257/#multi-line-docstrings
-
-The docstring for a module should generally list the classes, exceptions and
-functions (and any other objects) that are exported by the module, with a
-one-line summary of each. (These summaries generally give less detail than
-the summary line in the object's docstring.) The docstring for a package
-(i.e., the docstring of the package's __init__.py module) should also list
-the modules and subpackages exported by the package.
-
-Classes
--------
-ClassTemplate
-    Template for the Python version of a Java class
-"""
+"""emmpy.magmodel.core.math.bessel.albertbesselfunctionevaluator"""
 
 
 class AlbertBesselFunctionEvaluator:
-
-    """AlbertBesselFunctionEvaluator
-
-    A Python class template ...
-    
-    Based on the original Java class:
-    magmodel.core.math.bessel.AlbertBesselFunctionEvaluator
-    """
+    """AlbertBesselFunctionEvaluator"""
 
     def __init__(self):
-        """Initialize a new AlbertBesselFunctionEvaluator object.
-
-        The docstring for a function or method should summarize its behavior
-        and document its arguments, return value(s), side effects, exceptions
-        raised, and restrictions on when it can be called (all if applicable).
-        Optional arguments should be indicated. It should be documented whether
-        keyword arguments are part of the interface.
-
-        Parameters
-        ----------
-        self : AlbertBesselFunctionEvaluator
-            New object to initialize.
-        
-        Returns
-        -------
-        self : AlbertBesselFunctionEvaluator
-            New object after initialization.
-        """
+        """Initialize a new AlbertBesselFunctionEvaluator object."""
         pass
--- emmpy/magmodel/core/math/bessel/tests/test_albertbesselfunctionevaluator.py
+++ emmpy/magmodel/core/math/bessel/tests/test_albertbesselfunctionevaluator.py
@@ -1,12 +1,15 @@
 import unittest
 
-from emmpy.magmodel.core.math.bessel.albertbesselfunctionevaluator import AlbertBesselFunctionEvaluator
+from emmpy.magmodel.core.math.bessel.albertbesselfunctionevaluator import (
+    AlbertBesselFunctionEvaluator
+)
 
-class TestAlbertBesselFunctionEvaluator(unittest.TestCase):
+
+class TestBuilder(unittest.TestCase):
 
     def test___init__(self):
         AlbertBesselFunctionEvaluator()
-    
+
 
 if __name__ == '__main__':
     unittest.main()

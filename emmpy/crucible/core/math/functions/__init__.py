e98daf0796a312d788b152c4a7571c3dfd8a26af
Eric Winter <eric.winter@jhuapl.edu>
2021-04-20 13:43:49 -0400

    Stubs for UnivariateFunction.

--- emmpy/crucible/core/math/functions/__init__.py
+++ emmpy/crucible/core/math/functions/__init__.py
@@ -4,4 +4,5 @@ Python port of Java classes from the crucible.core.math.functions hierarchy.
 
 Modules
 -------
+univariatefunction.py
 """
---
+++ emmpy/crucible/core/math/functions/tests/__init__.py
@@ -0,0 +1,9 @@
+"""empy.crucible.core.math.vectorspace.tests
+
+Tests for Python port of Java classes from the crucible.core.math.functions
+hierarchy.
+
+Modules
+-------
+test_UnivariateFunction.py
+"""

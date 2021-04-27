459d27c203a5407a14300fd6f0ef00b602ea3956
Eric Winter <eric.winter@jhuapl.edu>
2021-04-27 10:52:35 -0400

    First complete version of DefaultFacConfigurationOptions. Tests are stubs for now.

---
+++ emmpy/geomagmodel/ts07/coefficientreader/tests/test_DefaultFacConfigurationOptions.py
@@ -0,0 +1,44 @@
+import unittest
+
+from emmpy.geomagmodel.ts07.coefficientreader.defaultfacconfigurationoptions import (
+    DefaultFacConfigurationOptions
+)
+
+
+class TestBuilder(unittest.TestCase):
+
+    def test___init__(self):
+        dfco = DefaultFacConfigurationOptions(
+            DefaultFacConfigurationOptions.TS07D
+        )
+        self.assertIsNotNone(dfco)
+        self.assertEqual(
+            dfco.numberOfFields, DefaultFacConfigurationOptions.TS07D
+        )
+
+    def test_createFromCoeffs(self):
+        pass
+
+    def test_getNumberOfFields(self):
+        dfco = DefaultFacConfigurationOptions(
+            DefaultFacConfigurationOptions.TS07D
+        )
+        self.assertEqual(
+            dfco.getNumberOfFields(), DefaultFacConfigurationOptions.TS07D
+        )
+
+    def test_getTs07(self):
+        pass
+
+    def test_get6Fac(self):
+        pass
+
+    def test_get12Fac(self):
+        pass
+
+    def test_get16Fac(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

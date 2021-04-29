48381cb64192bb96d149e4d52cd122ceefba3484
Eric Winter <eric.winter@jhuapl.edu>
2021-04-29 10:04:46 -0400

    First complete version of UTCEpoch.

--- emmpy/crucible/core/time/__init__.py
+++ emmpy/crucible/core/time/__init__.py
@@ -4,4 +4,9 @@ Python port of Java classes from the crucible.core.math.time hierarchy.
 
 Modules
 -------
+utcepoch.py
+
+Packages
+--------
+tests
 """
---
+++ emmpy/crucible/core/time/tests/__init__.py
@@ -0,0 +1,10 @@
+"""empy.crucible.core.time.tests
+
+Tests for Python port of Java classes from the
+crucible.core.time
+hierarchy.
+
+Modules
+-------
+test_UTCEpoch.py
+"""

0a5c0ce8625d4d014f5d0a4fc63d2ea90d46dfb8
Eric Winter <eric.winter@jhuapl.edu>
2021-05-05 08:41:02 -0400

    Placeholders

--- emmpy/crucible/core/rotations/tests/test_AxisAndAngle.py
+++ emmpy/crucible/core/rotations/tests/test_AxisAndAngle.py
@@ -1,12 +1,16 @@
 import unittest
 
+from emmpy.crucible.core.math.vectorspace.vectorijk import VectorIJK
 from emmpy.crucible.core.rotations.axisandangle import AxisAndAngle
 
 
 class TestBuilder(unittest.TestCase):
 
     def test___init__(self):
-        pass
+        aaa = AxisAndAngle()
+        self.assertIsNotNone(aaa)
+        self.assertTrue(aaa.axis.equals(VectorIJK.K))
+        # self.assertAlmostEqual(aaa.angle, 0.0)
 
 
 if __name__ == '__main__':

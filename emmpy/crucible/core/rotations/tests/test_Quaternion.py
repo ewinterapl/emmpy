cfda815820c84380905159618d1fb0fb2454c467
Eric Winter <eric.winter@jhuapl.edu>
2021-05-06 18:23:13 -0400

    First complete version of Quaternion. Tests are stubs for now.

--- emmpy/crucible/core/rotations/tests/test_Quaternion.py
+++ emmpy/crucible/core/rotations/tests/test_Quaternion.py
@@ -10,6 +10,45 @@ class TestBuilder(unittest.TestCase):
     def test___init__(self):
         pass
 
+    def test_multiply(self):
+        pass
+
+    def test_computeNorm(self):
+        pass
+
+    def test_getScalar(self):
+        pass
+
+    def test_getVector(self):
+        pass
+
+    def test_getRotationAxis(self):
+        pass
+
+    def test_getRotationAngle(self):
+        pass
+
+    def test_conjugate(self):
+        pass
+
+    def test_negate(self):
+        pass
+
+    def test_setTo(self):
+        pass
+
+    def test_getRotation(self):
+        pass
+
+    def test_hashCode(self):
+        pass
+
+    def test_equals(self):
+        pass
+
+    def test_toString(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()

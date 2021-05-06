8a59018d0db77598437be8433a1383d852e3d6ed
Eric Winter <eric.winter@jhuapl.edu>
2021-05-06 09:26:31 -0400

    Fixed typos in test module.

--- emmpy/crucible/core/rotations/tests/test_Quaternion.py
+++ emmpy/crucible/core/rotations/tests/test_Quaternion.py
@@ -1,19 +1,14 @@
 import unittest
 
-from emmpy.crucible.core.rotations.rotation import (
-    Rotation
+from emmpy.crucible.core.rotations.quaternion import (
+    Quaternion
 )
 
 
 class TestBuilder(unittest.TestCase):
 
     def test___init__(self):
-        with self.assertRaises(Exception):
-            Rotation()
-
-    def test_setTo(self):
-        with self.assertRaises(Exception):
-            Rotation.setTo(None, None)
+        pass
 
 
 if __name__ == '__main__':

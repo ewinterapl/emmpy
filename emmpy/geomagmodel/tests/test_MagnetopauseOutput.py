475895bb328d720bafb3489dc99424dceac7f408
Eric Winter <eric.winter@jhuapl.edu>
2021-05-08 15:24:26 -0400

    First complete version of MagnetopauseOutput.

--- emmpy/geomagmodel/tests/test_MagnetopauseOutput.py
+++ emmpy/geomagmodel/tests/test_MagnetopauseOutput.py
@@ -8,6 +8,21 @@ class TestBuilder(unittest.TestCase):
     def test___init__(self):
         pass
 
+    def test_getDistanceToMagnetopause(self):
+        pass
+
+    def test_isWithinMagnetosphere(self):
+        pass
+
+    def test_getMagnetopauseLocation(self):
+        pass
+
+    def test_toString(self):
+        pass
+
+    def test_hashCode(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()

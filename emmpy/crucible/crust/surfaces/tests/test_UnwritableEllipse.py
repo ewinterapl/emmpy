b3e4579b56fe3d208ee199f35e32ba49ae91d2ed
Eric Winter <eric.winter@jhuapl.edu>
2021-05-09 17:23:30 -0400

    First complete version of UnwritableEllipse. May need changed once EllipseType is completed.

--- emmpy/crucible/crust/surfaces/tests/test_UnwritableEllipse.py
+++ emmpy/crucible/crust/surfaces/tests/test_UnwritableEllipse.py
@@ -6,7 +6,63 @@ from emmpy.crucible.crust.surfaces.unwritableellipse import (
 
 
 class TestBuilder(unittest.TestCase):
-    pass
+
+    def test___init__(self):
+        pass
+
+    def test_copyOf(self):
+        pass
+
+    def test_getCenter(self):
+        pass
+
+    def test_getSemiMajorAxis(self):
+        pass
+
+    def test_getSemiMinorAxis(self):
+        pass
+
+    def test_getType(self):
+        pass
+
+    def test_isDegnerate(self):
+        pass
+
+    def test_isCircular(self):
+        pass
+
+    def test_isContainedWithin(self):
+        pass
+
+    def test_intersects(self):
+        pass
+
+    def test_getPlane(self):
+        pass
+
+    def test_setTo(self):
+        pass
+
+    def test_determineType(self):
+        pass
+
+    def test_rotate(self):
+        pass
+
+    def test_offset(self):
+        pass
+
+    def test_scale(self):
+        pass
+
+    def test_toString(self):
+        pass
+
+    def test_hashCode(self):
+        pass
+
+    def test_equals(self):
+        pass
 
 
 if __name__ == '__main__':

a0c7656465792b49e4e615dbf197d297221e3665
Eric Winter <eric.winter@jhuapl.edu>
2021-05-11 13:50:38 -0400

    First complete version of OffsetSurface. Tests are stubs for now.

--- emmpy/crucible/crust/surfaces/offsetsurface.py
+++ emmpy/crucible/crust/surfaces/offsetsurface.py
@@ -1,5 +1,38 @@
 """emmpy.crucible.crust.surfaces.offsetsurface"""
 
 
-class OffsetSurface:
-    pass
+from emmpy.com.google.common.base.preconditions import Preconditions
+from emmpy.crucible.core.math.vectorspace.unwritablevectorijk import (
+    UnwritableVectorIJK
+)
+from emmpy.crucible.core.math.vectorspace.vectorijk import VectorIJK
+from emmpy.crucible.crust.surfaces.surface import Surface
+
+
+class OffsetSurface(Surface):
+
+    def __init__(self, delegate, offset):
+        """Constructor"""
+        Surface.__init__(self)
+        self.delegate = Preconditions.checkNotNull(delegate)
+        # defensive copy
+        self.offset = UnwritableVectorIJK(
+            Preconditions.checkNotNull(offset)
+        )
+
+    def computeOutwardNormal(self, surfacePoint, buffer):
+        offsetSurfacePoint = VectorIJK.add(
+            surfacePoint, self.offset, VectorIJK()
+        )
+        return self.delegate.computeOutwardNormal(offsetSurfacePoint, buffer)
+
+    def intersects(self, source, ray):
+        offsetSource = VectorIJK.add(source, self.offset, VectorIJK())
+        return self.delegate.intersects(offsetSource, ray)
+
+    def compute(self, source, ray, buffer):
+        offsetSource = VectorIJK.add(source, self.offset, VectorIJK())
+        return VectorIJK.subtract(
+            self.delegate.compute(offsetSource, ray, buffer),
+            self.offset, buffer
+        )
--- emmpy/crucible/crust/surfaces/tests/test_OffsetSurface.py
+++ emmpy/crucible/crust/surfaces/tests/test_OffsetSurface.py
@@ -4,7 +4,18 @@ from emmpy.crucible.crust.surfaces.offsetsurface import OffsetSurface
 
 
 class TestBuilder(unittest.TestCase):
-    pass
+
+    def test___init__(self):
+        pass
+
+    def test_computeOutwardNormal(self):
+        pass
+
+    def test_intersects(self):
+        pass
+
+    def test_compute(self):
+        pass
 
 
 if __name__ == '__main__':

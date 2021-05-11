aabcc4da13634e900d1bb669a77e4416a15dc444
Eric Winter <eric.winter@jhuapl.edu>
2021-05-11 12:51:11 -0400

    First complete version of EllipsoidalSurfceNormalComputer.py. Tests are stubs for now.

--- emmpy/crucible/crust/surfaces/ellipsoidalsurfacenormalcomputer.py
+++ emmpy/crucible/crust/surfaces/ellipsoidalsurfacenormalcomputer.py
@@ -1,5 +1,26 @@
 """emmpy.crucible.crust.surfaces.ellipsoidalsurfacenormalcomputer"""
 
 
-class EllipsoidalSurfaceNormalComputer:
-    pass
+from emmpy.crucible.crust.surfaces.surfacenormalcomputer import (
+    SurfaceNormalComputer
+)
+
+
+class EllipsoidalSurfaceNormalComputer(SurfaceNormalComputer):
+
+    def __init__(self, a, b, c, minRadius):
+        self.a1 = minRadius/a
+        self.b1 = minRadius/b
+        self.c1 = minRadius/c
+
+    def computeOutwardNormal(self, surfacePoint, buffer):
+        """Compute (surfacePoint.getI()/a/a, surfacePoint.getJ()/b/b,
+        surfacePoint.getK()/c/c).
+
+        This could be numerically unwise due to overflow. Use minRadius as a
+        scaling.
+        """
+        buffer.setTo(surfacePoint.getI()*self.a1*self.a1,
+                     surfacePoint.getJ()*self.b1*self.b1,
+                     surfacePoint.getK()*self.c1*self.c1)
+        return buffer
--- emmpy/crucible/crust/surfaces/tests/test_EllipsoidalSurfaceNormalComputer.py
+++ emmpy/crucible/crust/surfaces/tests/test_EllipsoidalSurfaceNormalComputer.py
@@ -6,7 +6,12 @@ from emmpy.crucible.crust.surfaces.ellipsoidalsurfacenormalcomputer import (
 
 
 class TestBuilder(unittest.TestCase):
-    pass
+
+    def test___init__(self):
+        pass
+
+    def test_computeOutwardNormal(self):
+        pass
 
 
 if __name__ == '__main__':

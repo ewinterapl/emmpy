fbf4ebbf86dcd23fbea915a594b52a71e310f4d9
Eric Winter <eric.winter@jhuapl.edu>
2021-05-11 09:48:44 -0400

    First complete version of Ellipse. Type class moved to EllipseType.

--- emmpy/crucible/crust/surfaces/tests/test_Ellipse.py
+++ emmpy/crucible/crust/surfaces/tests/test_Ellipse.py
@@ -4,7 +4,21 @@ from emmpy.crucible.crust.surfaces.ellipse import Ellipse
 
 
 class TestBuilder(unittest.TestCase):
-    pass
+
+    def test___init__(self):
+        pass
+
+    def test_setTo(self):
+        pass
+
+    def test_rotate(self):
+        pass
+
+    def test_offset(self):
+        pass
+
+    def test_projectOnto(self):
+        pass
 
 
 if __name__ == '__main__':

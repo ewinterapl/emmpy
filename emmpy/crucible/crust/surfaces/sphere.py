7665c153bf1cbc0fcf682c73d290a2de2e011287
Eric Winter <eric.winter@jhuapl.edu>
2021-05-11 13:23:58 -0400

    Placeholder.

--- emmpy/crucible/crust/surfaces/sphere.py
+++ emmpy/crucible/crust/surfaces/sphere.py
@@ -1,8 +1,31 @@
 """emmpy.crucible.crust.surfaces.sphere"""
 
 
+# import crucible.core.math.vectorspace.UnwritableVectorIJK;
+# import crucible.core.math.vectorspace.VectorIJK;
+
+from emmpy.com.google.common.base.preconditions import Preconditions
 from emmpy.crucible.crust.surfaces.ellipsoid import Ellipsoid
 
 
 class Sphere(Ellipsoid):
-    pass
+
+    def __init__(self, radius):
+        """Constructor"""
+        Ellipsoid.__init__(self, radius, radius, radius)
+        Preconditions.checkArgument(
+            radius > 0,
+            "A sphere's radius must be strictly greater than zero."
+        )
+        self.normalComputer = SphericalSurfaceNormalComputer()
+
+    # public double getRadius() {
+    #     return getA();
+    # }
+
+    # @Override
+    # public VectorIJK computeOutwardNormal(UnwritableVectorIJK surfacePoint, VectorIJK buffer) {
+    #     return normalComputer.computeOutwardNormal(surfacePoint, buffer);
+    # }
+
+    # }

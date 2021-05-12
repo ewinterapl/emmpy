ec5e350ede189ee6620f79b1afc8666349d71963
Eric Winter <eric.winter@jhuapl.edu>
2021-05-12 14:05:01 -0400

    Added CoefficientExpansion1D stub.

--- emmpy/magmodel/core/math/expansions/coefficientexpansion1d.py
+++ emmpy/magmodel/core/math/expansions/coefficientexpansion1d.py
@@ -6,40 +6,41 @@ class CoefficientExpansion1D:
     i.e. doubles), that starts at a lower bound index (L) and ends at an upper
     bound index (U).
 
-    This is similar to double[] or {@link List} of {@link Double}s, but with a
-    non-zero starting index.
+    This is similar to double[] or List of Double, but with a non-zero starting
+    index.
 
-    @author G.K.Stephens
+    author G.K.Stephens
     """
+    pass
 
-    def toArray(self):
-        """@return a newly constructed double array containing the elements of
-        the expansion"""
-        size = self.size()
-        anArray = [None]*size
-        for i in range(size):
-            anArray[i] = self.getCoefficient(i + self.getLowerBoundIndex())
-        return anArray
+    # def toArray(self):
+    #     """@return a newly constructed double array containing the elements of
+    #     the expansion"""
+    #     size = self.size()
+    #     anArray = [None]*size
+    #     for i in range(size):
+    #         anArray[i] = self.getCoefficient(i + self.getLowerBoundIndex())
+    #     return anArray
 
-    def size(self):
-        """@return the number of elements of the expansion"""
-        size = self.getUpperBoundIndex() - self.getLowerBoundIndex() + 1
-        return size
+    # def size(self):
+    #     """@return the number of elements of the expansion"""
+    #     size = self.getUpperBoundIndex() - self.getLowerBoundIndex() + 1
+    #     return size
 
-    def getLowerBoundIndex(self):
-        """@return the lower bound index of the expansion (L)"""
-        raise Exception
+    # def getLowerBoundIndex(self):
+    #     """@return the lower bound index of the expansion (L)"""
+    #     raise Exception
 
-    def getUpperBoundIndex(self):
-        """@return the upper bound index of the expansion (U)"""
-        raise Exception
+    # def getUpperBoundIndex(self):
+    #     """@return the upper bound index of the expansion (U)"""
+    #     raise Exception
 
-    def getCoefficient(self, index):
-        """gets ith coefficient for the expansion T<sub>i</sub> in &#8721;
-        T<sub>i</sub>
+    # def getCoefficient(self, index):
+    #     """gets ith coefficient for the expansion T<sub>i</sub> in &#8721;
+    #     T<sub>i</sub>
 
-        @param index the index i
-        @return the ith coefficient for the expansion corresponding to the
-        index T<sub>i</sub>
-        """
-        raise Exception
+    #     @param index the index i
+    #     @return the ith coefficient for the expansion corresponding to the
+    #     index T<sub>i</sub>
+    #     """
+    #     raise Exception

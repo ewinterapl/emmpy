5ed3a5ebf7b67a56bd8ac940b61c25933912131d
Eric Winter <eric.winter@jhuapl.edu>
2021-05-12 17:06:25 -0400

    Reenabled evaluate().

--- emmpy/magmodel/core/modeling/equatorial/expansion/thinasymmetriccurrentsheetbasisvectorfield.py
+++ emmpy/magmodel/core/modeling/equatorial/expansion/thinasymmetriccurrentsheetbasisvectorfield.py
@@ -70,10 +70,6 @@ class ThinAsymmetricCurrentSheetBasisVectorField(BasisVectorField):
 
     def evaluate(self, location):
         buffer = VectorIJK()
-        # e = self.evaluateExpansions(location)
-        # sum = e.sum()
-        # buffer.setTo(sum)
-        # # ORIGINAL
         return buffer.setTo(self.evaluateExpansions(location).sum())
 
     # def evaluateExpansion(self, location):

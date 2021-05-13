4034ad50ad2f64dad0df342a77e3a58067d8a2ee
Eric Winter <eric.winter@jhuapl.edu>
2021-05-13 12:22:52 -0400

    createUnity() stub

--- emmpy/geomagmodel/ts07/modeling/equatorial/shieldedthincurrentsheetfield.py
+++ emmpy/geomagmodel/ts07/modeling/equatorial/shieldedthincurrentsheetfield.py
@@ -1,5 +1,146 @@
 """emmpy.geomagmodel.ts07.modeling.equatorial.shieldedthincurrentsheetfield"""
 
 
-class ShieldedThinCurrentSheetField:
-    pass
+# import com.google.common.collect.ImmutableList;
+# import crucible.core.math.vectorspace.UnwritableVectorIJK;
+# import crucible.crust.vectorfieldsij.DifferentiableScalarFieldIJ;
+# import geomagmodel.ts07.coefficientreader.ThinCurrentSheetShieldingCoefficients;
+# import magmodel.core.math.bessel.BesselFunctionEvaluator;
+# import magmodel.core.math.expansions.Expansion1D;
+# import magmodel.core.math.expansions.Expansion1Ds;
+# import magmodel.core.math.expansions.Expansion2D;
+# import magmodel.core.math.expansions.Expansion2Ds;
+# import magmodel.core.modeling.equatorial.expansion.TailSheetCoefficients;
+# import magmodel.core.modeling.equatorial.expansion.TailSheetExpansions;
+# import magmodel.core.modeling.equatorial.expansion.ThinAsymmetricCurrentSheetBasisVectorField;
+
+from emmpy.magmodel.core.math.vectorfields.basisvectorfield import (
+    BasisVectorField
+)
+
+
+class ShieldedThinCurrentSheetField(BasisVectorField):
+    """author G.K.Stephens"""
+
+    #   private final ThinAsymmetricCurrentSheetBasisVectorField thinCurrentSheet;
+    #   private final ThinAsymmetricCurrentSheetBasisVectorShieldingField thinCurrentSheetShield;
+    #   private final int numAzimuthalExpansions;
+    #   private final int numRadialExpansions;
+    #   private final boolean includeShield;
+
+    #   /**
+    #    * 
+    #    * @param thinCurrentSheet
+    #    * @param thinCurrentSheetShield
+    #    * @param includeShield
+    #    */
+    #   ShieldedThinCurrentSheetField(ThinAsymmetricCurrentSheetBasisVectorField thinCurrentSheet,
+    #       ThinAsymmetricCurrentSheetBasisVectorShieldingField thinCurrentSheetShield,
+    #       boolean includeShield) {
+    #     super();
+    #     this.thinCurrentSheet = thinCurrentSheet;
+    #     this.thinCurrentSheetShield = thinCurrentSheetShield;
+    #     this.includeShield = includeShield;
+    #     this.numAzimuthalExpansions = thinCurrentSheet.getNumAzimuthalExpansions();
+    #     this.numRadialExpansions = thinCurrentSheet.getNumRadialExpansions();
+    #   }
+
+    #   /**
+    #    * 
+    #    * @param tailCoeffs
+    #    * @param currentSheetHalfThickness
+    #    * @param tailLength
+    #    * @param bessel
+    #    * @param staticCoefficients
+    #    * @param includeShield
+    #    * @return
+    #    */
+    #   public static ShieldedThinCurrentSheetField create(TailSheetCoefficients tailCoeffs,
+    #       DifferentiableScalarFieldIJ currentSheetHalfThickness, double tailLength,
+    #       BesselFunctionEvaluator bessel, ThinCurrentSheetShieldingCoefficients staticCoefficients,
+    #       boolean includeShield) {
+    #     ThinAsymmetricCurrentSheetBasisVectorField thinCurrentSheet =
+    #         new ThinAsymmetricCurrentSheetBasisVectorField(tailLength, currentSheetHalfThickness,
+    #             tailCoeffs, bessel);
+    #     ThinAsymmetricCurrentSheetBasisVectorShieldingField thinCurrentSheetShield =
+    #         new ThinAsymmetricCurrentSheetBasisVectorShieldingField(staticCoefficients, bessel);
+    #     return new ShieldedThinCurrentSheetField(thinCurrentSheet, thinCurrentSheetShield,
+    #         includeShield);
+    #   }
+
+    @staticmethod
+    def createUnity(currentSheetHalfThickness, tailLength, bessel,
+                    staticCoefficients, includeShield):
+        """createUnity
+
+        param DifferentiableScalarFieldIJ currentSheetHalfThickness
+        param double tailLength
+        param BesselFunctionEvaluator bessel
+        param ThinCurrentSheetShieldingCoefficients staticCoefficients
+        param boolean includeShield
+        return ShieldedThinCurrentSheetField
+        """
+        pass
+    #     ThinAsymmetricCurrentSheetBasisVectorField thinCurrentSheet =
+    #         ThinAsymmetricCurrentSheetBasisVectorField.createUnity(tailLength,
+    #             currentSheetHalfThickness, staticCoefficients.getNumAzimuthalExpansions(),
+    #             staticCoefficients.getNumRadialExpansions(), bessel);
+    #     ThinAsymmetricCurrentSheetBasisVectorShieldingField thinCurrentSheetShield =
+    #         new ThinAsymmetricCurrentSheetBasisVectorShieldingField(staticCoefficients, bessel);
+    #     return new ShieldedThinCurrentSheetField(thinCurrentSheet, thinCurrentSheetShield,
+    #         includeShield);
+    #   }
+
+    #   /**
+    #    * 
+    #    * @param position
+    #    * @return
+    #    */
+    #   public TailSheetExpansions evaluateExpansions(UnwritableVectorIJK position) {
+
+    #     TailSheetExpansions equatorialExpansions = thinCurrentSheet.evaluateExpansions(position);
+
+    #     // Calculate the field expansions
+    #     Expansion1D<UnwritableVectorIJK> tailSheetSymmetricValues =
+    #         equatorialExpansions.getTailSheetSymmetricValues();
+    #     Expansion2D<UnwritableVectorIJK> tailSheetOddValues =
+    #         equatorialExpansions.getTailSheetOddValues();
+    #     Expansion2D<UnwritableVectorIJK> tailSheetEvenValues =
+    #         equatorialExpansions.getTailSheetEvenValues();
+
+    #     // This is the most expensive lines of the module, If you don't need the
+    #     // shielding, it should NOT be computed.
+    #     // These three lines account for for over 90% of the model evaluation
+    #     if (includeShield) {
+
+    #       TailSheetExpansions shield = thinCurrentSheetShield.evaluateExpansions(position);
+
+    #       Expansion1D<UnwritableVectorIJK> tailSheetSymmetricShieldValues =
+    #           shield.getTailSheetSymmetricValues();
+
+    #       Expansion2D<UnwritableVectorIJK> tailSheetOddShieldValues = shield.getTailSheetOddValues();
+
+    #       Expansion2D<UnwritableVectorIJK> tailSheetEvenShieldValues = shield.getTailSheetEvenValues();
+
+    #       tailSheetSymmetricValues =
+    #           Expansion1Ds.Vectors.add(tailSheetSymmetricValues, tailSheetSymmetricShieldValues);
+    #       tailSheetOddValues = Expansion2Ds.Vectors.add(tailSheetOddValues, tailSheetOddShieldValues);
+    #       tailSheetEvenValues =
+    #           Expansion2Ds.Vectors.add(tailSheetEvenValues, tailSheetEvenShieldValues);
+    #     }
+
+    #     return new TailSheetExpansions(tailSheetSymmetricValues, tailSheetOddValues,
+    #         tailSheetEvenValues);
+    #   }
+
+    #   @Override
+    #   public ImmutableList<UnwritableVectorIJK> evaluateExpansion(UnwritableVectorIJK location) {
+    #     return evaluateExpansions(location).getExpansionsAsList();
+    #   }
+
+    #   @Override
+    #   public int getNumberOfBasisFunctions() {
+    #     return numRadialExpansions + 2 * (numAzimuthalExpansions * numRadialExpansions);
+    #   }
+
+    # }

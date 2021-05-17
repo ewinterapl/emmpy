0a83eaf41c2a41681ac0f4aaea2caddf19740ccd
Eric Winter <eric.winter@jhuapl.edu>
2021-05-17 16:34:20 -0400

    First complete BirkelandDeformationFunction with stub tests.

--- emmpy/geomagmodel/ts07/modeling/fieldaligned/birkelanddeformationfunction.py
+++ emmpy/geomagmodel/ts07/modeling/fieldaligned/birkelanddeformationfunction.py
@@ -1,14 +1,11 @@
 """emmpy.geomagmodel.ts07.modeling.fieldaligned.birkelanddeformation"""
 
 
-# import static crucible.core.math.CrucibleMath.cos;
-# import static crucible.core.math.CrucibleMath.sin;
-# import static crucible.core.math.CrucibleMath.sqrt;
-# import java.util.Arrays;
-# import crucible.core.math.coords.SphericalVector;
+from math import cos, sin, sqrt
 
+from emmpy.crucible.core.math.coords.sphericalvector import SphericalVector
 from emmpy.magmodel.core.math.vectorfields.differentiablesphericalvectorfield import (
-    DifferentiableSphericalVectorField
+    Results, DifferentiableSphericalVectorField
 )
 
 
@@ -20,128 +17,152 @@ class BirkelandDeformationFunction(DifferentiableSphericalVectorField):
 
     author G.K.Stephens
     """
-    pass
-    # public class BirkelandDeformationFunction implements DifferentiableSphericalVectorField {
 
-    #   // Used for numerical differentiation
-    #   private final static double delta = 1E-6;
+    # Used for numerical differentiation
+    delta = 1E-6
 
-    #   private final double[] a;
-    #   private final double[] b;
-    #   private final double[] c;
-    #   private final double[] d;
+    def __init__(self, a, b, c, d):
+        """Construct deformed field by providing the coefficients a, b, c,
+        and d.
 
-    #   /**
-    #    * Construct deformed field:
-    #    * <p>
-    #    * <img src="./doc-files/t02_eq18_19.png"/>
-    #    * <p>
-    #    * by providing the coefficients a, b, c, and d.
-    #    * 
-    #    * @param a an array of doubles containing the radial deformation constants
-    #    * @param b an array of doubles containing the radial deformation constants
-    #    * @param c an array of doubles containing the co-latitudinal deformation constants
-    #    * @param d an array of doubles containing the co-latitudinal deformation constants
-    #    */
-    #   public BirkelandDeformationFunction(double[] a, double[] b, double[] c, double[] d) {
-    #     super();
-    #     // make defensive copy
-    #     this.a = Arrays.copyOf(a, a.length);
-    #     this.b = Arrays.copyOf(b, b.length);
-    #     this.c = Arrays.copyOf(c, c.length);
-    #     this.d = Arrays.copyOf(d, d.length);
-    #   }
+        param a an array of doubles containing the radial deformation constants
+        param b an array of doubles containing the radial deformation constants
+        param c an array of doubles containing the co-latitudinal deformation constants
+        param d an array of doubles containing the co-latitudinal deformation constants
+        """
+        # make defensive copy
+        self.a = a[:]
+        self.b = b[:]
+        self.c = c[:]
+        self.d = d[:]
 
-    #   @Override
-    #   public SphericalVector evaluate(SphericalVector location) {
+    def evaluate(self, location):
+        """evaluate
 
-    #     double r = location.getRadius();
-    #     double theta = location.getColatitude();
-    #     double phi = location.getLongitude();
+        param SphericalVector location
+        return SphericalVector
+        """
+        # float r, theta, phi
+        r = location.getRadius()
+        theta = location.getColatitude()
+        phi = location.getLongitude()
 
-    #     // Deform the coordinate system
-    #     double rDef = rDeform(r, theta);
-    #     double thetaDef = thetaDeform(r, theta);
+        # Deform the coordinate system
+        # float rDef, thetaDef
+        rDef = self.rDeform(r, theta)
+        thetaDef = self.thetaDeform(r, theta)
 
-    #     return new SphericalVector(rDef, thetaDef, phi);
-    #   }
+        return SphericalVector(rDef, thetaDef, phi)
 
-    #   @Override
-    #   public Results differentiate(SphericalVector location) {
+    def differentiate(self, location):
+        """differentiate
 
-    #     double r = location.getRadius();
-    #     double theta = location.getColatitude();
-    #     double phi = location.getLongitude();
+        param SphericalVector location
+        return Results
+        """
 
-    #     // Deform the coordinate system
-    #     double rDef = rDeform(r, theta);
-    #     double thetaDef = thetaDeform(r, theta);
+        # float r, theta, phi
+        r = location.getRadius()
+        theta = location.getColatitude()
+        phi = location.getLongitude()
 
-    #     SphericalVector locDef = new SphericalVector(rDef, thetaDef, phi);
+        # Deform the coordinate system
+        # float rDef, thetaDef
+        rDef = self.rDeform(r, theta)
+        thetaDef = self.thetaDeform(r, theta)
+        # SphericalVector locDef
+        locDef = SphericalVector(rDef, thetaDef, phi)
 
-    #     // Numerically approximate derivatives for deformation
-    #     SphericalVector locPr = evaluate(new SphericalVector(r + delta, theta, phi));
-    #     SphericalVector locMr = evaluate(new SphericalVector(r - delta, theta, phi));
+        # Numerically approximate derivatives for deformation
+        # SphericalVector locPr, locMr
+        locPr = self.evaluate(
+            SphericalVector(r + BirkelandDeformationFunction.delta, theta,
+                            phi)
+        )
+        locMr = self.evaluate(
+            SphericalVector(r - BirkelandDeformationFunction.delta, theta,
+                            phi)
+        )
 
-    #     SphericalVector locPt = evaluate(new SphericalVector(r, theta + delta, phi));
-    #     SphericalVector locMt = evaluate(new SphericalVector(r, theta - delta, phi));
+        # SphericalVector locPt, locMt
+        locPt = self.evaluate(
+            SphericalVector(r, theta + BirkelandDeformationFunction.delta,
+            phi)
+        )
+        locMt = self.evaluate(
+            SphericalVector(r, theta - BirkelandDeformationFunction.delta,
+            phi)
+        )
 
-    #     double drDef_dr = (locPr.getRadius() - locMr.getRadius()) / (2 * delta);
-    #     double drDef_dtheta = (locPt.getRadius() - locMt.getRadius()) / (2 * delta);
+        # double drDef_dr, drDef_dtheta, dthetaDef_dr, dthetaDef_dTheta,
+        # dphiDef_dPhi
+        drDef_dr = (
+            (locPr.getRadius() - locMr.getRadius()) /
+            (2*BirkelandDeformationFunction.delta)
+        )
+        drDef_dtheta = (
+            (locPt.getRadius() - locMt.getRadius()) /
+            (2*BirkelandDeformationFunction.delta)
+        )
+        dthetaDef_dr = (
+            (locPr.getColatitude() - locMr.getColatitude()) /
+            (2*BirkelandDeformationFunction.delta)
+        )
+        dthetaDef_dTheta = (
+            (locPt.getColatitude() - locMt.getColatitude()) /
+            (2*BirkelandDeformationFunction.delta)
+        )
+        dphiDef_dPhi = 1.0
 
-    #     double dthetaDef_dr = (locPr.getColatitude() - locMr.getColatitude()) / (2 * delta);
-    #     double dthetaDef_dTheta = (locPt.getColatitude() - locMt.getColatitude()) / (2 * delta);
+        return Results(locDef, drDef_dr, drDef_dtheta, 0, dthetaDef_dr,
+                       dthetaDef_dTheta, 0, 0, 0, dphiDef_dPhi)
 
-    #     double dphiDef_dPhi = 1.0;
+    def rDeform(self, r, theta):
+        """Deforms the radius to describe the field aligned current.
 
-    #     return new DifferentiableSphericalVectorField.Results(locDef, drDef_dr, drDef_dtheta, 0,
-    #         dthetaDef_dr, dthetaDef_dTheta, 0, 0, 0, dphiDef_dPhi);
-    #   }
+        See Tsy 2002-1 Eqn 18
+        The b coefficients as given in table 2 were always squared, so they are
+        squared here to save math. Note the difference.
 
-    #   /**
-    #    * Deforms the radius to describe the field aligned current. See Tsy 2002-1 Eqn 18
-    #    * 
-    #    * The b coefficients as given in table 2 were always squared, so they are squared here to save
-    #    * math. Note the difference.
-    #    * 
-    #    * There are two differences between this calculation and the published equation. An email to
-    #    * Nikolai Tsyganenko confirmed that these are the proper versions (6/25/2011).
-    #    * 
-    #    * @param r the undeformed radius
-    #    * @param theta the underformed polar angle (measured from the GSM Z axis)
-    #    * @return the deformed radius
-    #    */
-    #   private double rDeform(double r, double theta) {
+        There are two differences between this calculation and the published
+        equation. An email to Nikolai Tsyganenko confirmed that these are the
+        proper versions (6/25/2011).
 
-    #     double r2 = r * r;
+        param double r the undeformed radius
+        param double theta the underformed polar angle (measured from the GSM Z axis)
+        return (double) the deformed radius
+        """
+        # double r2
+        r2 = r*r
+        return (
+            r + self.a[0]/r + (self.a[1]*r /(sqrt(r2 + self.b[0]*self.b[0]))) +
+            (self.a[2]*r/(r2 + self.b[1]*self.b[1])) +
+            cos(theta)*(self.a[3] + self.a[4]/r + self.a[5]*r /
+                        sqrt(r2 + self.b[2]*self.b[2]) +
+                        self.a[6]*r/(r2 + self.b[3]*self.b[3])) +
+            cos(2*theta)*(self.a[7]*r/sqrt(r2 + self.b[4]*self.b[4]) +
+                          self.a[8]*r/((r2 + self.b[5]*self.b[5]) *
+                                       (r2 + self.b[5]*self.b[5])))
+        )
 
-    #     return r + a[0] / r + (a[1] * r / (sqrt(r2 + b[0] * b[0]))) + (a[2] * r / (r2 + b[1] * b[1]))
+    def thetaDeform(self, r, theta):
+        """Deforms theta to describe the field aligned current.
 
-    #         + cos(theta)
-    #             * (a[3] + a[4] / r + a[5] * r / sqrt(r2 + b[2] * b[2]) + a[6] * r / (r2 + b[3] * b[3]))
+        See Tsy 2002-1 Eqn 19.
+        The d coefficients as given in table 2 were always squared, so they
+        are squared here to save math. Note the difference.
 
-    #         + cos(2 * theta) * (a[7] * r / sqrt(r2 + b[4] * b[4])
-    #             + a[8] * r / ((r2 + b[5] * b[5]) * (r2 + b[5] * b[5])));
-    #   }
-
-    #   /**
-    #    * Deforms theta to describe the field aligned current. See Tsy 2002-1 Eqn 19.
-    #    * 
-    #    * The d coefficients as given in table 2 were always squared, so they are squared here to save
-    #    * math. Note the difference.
-    #    * 
-    #    * @param r the initial radius
-    #    * @param theta the initial polar angle (measured from the GSM Z axis)
-    #    * @return the deformed polar angle (theta)
-    #    */
-    #   private double thetaDeform(double r, double theta) {
-
-    #     double r2 = r * r;
-
-    #     return theta + (c[0] + c[1] / r + c[2] / r2 + c[3] * r / sqrt(r2 + d[0] * d[0])) * sin(theta)
-    #         + (c[4] + c[5] * r / sqrt(r2 + d[1] * d[1]) + c[6] * r / (r2 + d[2] * d[2]))
-    #             * sin(2 * theta)
-    #         + (c[7] + c[8] / r + c[9] * r / (r2 + d[3] * d[3])) * sin(3 * theta);
-    #   }
-
-    # }
+        param r the initial radius
+        param theta the initial polar angle (measured from the GSM Z axis)
+        return the deformed polar angle (theta)
+        """
+        # double r2
+        r2 = r*r
+        return (
+            theta + (self.c[0] + self.c[1]/r + self.c[2]/r2 +
+                     self.c[3]*r/sqrt(r2 + self.d[0]*self.d[0]))*sin(theta) +
+            (self.c[4] + self.c[5]*r/sqrt(r2 + self.d[1]*self.d[1]) +
+             self.c[6]*r/(r2 + self.d[2]*self.d[2])) * sin(2 * theta) +
+            (self.c[7] + self.c[8]/r + self.c[9]*r /
+             (r2 + self.d[3]*self.d[3]))*sin(3*theta)
+        )
--- emmpy/geomagmodel/ts07/modeling/fieldaligned/tests/test_BirkelandDeformationFunction.py
+++ emmpy/geomagmodel/ts07/modeling/fieldaligned/tests/test_BirkelandDeformationFunction.py
@@ -6,7 +6,21 @@ from emmpy.geomagmodel.ts07.modeling.fieldaligned.birkelanddeformationfunction i
 
 
 class TestBuilder(unittest.TestCase):
-    pass
+
+    def test___init__(self):
+        pass
+
+    def test_evaluate(self):
+        pass
+
+    def test_differentiate(self):
+        pass
+
+    def test_rDeform(self):
+        pass
+
+    def test_thetaDeform(self):
+        pass
 
 
 if __name__ == '__main__':

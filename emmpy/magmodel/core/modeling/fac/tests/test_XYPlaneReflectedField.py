c585d77293634551c2ee012e9fe9825f6b333883
Eric Winter <eric.winter@jhuapl.edu>
2021-05-17 17:17:26 -0400

    First complete XYPlaneReflectedField with stub tests.

--- emmpy/magmodel/core/modeling/fac/tests/test_XYPlaneReflectedField.py
+++ emmpy/magmodel/core/modeling/fac/tests/test_XYPlaneReflectedField.py
@@ -6,7 +6,12 @@ from emmpy.magmodel.core.modeling.fac.xyplanereflectedfield import (
 
 
 class TestBuilder(unittest.TestCase):
-    pass
+
+    def test___init__(self):
+        pass
+
+    def test_evaluate(self):
+        pass
 
 
 if __name__ == '__main__':
--- emmpy/magmodel/core/modeling/fac/xyplanereflectedfield.py
+++ emmpy/magmodel/core/modeling/fac/xyplanereflectedfield.py
@@ -1,5 +1,46 @@
 """emmpy.magmodel.core.modeling.fac.xyplanereflectedfield"""
 
 
-class XYPlaneReflectedField:
-    pass
+# import crucible.core.math.vectorspace.VectorIJK;
+
+from emmpy.crucible.core.math.vectorfields.vectorfield import VectorField
+from emmpy.crucible.core.math.vectorspace.unwritablevectorijk import (
+    UnwritableVectorIJK
+)
+
+
+class XYPlaneReflectedField(VectorField):
+    """The reflection of the input field about the X-Y plane.
+
+    From T02: "The contribution from the southern currents (symmetric to the
+    northern ones for TILT = 0) can be readily represented by rotating the
+    northern (N) part by 180deg around the X-axis and changing the polarity
+    of the current."
+
+    author G.K.Stephens
+    """
+
+    def __init__(self, delgate):
+        """Constructor
+
+        param VectorField delgate
+        """
+        # VectorField delgate
+        self.delgate = delgate
+
+    def evaluate(self, location, buffer):
+        """evaluate
+
+        param UnwritableVectorIJK location
+        param VectorIJK buffer
+        return VectorIJK
+        """
+        # UnwritableVectorIJK reflectedLocation
+        reflectedLocation = UnwritableVectorIJK(
+            location.getI(), location.getJ(), -location.getK()
+        )
+        delgate.evaluate(reflectedLocation, buffer)
+        # double bz
+        bz = buffer.getK()
+        buffer.setK(-bz)
+        return buffer

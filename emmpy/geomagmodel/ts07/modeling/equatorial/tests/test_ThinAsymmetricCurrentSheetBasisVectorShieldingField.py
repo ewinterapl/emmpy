dbe9113f5a5d0d794b467b2fd581a0474ea45368
Eric Winter <eric.winter@jhuapl.edu>
2021-05-25 11:46:30 -0400

    First complete ThinAsymmetricCurrentSheetBasisVectorShieldingField. Tests are stubs.

--- emmpy/geomagmodel/ts07/modeling/equatorial/tests/test_ThinAsymmetricCurrentSheetBasisVectorShieldingField.py
+++ emmpy/geomagmodel/ts07/modeling/equatorial/tests/test_ThinAsymmetricCurrentSheetBasisVectorShieldingField.py
@@ -6,7 +6,18 @@ from emmpy.geomagmodel.ts07.modeling.equatorial.thinasymmetriccurrentsheetbasisv
 
 
 class TestBuilder(unittest.TestCase):
-    pass
+
+    def test___init__(self):
+        pass
+
+    def test_evaluateExpansion(self):
+        pass
+
+    def test_evaluateExpansions(self):
+        pass
+
+    def test_getNumberOfBasisFunctions(self):
+        pass
 
 
 if __name__ == '__main__':
--- emmpy/geomagmodel/ts07/modeling/equatorial/thinasymmetriccurrentsheetbasisvectorshieldingfield.py
+++ emmpy/geomagmodel/ts07/modeling/equatorial/thinasymmetriccurrentsheetbasisvectorshieldingfield.py
@@ -2,31 +2,32 @@
 
 
 # import com.google.common.collect.ImmutableList;
-# import crucible.core.math.vectorspace.UnwritableVectorIJK;
 # import crucible.core.math.vectorspace.VectorIJK;
 # import geomagmodel.ts07.coefficientreader.ThinCurrentSheetShieldingCoefficients;
-# import magmodel.core.math.CylindricalHarmonicField;
-# import magmodel.core.math.TrigParity;
 # import magmodel.core.math.bessel.BesselFunctionEvaluator;
 # import magmodel.core.math.expansions.CoefficientExpansion1D;
 # import magmodel.core.math.expansions.CoefficientExpansion2D;
-# import magmodel.core.math.expansions.Expansion1Ds;
-# import magmodel.core.math.expansions.Expansion2Ds;
-# import magmodel.core.modeling.equatorial.expansion.TailSheetExpansions;
 
+from emmpy.crucible.core.math.vectorspace.unwritablevectorijk import (
+    UnwritableVectorIJK
+)
 from emmpy.crucible.core.math.vectorspace.vectorijk import VectorIJK
 from emmpy.crucible.core.math.vectorfields.vectorfield import VectorField
+from emmpy.magmodel.core.math.cylindricalharmonicfield import (
+    CylindricalHarmonicField
+)
+from emmpy.magmodel.core.math.expansions.expansion1ds import Expansion1Ds
+from emmpy.magmodel.core.math.expansions.expansion2ds import Expansion2Ds
+from emmpy.magmodel.core.math.trigparity import TrigParity
 from emmpy.magmodel.core.math.vectorfields.basisvectorfield import (
     BasisVectorField
+)
+from emmpy.magmodel.core.modeling.equatorial.expansion.tailsheetexpansions import (
+    TailSheetExpansions
 )
 
 
 class ThinAsymmetricCurrentSheetBasisVectorShieldingField(BasisVectorField):
-
-    # private final ThinCurrentSheetShieldingCoefficients coeffs;
-    # private final BesselFunctionEvaluator bessel;
-    # private final int numAzimuthalExpansions;
-    # private final int numRadialExpansions;
 
     def __init__(self, coeffs, bessel):
         """Constructor
@@ -34,15 +35,22 @@ class ThinAsymmetricCurrentSheetBasisVectorShieldingField(BasisVectorField):
         param ThinCurrentSheetShieldingCoefficients coeffs
         param BesselFunctionEvaluator bessel
         """
+        # ThinCurrentSheetShieldingCoefficients coeffs
         self.coeffs = coeffs
+        # BesselFunctionEvaluator bessel
         self.bessel = bessel
+        # int numAzimuthalExpansions
         self.numAzimuthalExpansions = coeffs.getNumAzimuthalExpansions()
+        # int numRadialExpansions
         self.numRadialExpansions = coeffs.getNumRadialExpansions()
 
-    #   @Override
-    #   public ImmutableList<UnwritableVectorIJK> evaluateExpansion(UnwritableVectorIJK location) {
-    #     return evaluateExpansions(location).getExpansionsAsList();
-    #   }
+    def evaluateExpansion(self, location):
+        """evaluateExpansion
+
+        param UnwritableVectorIJK location
+        return ImmutableList<UnwritableVectorIJK>
+        """
+        return self.evaluateExpansions(location).getExpansionsAsList()
 
     def evaluateExpansions(self, location):
         """evaluateExpansions
@@ -63,66 +71,82 @@ class ThinAsymmetricCurrentSheetBasisVectorShieldingField(BasisVectorField):
                 evenExpansions[i].append(None)
 
         # n is the radial expansion number
-        for n in range(self.numRadialExpansions + 1):
+        for n in range(1, self.numRadialExpansions + 1):
             # CoefficientExpansion2D tailExpansion
             tailExpansion = (
                 self.coeffs.getSymmetricTailExpansion().getExpansion(n)
             )
+            # CoefficientExpansion1D waveNumberExpansion
             waveNumberExpansion = (
                 self.coeffs.getSymmetricTailWaveExpansion().getExpansion(n)
             )
             buffer = VectorIJK()
             chf = CylindricalHarmonicField(tailExpansion, waveNumberExpansion,
-                                           bessel, TrigParity.ODD)
-    #     for (int n = 1; n <= numRadialExpansions; n++) {
-    #       CoefficientExpansion2D tailExpansion = coeffs.getSymmetricTailExpansion().getExpansion(n);
-    #       CoefficientExpansion1D waveNumberExpansion =
-    #           coeffs.getSymmetricTailWaveExpansion().getExpansion(n);
-    #       VectorIJK buffer = new VectorIJK();
-    #       new CylindricalHarmonicField(tailExpansion, waveNumberExpansion, bessel, TrigParity.ODD)
-    #           .evaluate(location, buffer);
-    #       symmetricExpansions[n - 1] =
-    #           new UnwritableVectorIJK(buffer.getI(), buffer.getJ(), buffer.getK());
-    #     }
+                                           self.bessel, TrigParity.ODD)
+            chf.evaluate(location, buffer)
+            symmetricExpansions[n - 1] = UnwritableVectorIJK(
+                buffer.getI(), buffer.getJ(), buffer.getK()
+            )
 
-    #     // n is the radial expansion number
-    #     // m is the azimuthal expansion number
-    #     double negateConst = 1.0;
-    #     for (int n = 1; n <= numRadialExpansions; n++) {
-    #       for (int m = 1; m <= numAzimuthalExpansions; m++) {
-    #         CoefficientExpansion2D tailExpansion = coeffs.getOddTailExpansion().getExpansion(m, n);
-    #         CoefficientExpansion1D waveNumberExpansion =
-    #             coeffs.getOddTailWaveExpansion().getExpansion(m, n);
-    #         VectorIJK buffer = new VectorIJK();
-    #         new CylindricalHarmonicField(tailExpansion, waveNumberExpansion, bessel, TrigParity.ODD)
-    #             .evaluate(location, buffer);
-    #         buffer.scale(negateConst);
-    #         oddExpansions[m - 1][n - 1] =
-    #             new UnwritableVectorIJK(buffer.getI(), buffer.getJ(), buffer.getK());
-    #       }
-    #     }
-    #     // n is the radial expansion number
-    #     // m is the azimuthal expansion number
-    #     negateConst = -1.0;
-    #     for (int n = 1; n <= numRadialExpansions; n++) {
-    #       for (int m = 1; m <= numAzimuthalExpansions; m++) {
-    #         CoefficientExpansion2D tailExpansion = coeffs.getEvenTailExpansion().getExpansion(m, n);
-    #         CoefficientExpansion1D waveNumberExpansion =
-    #             coeffs.getEvenTailWaveExpansion().getExpansion(m, n);
-    #         VectorIJK buffer = new VectorIJK();
-    #         new CylindricalHarmonicField(tailExpansion, waveNumberExpansion, bessel, TrigParity.EVEN)
-    #             .evaluate(location, buffer);
-    #         buffer.scale(negateConst);
-    #         evenExpansions[m - 1][n - 1] =
-    #             new UnwritableVectorIJK(buffer.getI(), buffer.getJ(), buffer.getK());
-    #       }
-    #     }
-    #     return new TailSheetExpansions(Expansion1Ds.createFromArray(symmetricExpansions, 1),
-    #         Expansion2Ds.createFromArray(oddExpansions, 1, 1),
-    #         Expansion2Ds.createFromArray(evenExpansions, 1, 1));
-    #   }
+        # n is the radial expansion number
+        # m is the azimuthal expansion number
+        # float negateConst
+        negateConst = 1.0
+        for n in range(1, self.numRadialExpansions + 1):
+            for m in range(1, self.numAzimuthalExpansions + 1):
+                # CoefficientExpansion2D tailExpansion
+                tailExpansion = (
+                    self.coeffs.getOddTailExpansion().getExpansion(m, n)
+                )
+                # CoefficientExpansion1D waveNumberExpansion
+                waveNumberExpansion = (
+                    self.coeffs.getOddTailWaveExpansion().getExpansion(m, n)
+                )
+                buffer = VectorIJK()
+                chf = CylindricalHarmonicField(
+                    tailExpansion, waveNumberExpansion, self.bessel,
+                    TrigParity.ODD
+                )
+                chf.evaluate(location, buffer)
+                buffer.scale(negateConst)
+                oddExpansions[m - 1][n - 1] = (
+                    UnwritableVectorIJK(buffer.getI(), buffer.getJ(),
+                                        buffer.getK())
+                )
 
-    #   @Override
-    #   public int getNumberOfBasisFunctions() {
-    #     return numRadialExpansions + 2 * numRadialExpansions * numAzimuthalExpansions;
-    #   }
+        # n is the radial expansion number
+        # m is the azimuthal expansion number
+        # float negateConst
+        negateConst = -1.0
+        for n in range(1, self.numRadialExpansions + 1):
+            for m in range(1, self.numAzimuthalExpansions + 1):
+                # CoefficientExpansion2D tailExpansion
+                tailExpansion = (
+                    self.coeffs.getEvenTailExpansion().getExpansion(m, n)
+                )
+                # CoefficientExpansion1D waveNumberExpansion
+                waveNumberExpansion = (
+                    self.coeffs.getEvenTailWaveExpansion().getExpansion(m, n)
+                )
+                buffer = VectorIJK()
+                chf = CylindricalHarmonicField(
+                    tailExpansion, waveNumberExpansion, self.bessel,
+                    TrigParity.EVEN
+                )
+                chf.evaluate(location, buffer)
+                buffer.scale(negateConst)
+                evenExpansions[m - 1][n - 1] = (
+                    UnwritableVectorIJK(buffer.getI(), buffer.getJ(),
+                                        buffer.getK())
+                )
+
+        return TailSheetExpansions(Expansion1Ds.createFromArray(symmetricExpansions, 1),
+            Expansion2Ds.createFromArray(oddExpansions, 1, 1),
+            Expansion2Ds.createFromArray(evenExpansions, 1, 1))
+
+    def getNumberOfBasisFunctions(self):
+        """return int"""
+        return (
+            self.numRadialExpansions +
+            2*self.numRadialExpansions*self.numAzimuthalExpansions
+        )

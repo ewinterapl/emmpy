83c2bdf1dcac7120914e06268f922dc217c64cda
Eric Winter <eric.winter@jhuapl.edu>
2021-06-01 17:52:34 -0400

    Complete overhaul to remove maximum warnings from all linters.

--- emmpy/crucible/core/math/vectorspace/tests/test_UnwritableVectorIJK.py
+++ emmpy/crucible/core/math/vectorspace/tests/test_UnwritableVectorIJK.py
@@ -1,3 +1,6 @@
+"""Tests for UnwritableVectorIJK."""
+
+
 from math import sqrt
 import unittest
 
@@ -7,42 +10,52 @@ from emmpy.crucible.core.math.vectorspace.unwritablevectorijk import (
 
 
 class TestBuilder(unittest.TestCase):
+    """Build the tests."""
 
     def test___init__(self):
+        """Test __init__()."""
+        # 0 args is invalid
         with self.assertRaises(Exception):
             UnwritableVectorIJK()
-        # 1-argument forms
-        v = UnwritableVectorIJK([1.1, 2.2, 3.3])
-        self.assertAlmostEqual(v.i, 1.1)
-        self.assertAlmostEqual(v.j, 2.2)
-        self.assertAlmostEqual(v.k, 3.3)
-        v2 = UnwritableVectorIJK(v)
-        self.assertAlmostEqual(v2.i, 1.1)
-        self.assertAlmostEqual(v2.j, 2.2)
-        self.assertAlmostEqual(v2.k, 3.3)
-        with self.assertRaises(Exception):
-            UnwritableVectorIJK(None)
-        # 2-argument forms
-        v = UnwritableVectorIJK(1, [0, 1.11, 2.22, 3.33])
-        self.assertAlmostEqual(v.i, 1.11)
-        self.assertAlmostEqual(v.j, 2.22)
-        self.assertAlmostEqual(v.k, 3.33)
-        v2 = UnwritableVectorIJK(-2, v)
-        self.assertAlmostEqual(v2.i, -2.22)
-        self.assertAlmostEqual(v2.j, -4.44)
-        self.assertAlmostEqual(v2.k, -6.66)
-        with self.assertRaises(Exception):
-            UnwritableVectorIJK(None, None)
-        # 3-argument form
+        # Basic 3-argument form
         v = UnwritableVectorIJK(1.1, 2.2, 3.3)
         self.assertAlmostEqual(v.i, 1.1)
         self.assertAlmostEqual(v.j, 2.2)
         self.assertAlmostEqual(v.k, 3.3)
-        # Invlaid form
+        # 1-argument forms
+        # list
+        v = UnwritableVectorIJK([1.1, 2.2, 3.3])
+        self.assertAlmostEqual(v.i, 1.1)
+        self.assertAlmostEqual(v.j, 2.2)
+        self.assertAlmostEqual(v.k, 3.3)
+        # copy constructor
+        v2 = UnwritableVectorIJK(v)
+        self.assertAlmostEqual(v2.i, 1.1)
+        self.assertAlmostEqual(v2.j, 2.2)
+        self.assertAlmostEqual(v2.k, 3.3)
+        # invalid single argument
+        with self.assertRaises(Exception):
+            UnwritableVectorIJK(None)
+        # 2-argument forms
+        # offset and list
+        v = UnwritableVectorIJK(1, [0, 1.11, 2.22, 3.33])
+        self.assertAlmostEqual(v.i, 1.11)
+        self.assertAlmostEqual(v.j, 2.22)
+        self.assertAlmostEqual(v.k, 3.33)
+        # scale and vector
+        v2 = UnwritableVectorIJK(-2, v)
+        self.assertAlmostEqual(v2.i, -2.22)
+        self.assertAlmostEqual(v2.j, -4.44)
+        self.assertAlmostEqual(v2.k, -6.66)
+        # 2 bad args
+        with self.assertRaises(Exception):
+            UnwritableVectorIJK(None, None)
+        # 4 args is invalid
         with self.assertRaises(Exception):
             UnwritableVectorIJK(1, 2, 3, 4)
 
     def test_createUnitized(self):
+        """Test createUnitized()."""
         v = UnwritableVectorIJK(1, 2, 3)
         v2 = v.createUnitized()
         self.assertAlmostEqual(v2.i, 1/sqrt(14))
@@ -53,6 +66,7 @@ class TestBuilder(unittest.TestCase):
             v.createUnitized()
 
     def test_createNegated(self):
+        """Test createNegated()."""
         v = UnwritableVectorIJK(1, 2, 3)
         v2 = v.createNegated()
         self.assertAlmostEqual(v2.i, -1)
@@ -60,6 +74,7 @@ class TestBuilder(unittest.TestCase):
         self.assertAlmostEqual(v2.k, -3)
 
     def test_createScaled(self):
+        """Test createScaled()."""
         v = UnwritableVectorIJK(1, 2, 3)
         v2 = v.createScaled(2)
         self.assertAlmostEqual(v2.i, 2)
@@ -67,18 +82,22 @@ class TestBuilder(unittest.TestCase):
         self.assertAlmostEqual(v2.k, 6)
 
     def test_getI(self):
+        """Test getI()."""
         v = UnwritableVectorIJK(1, 2, 3)
         self.assertAlmostEqual(v.getI(), 1)
 
     def test_getJ(self):
+        """Test getJ()."""
         v = UnwritableVectorIJK(1, 2, 3)
         self.assertAlmostEqual(v.getJ(), 2)
 
     def test_getK(self):
+        """Test getK()."""
         v = UnwritableVectorIJK(1, 2, 3)
         self.assertAlmostEqual(v.getK(), 3)
 
     def test_get(self):
+        """Test get()."""
         v = UnwritableVectorIJK(1, 2, 3)
         self.assertAlmostEqual(v.get(0), 1)
         self.assertAlmostEqual(v.get(1), 2)
@@ -87,15 +106,18 @@ class TestBuilder(unittest.TestCase):
             v.get(3)
 
     def test_getLength(self):
+        """Test getLength()."""
         v = UnwritableVectorIJK(1, 2, 3)
         self.assertAlmostEqual(v.getLength(), sqrt(14))
 
     def test_getDot(self):
+        """Test getDot()."""
         v = UnwritableVectorIJK(1, 2, 3)
         v2 = UnwritableVectorIJK(2, 3, 4)
         self.assertAlmostEqual(v.getDot(v2), 20)
 
     def test_getSeparation(self):
+        """Test getSeparation()."""
         v0 = UnwritableVectorIJK(0, 0, 0)
         v1 = UnwritableVectorIJK(1, 1, 1)
         v2 = UnwritableVectorIJK(2, 1, 1)
@@ -106,21 +128,25 @@ class TestBuilder(unittest.TestCase):
         self.assertAlmostEqual(v1.getSeparation(v2), 0.33983690945412204)
 
     def test_getSeparationOutOfPlane(self):
+        """Test getSeparationOutOfPlane()."""
         v = UnwritableVectorIJK(1, 1, 1)
         v2 = UnwritableVectorIJK(2, 1, 1)
         self.assertAlmostEqual(v.getSeparationOutOfPlane(v2),
                                1.2309594173407745)
 
     def test_copyOf(self):
+        """Test copyOf()."""
         v = UnwritableVectorIJK(1, 1, 1)
         v2 = UnwritableVectorIJK.copyOf(v)
         self.assertEqual(v, v2)
 
     def test_hashCode(self):
+        """Test hashCode()."""
         v = UnwritableVectorIJK(1, 1, 1)
         self.assertEqual(v.hashCode(), 1057895773279)
 
     def test_equals(self):
+        """Test equals()."""
         v1 = UnwritableVectorIJK(1.1, 2.2, 3.3)
         self.assertTrue(v1.equals(v1))
         self.assertFalse(v1.equals(None))
@@ -135,6 +161,7 @@ class TestBuilder(unittest.TestCase):
         self.assertTrue(v1.equals(v2))
 
     def test_toString(self):
+        """Test toString()."""
         v = UnwritableVectorIJK(1.1, 2.2, 3.3)
         self.assertEqual(v.toString(), "[1.1,2.2,3.3]")
 
--- emmpy/crucible/core/math/vectorspace/unwritablevectorijk.py
+++ emmpy/crucible/core/math/vectorspace/unwritablevectorijk.py
@@ -1,4 +1,4 @@
-"""emmpy.crucible.core.math.vectorspace.unwritablevectorijk
+"""emmpy.crucible.core.math.vectorspace.unwritablevectorijk.
 
 A weakly immutable 3-dimensional vector designed to properly support a writable
 subclass.
@@ -18,247 +18,245 @@ works, is because by design the only member variables of this class live in the
 parent class. If one subclasses this class and defines additional members then
 this will most certainly break the implementation presented here.
 
-@author F.S.Turner
+author F.S.Turner
 """
+
 
 from math import asin, pi
 
 from emmpy.crucible.core.math.vectorspace.internaloperations import (
     computeNorm
 )
-# from emmpy.java.lang.double import Double
+from emmpy.java.lang.double import Double
 from emmpy.utilities.isrealnumber import isRealNumber
 
 
 class UnwritableVectorIJK:
+    """UnwritableVectorIJK."""
 
     def __init__(self, *args):
+        """Build a UnwritableVectorIJK."""
         if len(args) == 1:
             if isinstance(args[0], list):
                 # Constructs a vector from the first three elements of an
                 # array of doubles.
-                # @aram data the array of doubles
-                data = args[0]
-                UnwritableVectorIJK.__init__(self, data[0], data[1], data[2])
+                # param [float] data the array of doubles
+                (data,) = args
+                self.__init__(data[0], data[1], data[2])
             elif isinstance(args[0], UnwritableVectorIJK):
                 # Copy constructor, creates a vector by copying the values of a
                 # pre-existing one.
-                # @param vector the vector whose contents are to be copied
-                vector = args[0]
+                # param UnwritableVectgorIJK vector the vector whose contents
+                # are to be copied
+                (vector,) = args
                 self.__init__(vector.i, vector.j, vector.k)
             else:
                 raise Exception
         elif len(args) == 2:
             if isinstance(args[0], int) and isinstance(args[1], list):
-                raise Exception
-    #             # Constructs a vector from the three elements of an array of
-    #             # double starting with the offset index.
-    #             # @param offset index into the data array to copy into the ith
-    #             # component.
-    #             # @param data the array of doubles
-    #             (offset, data) = args
-    #             self.__init__(data[offset], data[offset + 1], data[offset + 2])
-            elif isRealNumber(args[0]) and isinstance(args[1],
-                                                      UnwritableVectorIJK):
+                # Constructs a vector from the three elements of an array of
+                # double starting with the offset index.
+                # param int offset index into the data array to copy into the
+                # ith component.
+                # param [float] data the array of doubles
+                (offset, data) = args
+                self.__init__(data[offset], data[offset + 1], data[offset + 2])
+            elif (
+                isRealNumber(args[0]) and
+                isinstance(args[1], UnwritableVectorIJK)
+            ):
                 # Scaling constructor, creates a new vector by applying a
                 # scalar multiple to the components of a pre-existing vector.
-                # @param scale the scale factor to apply
-                # @param vector the vector whose contents are to be scaled
+                # param float scale the scale factor to apply
+                # param UnwritableVectorIJK vector the vector whose contents
+                # are to be scaled
                 (scale, vector) = args
                 self.__init__(scale*vector.i, scale*vector.j, scale*vector.k)
             else:
                 raise Exception
         elif len(args) == 3:
             # Constructs a vector from the three basic components.
-            # param i the ith component
-            # param j the jth component
-            # param k the kth component
+            # param float i the ith component
+            # param float j the jth component
+            # param float k the kth component
             (i, j, k) = args
-            # The ith component of the vector, synonymous with the X-axis
             self.i = i
-            # The jth component of the vector, synonymous with the Y-axis
             self.j = j
-            # The kth component of the vector, synonymous with the Z-axis
             self.k = k
         else:
             raise Exception
 
-    # def createUnitized(self):
-    #     """Creates a new, unit length copy of the existing vector.
+    def createUnitized(self):
+        """Create a new, unit length copy of the existing vector.
 
-    #     This code is just a convenience method that implements:
-    #     new UnwritableVectorIJK(1.0/this.getLength(), this)  in a safe manner.
+        This code is just a convenience method that implements:
+        new UnwritableVectorIJK(1.0/this.getLength(), this)  in a safe manner.
 
-    #     @return a vector of unit length in the direction of the instance
-    #     @throws UnsupportedOperationException if the instance vector has zero
-    #     length.
-    #     """
-    #     norm = self.getLength()
-    #     if norm > 0.0:
-    #         return UnwritableVectorIJK(1.0/norm, self)
-    #     raise Exception("Unable to unitize. Supplied vector has zero length.")
+        return UnwritableVectorIJK a vector of unit length in the direction of
+        the instance
+        """
+        norm = self.getLength()
+        if norm > 0.0:
+            uvijk = UnwritableVectorIJK(1.0/norm, self)
+            return uvijk
+        raise Exception("Unable to unitize. Supplied vector has zero length.")
 
-    # def createNegated(self):
-    #     """Creates a new, negated copy of an the existing vector.
+    def createNegated(self):
+        """Create a new, negated copy of an the existing vector.
 
-    #     Convenience method for: new UnwritableVectorIJK(-1.0, this).
+        Convenience method for: new UnwritableVectorIJK(-1.0, this).
 
-    #     @return the negated vector, -this.
-    #     """
-    #     return UnwritableVectorIJK(-1, self)
+        return UnwritableVectorIJK the negated vector, -this.
+        """
+        return UnwritableVectorIJK(-1, self)
 
-    def createScaled(self, scale: float):
-        """Creates a new, scaled copy of an the existing vector by applying a
-        scalar multiple to the components.
+    def createScaled(self, scale):
+        """Create a scaled copy of an the vector.
 
         Convenience method for: new UnwritableVectorIJK(scale, this).
 
-        param scale the scale factor to apply
-        return the scaled vector, scale*this.
+        param float scale the scale factor to apply
+        return UnwritableVectorIJK the scaled vector, scale*this.
         """
         return UnwritableVectorIJK(scale, self)
 
     def getI(self):
-        """Gets the ith component."""
+        """Get the ith component (float)."""
         return self.i
 
     def getJ(self):
-        """Gets the jth component."""
+        """Get the jth component (float)."""
         return self.j
 
     def getK(self):
-        """Gets the kth component."""
+        """Get the kth component (float)."""
         return self.k
 
-    # def get(self, index: int) -> float:
-    #     """Retrieves the specified component of the vector.
+    def get(self, index):
+        """Retrieve the specified component of the vector.
 
-    #     @param index the index of the component to retrieve. 0 = ith, 1 = jth,
-    #     2 = kth.
+        param int index the index of the component to retrieve.
+        0 = ith, 1 = jth, 2 = kth.
+        return float the value for the requested component
+        throws Exception if an invalid index, outside the range [0,2], is
+        specified.
+        """
+        if index == 0:
+            return self.i
+        elif index == 1:
+            return self.j
+        elif index == 2:
+            return self.k
+        else:
+            raise Exception
 
-    #     @return the value for the requested component
+    def getLength(self):
+        """Compute the standard L-2 norm, or length, of the vector.
 
-    #     @throws IndexOutOfBoundsException if an invalid index, outside the
-    #     range [0,2], is specified.
-    #     """
-    #     if index == 0:
-    #         return self.i
-    #     elif index == 1:
-    #         return self.j
-    #     elif index == 2:
-    #         return self.k
-    #     else:
-    #         raise Exception
+        return float (i*i + j*j + k*k)^(1/2) without danger of overflow.
+        """
+        return computeNorm(self.i, self.j, self.k)
 
-    # def getLength(self) -> float:
-    #     """Computes the standard L-2 norm, or length, of the vector.
+    def getDot(self, vector):
+        """Compute the dot product of this instance with another vector.
 
-    #     @return (i*i + j*j + k*k)^(1/2) without danger of overflow.
-    #     """
-    #     return computeNorm(self.i, self.j, self.k)
+        param UnwritableVectorIJK vector the vector to dot against the
+        instance.
 
-    # def getDot(self, vector) -> float:
-    #     """Compute the dot product of this instance with another vector.
+        return gloat i*vector.i + j*vector.j + k*vector.k
+        """
+        return self.i*vector.i + self.j*vector.j + self.k*vector.k
 
-    #     @param vector the vector to dot against the instance.
+    def getSeparation(self, vector):
+        """Compute the angular separation between this and another vector.
 
-    #     @return i*vector.i + j*vector.j + k*vector.k
-    #     """
-    #     return self.i*vector.i + self.j*vector.j + self.k*vector.k
+        param UnwritableVectorIJK vector
+        return float the angular separation between vector and this instance in
+        radians.
+        throws Exception if either this instance or the supplied vector are
+        zero
+        """
+        thisNorm = self.getLength()
+        vectorNorm = vector.getLength()
+        if thisNorm == 0.0:
+            raise Exception("Unable to compute angular separation. " +
+                            "This vector is the zero vector.")
+        elif vectorNorm == 0.0:
+            raise Exception("Unable to compute angular separation. " +
+                            "The argument supplied is the zero vector.")
+        dotProduct = self.getDot(vector)
+        if dotProduct > 0:
+            x = self.i/thisNorm - vector.i/vectorNorm
+            y = self.j/thisNorm - vector.j/vectorNorm
+            z = self.k/thisNorm - vector.k/vectorNorm
+            return 2.0*asin(0.5*computeNorm(x, y, z))
+        elif dotProduct < 0:
+            x = self.i/thisNorm + vector.i/vectorNorm
+            y = self.j/thisNorm + vector.j/vectorNorm
+            z = self.k/thisNorm + vector.k/vectorNorm
+            return pi - 2.0*asin(0.5*computeNorm(x, y, z))
+        return pi/2
 
-    # def getSeparation(self, vector) -> float:
-    #     """Compute the angular separation in radians between this instance and
-    #     another vector.
+    def getSeparationOutOfPlane(self, normal):
+        """Compute the angle between this and a plane.
 
-    #     @param vector
+        Note: this really is simply
+        FundamentalPhysicalConstants.HALFPI
+        UnwritableVectorIJK.getSeparation(UnwritableVectorIJK)
+        but is useful as a convenience method.
 
-    #     @return the angular separation between vector and this instance in
-    #     radians.
-
-    #     @throws UnsupportedOperationException if either this instance or the
-    #     supplied vector are {@link VectorIJK#ZERO}
-    #     """
-    #     thisNorm = self.getLength()
-    #     vectorNorm = vector.getLength()
-    #     if thisNorm == 0.0:
-    #         raise Exception("Unable to compute angular separation. " +
-    #                         "This vector is the zero vector.")
-    #     elif vectorNorm == 0.0:
-    #         raise Exception("Unable to compute angular separation. " +
-    #                         "The argument supplied is the zero vector.")
-    #     dotProduct = self.getDot(vector)
-    #     if dotProduct > 0:
-    #         x = self.i/thisNorm - vector.i/vectorNorm
-    #         y = self.j/thisNorm - vector.j/vectorNorm
-    #         z = self.k/thisNorm - vector.k/vectorNorm
-    #         return 2.0*asin(0.5*computeNorm(x, y, z))
-    #     elif dotProduct < 0:
-    #         x = self.i/thisNorm + vector.i/vectorNorm
-    #         y = self.j/thisNorm + vector.j/vectorNorm
-    #         z = self.k/thisNorm + vector.k/vectorNorm
-    #         return pi - 2.0*asin(0.5*computeNorm(x, y, z))
-    #     return pi/2
-
-    # def getSeparationOutOfPlane(self, normal) -> float:
-    #     """Compute the angle this instance lies out of the plane specified by
-    #     normal.
-
-    #     Note: this really is simply
-    #     {@link FundamentalPhysicalConstants#HALFPI} -
-    #     {@link UnwritableVectorIJK#getSeparation(UnwritableVectorIJK)},
-    #     but is useful as a convenience method.
-
-    #     @param normal the normal to the plane
-
-    #     @return the angular separation of this vector and the plane with the
-    #     specified normal. Positive values lie on the same side as normal,
-    #     negative on the other.
-    #     """
-    #     return pi/2 - self.getSeparation(normal)
+        param UnwritableVectorIJK normal the normal to the plane
+        return float the angular separation of this vector and the plane with
+        the specified normal. Positive values lie on the same side as normal,
+        negative on the other.
+        """
+        return pi/2 - self.getSeparation(normal)
 
     @staticmethod
     def copyOf(vector):
-        """Makes an unwritable copy of the supplied vector.
+        """Make an unwritable copy of the supplied vector.
 
         This method makes an unwritable copy only if necessary. It tries to
         avoid making a copy wherever possible.
 
-        @param vector a vector to copy.
+        param UnwritableVectorIJK vector a vector to copy.
 
-        @return either a reference to vector (if vector is already only an
-        instance of {@link UnwritableVectorIJK}, otherwise an unwritable copy
-        of vector's contents
+        return UnwritableVectorIJK either a reference to vector (if vector is
+        already only an instance of UnwritableVectorIJK, otherwise an
+        unwritable copy of vector's contents
         """
         if isinstance(vector, UnwritableVectorIJK):
             return vector
         return UnwritableVectorIJK(vector)
 
-    # def hashCode(self) -> int:
-    #     """Compute the hash code."""
-    #     prime = 31
-    #     result = 1
-    #     temp = Double.doubleToLongBits(self.i)
-    #     result = prime*result + temp ^ (temp >> 32)
-    #     temp = Double.doubleToLongBits(self.j)
-    #     result = prime*result + temp ^ (temp >> 32)
-    #     temp = Double.doubleToLongBits(self.k)
-    #     result = prime*result + temp ^ (temp >> 32)
-    #     return result
+    def hashCode(self):
+        """Compute the hash code."""
+        prime = 31
+        result = 1
+        temp = Double.doubleToLongBits(self.i)
+        result = prime*result + temp ^ (temp >> 32)
+        temp = Double.doubleToLongBits(self.j)
+        result = prime*result + temp ^ (temp >> 32)
+        temp = Double.doubleToLongBits(self.k)
+        result = prime*result + temp ^ (temp >> 32)
+        return result
 
-    # def equals(self, obj) -> bool:
-    #     if self is obj:
-    #         return True
-    #     if obj is None:
-    #         return False
-    #     if not isinstance(obj, UnwritableVectorIJK):
-    #         return False
-    #     if self.i != obj.i:
-    #         return False
-    #     if self.j != obj.j:
-    #         return False
-    #     if self.k != obj.k:
-    #         return False
-    #     return True
+    def equals(self, obj):
+        """Check for equality with another UnwritableVectorIJK."""
+        if self is obj:
+            return True
+        if obj is None:
+            return False
+        if not isinstance(obj, UnwritableVectorIJK):
+            return False
+        if self.i != obj.i:
+            return False
+        if self.j != obj.j:
+            return False
+        if self.k != obj.k:
+            return False
+        return True
 
     def toString(self):
+        """Create a string from the object."""
         return "[%s,%s,%s]" % (self.i, self.j, self.k)

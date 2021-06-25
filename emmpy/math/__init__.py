5a30c1a1fd5341d63c650fdc3b657fa70347b2bc
Eric Winter <eric.winter@jhuapl.edu>
2021-06-25 16:32:54 -0400

    Docstrings

--- emmpy/math/__init__.py
+++ emmpy/math/__init__.py
@@ -1,5 +1,9 @@
 """General math classes.
 
+These packages provide a variety of general-purpose math classes. These
+classes are built on top of Numpy, and so objects created from these
+classes should be usable wherever a Numpy ndarray object is usable.
+
 Modules
 -------
 
--- emmpy/math/coordinates/__init__.py
+++ emmpy/math/coordinates/__init__.py
@@ -1,5 +1,10 @@
 """Standard coordinate system-specific classes.
 
+These modules provide a variety of coordinate-system-specific vector
+classes. These classes are built on top of Numpy, and so objects created
+from these classes should be usable wherever a Numpy ndarray object is
+usable.
+
 Modules
 -------
 cartesianvector2d.py

65d08f515d55ba962e148dc5cf617102f3d526f6
Eric Winter <eric.winter@jhuapl.edu>
2021-07-08 13:26:33 -0400

    Added test code.

--- emmpy/math/matrices/tests/test_matrix3d.py
+++ emmpy/math/matrices/tests/test_matrix3d.py
@@ -19,24 +19,59 @@ class TestBuilder(unittest.TestCase):
     def test___new__(self):
         """Test the __new__ method."""
         # 0-arg form
-        m1 = Matrix3D()
-        self.assertIsInstance(m1, Matrix3D)
-        for i in range(3):
-            for j in  range(3):
-                self.assertTrue(np.isnan(m1[i, j]))
-        # 9-argument form - individual components
-        data1 = [i*1.1 for i in range(9)]
-        a1 = np.array(data1).reshape((3, 3))
-        m1 = Matrix3D(*data1)
-        self.assertIsInstance(m1, Matrix3D)
+        m = Matrix3D()
+        self.assertIsInstance(m, Matrix3D)
+        for x in m.flatten():
+            self.assertTrue(np.isnan(x))
+        # 1-arg forms.
+        # list of lists
+        data = [[0.0, 1.1, 2.2], [3.3, 4.4, 5.5], [6.6, 7.7, 8.8]]
+        m = Matrix3D(data)
+        self.assertIsInstance(m, Matrix3D)
         for row in range(3):
             for col in range(3):
-                self.assertAlmostEqual(m1[row, col], a1[row, col])
+                self.assertAlmostEqual(m[row, col], data[row][col])
+        # tuple of tuples
+        data = ((0.0, 1.1, 2.2), (3.3, 4.4, 5.5), (6.6, 7.7, 8.8))
+        m = Matrix3D(data)
+        self.assertIsInstance(m, Matrix3D)
+        for row in range(3):
+            for col in range(3):
+                self.assertAlmostEqual(m[row, col], data[row][col])
+        # list of tuples
+        data = [(0.0, 1.1, 2.2), (3.3, 4.4, 5.5), (6.6, 7.7, 8.8)]
+        m = Matrix3D(data)
+        self.assertIsInstance(m, Matrix3D)
+        for row in range(3):
+            for col in range(3):
+                self.assertAlmostEqual(m[row, col], data[row][col])
+        # tuple of lists
+        data = ([0.0, 1.1, 2.2], [3.3, 4.4, 5.5], [6.6, 7.7, 8.8])
+        m = Matrix3D(data)
+        self.assertIsInstance(m, Matrix3D)
+        for row in range(3):
+            for col in range(3):
+                self.assertAlmostEqual(m[row, col], data[row][col])
+        # ndarray
+        data = [i*1.1 for i in range(9)]
+        a = np.array(data).reshape((3, 3))
+        m = Matrix3D(*data)
+        self.assertIsInstance(m, Matrix3D)
+        for row in range(3):
+            for col in range(3):
+                self.assertAlmostEqual(m[row, col], a[row, col])
+        # 9-argument form - individual components
+        data = [i*1.1 for i in range(9)]
+        m = Matrix3D(*data)
+        self.assertIsInstance(m, Matrix3D)
+        for row in range(3):
+            for col in range(3):
+                self.assertAlmostEqual(m[row, col], data[row*3 + col])
         # Invalid forms.
         for n in (2, 3, 4, 5, 6, 7, 8, 10):
             with self.assertRaises(ValueError):
-                data1 = [None]*n
-                m1 = Matrix3D(*data1)
+                args = (None,)*n
+                m = Matrix3D(*args)
 
 if __name__ == '__main__':
     unittest.main()

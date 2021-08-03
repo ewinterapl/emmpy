77f5dbd44adfcd88326a55652a5119bdba319837
Eric Winter <eric.winter@jhuapl.edu>
2021-08-03 13:33:38 -0400

    Replaced get*() methods with direct attribute reference.

--- emmpy/crucible/core/math/vectorspace/matrixijk.py
+++ emmpy/crucible/core/math/vectorspace/matrixijk.py
@@ -1030,34 +1030,34 @@ class MatrixIJK(Matrix3D):
         """
         return np.linalg.det(self)
 
-    def getII(self):
-        """Get the ith row, ith column component."""
-        return self.ii
+    # def getII(self):
+    #     """Get the ith row, ith column component."""
+    #     return self.ii
 
-    def getJI(self):
-        """Get the jth row, ith column component."""
-        return self.ji
+    # def getJI(self):
+    #     """Get the jth row, ith column component."""
+    #     return self.ji
 
-    def getKI(self):
-        """Get the kth row, ith column component."""
-        return self.ki
+    # def getKI(self):
+    #     """Get the kth row, ith column component."""
+    #     return self.ki
 
-    def getIJ(self):
-        """Get the ith row, jth column component."""
-        return self.ij
+    # def getIJ(self):
+    #     """Get the ith row, jth column component."""
+    #     return self.ij
 
-    def getJJ(self):
-        """Get the jth row, jth column component."""
-        return self.jj
+    # def getJJ(self):
+    #     """Get the jth row, jth column component."""
+    #     return self.jj
 
-    def getKJ(self):
-        """Get the kth row, jth column component."""
-        return self.kj
+    # def getKJ(self):
+    #     """Get the kth row, jth column component."""
+    #     return self.kj
 
-    def getIK(self):
-        """Get the ith row, kth column component."""
-        return self.ik
+    # def getIK(self):
+    #     """Get the ith row, kth column component."""
+    #     return self.ik
 
-    def getJK(self):
-        """Get the jth row, kth column component."""
-        return self.jk
+    # def getJK(self):
+    #     """Get the jth row, kth column component."""
+    #     return self.jk

bbc9de389af09f0d4c28b88ae00b95988be9ef1b
Eric Winter <eric.winter@jhuapl.edu>
2021-08-04 15:21:11 -0400

    Removed unused code from RotationMatrixIJK.

--- emmpy/crucible/core/math/vectorspace/rotationmatrixijk.py
+++ emmpy/crucible/core/math/vectorspace/rotationmatrixijk.py
@@ -1,166 +1,71 @@
-"""A 3-D rotation matrix."""
+"""A 3-D rotation matrix in Cartesian (i, j, k) coordinates.
+
+A 3-D rotation matrix in Cartesian (i, j, k) coordinates.
+
+Authors
+-------
+F.S. Turner
+Eric Winter (eric.winter@jhuapl.edu)
+"""
 
 
+from emmpy.crucible.core.math.vectorspace.internaloperations import (
+    checkRotation
+)
+from emmpy.crucible.core.math.vectorspace.malformedrotationexception import (
+    MalformedRotationException
+)
 from emmpy.crucible.core.math.vectorspace.matrixijk import MatrixIJK
 
 
+# Tolerances for determinant and column norms, relative to unity, for a
+# matrix to be a rotation matrix.
+ROTATION_DETERMINANT_TOLERANCE = 1e-12
+ROTATION_NORM_TOLERANCE = 1e-12
+
+
 class RotationMatrixIJK(MatrixIJK):
-    """A 3-D rotation matrix.
+    """A 3-D rotation matrix in Cartesian (i, j, k) coordinates.
 
-    A writable subclass of the unwritable 3D rotation matrix parent
-    completing another link the weak-immutability design pattern.
-
-    This class contains the mutator methods necessary to set or alter the
-    internals of the parent classes fields. Wherever these mutations may alter
-    the matrix to become one that no longer qualifies as a rotation, the
-    methods on this class validate the resultant content. This is accomplished
-    through the fact that each setTo method invokes one particular setTo
-    method:
-    {@link RotationMatrixIJK#setTo(double, double, double, double, double,
-                                   double, double, double, double)}
-    The validation code is confined to this method, allowing subclasses to
-    override a single method on this class to remove the check code.
-
-    Similarly each constructor, save the copy constructor and default
-    constructor, validate input content to ensure that it is a rotation matrix.
-    Validation is consistent with the {@link UnwritableMatrixIJK#isRotation()}
-    method.
-
-    @author F.S.Turner
+    Whenever this matrix changes, it is validated as a rotation matrix. If
+    no longer a valid rotation matrix, a MalformedRotationException is
+    raised.
     """
 
     def __init__(self, *args):
-        """Build a new object."""
+        """Initialize a new RotationMatrixIJK object.
+
+        Initialize a new RotationMatrixIJK object.
+
+        Parameters
+        ----------
+        a : 3x3 array-like of float, optional, default 3x3 identity matrix
+            Values for matrix elements.
+        OR
+        ii, ji, ki, ij, jj, kj, ik, jk, kk : float
+            Elements of new matrix in column-major order.
+
+        Raises
+        ------
+        MalformedRotationException
+            If the matrix is not a valid rotation matrix.
+        ValueError
+            If incorrect arguments are provided.
+        """
         if len(args) == 0:
-            # Creates a rotation matrix and sets it to the identity.
+            # Creates an identity rotation matrix.
             data = (1, 0, 0, 0, 1, 0, 0, 0, 1)
-            self.__init__(*data)
+            MatrixIJK.__init__(self, *data)
         elif len(args) == 1:
-            if isinstance(args[0], list):
-                # Constructs a matrix from the upper three by three block of a
-                # two dimensional array of doubles.
-                # @param data the array of doubles
-                # @throws IndexOutOfBoundsException if the supplied data array
-                # does not contain at least three arrays of arrays of length
-                # three or greater.
-                # @throws IllegalArgumentException if either the columns of the
-                # supplied matrix have norms that are not within
-                # {@link UnwritableMatrixIJK#NORM_TOLERANCE} or if the
-                # determinant is not within
-                # {@link UnwritableMatrixIJK#DETERMINANT_TOLERANCE}.
-                (data,) = args
-                self.__init__(data[0][0], data[1][0], data[2][0],
-                              data[0][1], data[1][1], data[2][1],
-                              data[0][2], data[1][2], data[2][2])
-            elif isinstance(args[0], RotationMatrixIJK):
-                # Copy constructor, creates a matrix by copying the values of a
-                # pre-existing instance of the parent unwritable matrix class.
-                # @param matrix the matrix whose contents are to be copied.
-                (m,) = args
-                self.__init__(m.ii, m.ji, m.ki,
-                              m.ij, m.jj, m.kj,
-                              m.ik, m.jk, m.kk)
-            elif isinstance(args[0], MatrixIJK):
-                # Copy constructor, of sorts, creates a matrix by copying the
-                # values of a pre-existing matrix.
-                # Note: This constructor performs no validation on the input by
-                # design.
-                # @param matrix the rotation matrix whose contents are to be
-                # copied.
-                # @throws IllegalArgumentException if either the columns of the
-                # supplied matrix have norms that are not within
-                # {@link UnwritableMatrixIJK#NORM_TOLERANCE} or if the
-                # determinant is not within
-                # {@link UnwritableMatrixIJK#DETERMINANT_TOLERANCE}.
-                (m,) = args
-                self.__init__(m.ii, m.ji, m.ki,
-                              m.ij, m.jj, m.kj,
-                              m.ik, m.jk, m.kk)
-        elif len(args) == 2:
-            # Scaling constructor, creates a new matrix by applying a scalar
-            # multiple to the components of a pre-existing matrix.
-            # @param scale the scale factor to apply
-            # @param matrix the matrix whose components are to be scaled and
-            # copied.
-            # @throws IllegalArgumentException if either the columns of the
-            # supplied matrix have norms that are not within
-            # {@link UnwritableMatrixIJK#NORM_TOLERANCE} or if the determinant
-            # is not within {@link UnwritableMatrixIJK#DETERMINANT_TOLERANCE}.
-            (s, m) = args
-            self.__init__(s*m.ii, s*m.ji, s*m.ki,
-                          s*m.ij, s*m.jj, s*m.kj,
-                          s*m.ik, s*m.jk, s*m.kk)
-        elif len(args) == 3:
-            # Column vector constructor, creates a new matrix by populating the
-            # columns of the rotation matrix with the supplied vectors.
-            # @param ithColumn the vector containing the ith column
-            # @param jthColumn the vector containing the jth column
-            # @param kthColumn the vector containing the kth column
-            # @throws IllegalArgumentException if either the columns of the
-            # supplied matrix have norms that are not within
-            # {@link UnwritableMatrixIJK#NORM_TOLERANCE} or if the determinant
-            # is not within {@link UnwritableMatrixIJK#DETERMINANT_TOLERANCE}.
-            (colI, colJ, colK) = args
-            self.__init__(colI.i, colI.j, colI.k,
-                          colJ.i, colJ.j, colJ.k,
-                          colK.i, colK.j, colK.k)
-        elif len(args) == 4:
-            # Column scaling constructor, creates a new rotation matrix by
-            # applying scalar multiples to the columns of a pre-existing
-            # matrix.
-            # @param scaleI scale factor to apply to the ith column
-            # @param scaleJ scale factor to apply to the jth column
-            # @param scaleK scale factor to apply to the kth column
-            # @param matrix the matrix whose components are to be scaled and
-            # copied
-            # @throws IllegalArgumentException if either the columns of the
-            # supplied matrix have norms that are not within
-            # {@link UnwritableMatrixIJK#NORM_TOLERANCE} or if the determinant
-            # is not within {@link UnwritableMatrixIJK#DETERMINANT_TOLERANCE}.
-            (sI, sJ, sK, m) = args
-            self.__init__(sI*m.ii, sI*m.ji, sI*m.ki,
-                          sJ*m.ij, sJ*m.jj, sJ*m.kj,
-                          sK*m.ik, sK*m.jk, sK*m.kk)
-        elif len(args) == 6:
-            # Scaled column vector constructor, creates a new rotation matrix
-            # by populating the columns of the matrix with scaled versions of
-            # the supplied vectors
-            # @param scaleI the scale factor to apply to the ith column
-            # @param ithColumn the vector containing the ith column
-            # @param scaleJ the scale factor to apply to the jth column
-            # @param jthColumn the vector containing the jth column
-            # @param scaleK the scale factor to apply to the kth column
-            # @param kthColumn the vector containing the kth column
-            # @throws IllegalArgumentException if either the columns of the
-            # supplied matrix have norms that are not within
-            # {@link UnwritableMatrixIJK#NORM_TOLERANCE} or if the determinant
-            # is not within {@link UnwritableMatrixIJK#DETERMINANT_TOLERANCE}.
-            (sI, colI, sJ, colJ, sK, colK) = args
-            self.__init__(sI*colI.i, sI*colI.j, sI*colI.k,
-                          sJ*colJ.i, sJ*colJ.j, sJ*colJ.k,
-                          sK*colK.i, sK*colK.j, sK*colK.k)
+            # Initialize matrix from a 3x3 array-like of floats.
+            (a,) = args
+            MatrixIJK.__init__(self, a)
         elif len(args) == 9:
-            # Constructs a rotation matrix from the nine basic components.
-            # @param ii ith row, ith column element
-            # @param ji jth row, ith column element
-            # @param ki kth row, ith column element
-            # @param ij ith row, jth column element
-            # @param jj jth row, jth column element
-            # @param kj kth row, jth column element
-            # @param ik ith row, kth column element
-            # @param jk jth row, kth column element
-            # @param kk kth row, kth column element
-            # @throws IllegalArgumentException if either the columns of the
-            # supplied matrix have norms that are not within
-            # {@link UnwritableMatrixIJK#NORM_TOLERANCE} or if the determinant
-            # is not within {@link UnwritableMatrixIJK#DETERMINANT_TOLERANCE}.
+            # Matrix elements in column-major order.
             (ii, ji, ki, ij, jj, kj, ik, jk, kk) = args
-            self.ii = ii
-            self.ji = ji
-            self.ki = ki
-            self.ij = ij
-            self.jj = jj
-            self.kj = kj
-            self.ik = ik
-            self.jk = jk
-            self.kk = kk
+            data = [ii, ji, ki, ij, jj, jk, ik, jk, kk]
+            MatrixIJK.__init__(self, *data)
+        else:
+            raise ValueError
+        checkRotation(self, ROTATION_NORM_TOLERANCE,
+                      ROTATION_DETERMINANT_TOLERANCE)
--- emmpy/crucible/core/math/vectorspace/tests/test_rotationmatrixijk.py
+++ emmpy/crucible/core/math/vectorspace/tests/test_rotationmatrixijk.py
@@ -4,6 +4,7 @@
 from math import cos, sin
 import unittest
 
+import numpy as np
 
 from emmpy.crucible.core.math.vectorspace.matrixijk import MatrixIJK
 from emmpy.crucible.core.math.vectorspace.rotationmatrixijk import (
@@ -17,127 +18,46 @@ class TestBuilder(unittest.TestCase):
 
     def test___init__(self):
         """Test the __init__ method."""
-        a = 1
-        # 0 args
-        m = RotationMatrixIJK()
-        self.assertAlmostEqual(m.ii, 1)
-        self.assertAlmostEqual(m.ji, 0)
-        self.assertAlmostEqual(m.ki, 0)
-        self.assertAlmostEqual(m.ij, 0)
-        self.assertAlmostEqual(m.jj, 1)
-        self.assertAlmostEqual(m.kj, 0)
-        self.assertAlmostEqual(m.ik, 0)
-        self.assertAlmostEqual(m.jk, 0)
-        self.assertAlmostEqual(m.kk, 1)
-        # 1 2-D list arg
-        m = RotationMatrixIJK([[cos(a), 0, -sin(a), 99],
-                               [0, 1, 0, 99],
-                               [sin(a), 0, cos(a), 99],
-                               [99, 99, 99, 99]])
-        self.assertAlmostEqual(m.ii, cos(a))
-        self.assertAlmostEqual(m.ji, 0)
-        self.assertAlmostEqual(m.ki, sin(a))
-        self.assertAlmostEqual(m.ij, 0)
-        self.assertAlmostEqual(m.jj, 1)
-        self.assertAlmostEqual(m.kj, 0)
-        self.assertAlmostEqual(m.ik, -sin(a))
-        self.assertAlmostEqual(m.jk, 0)
-        self.assertAlmostEqual(m.kk, cos(a))
-        # 1 RotationMatrixIJK arg
-        m2 = RotationMatrixIJK(m)
-        self.assertAlmostEqual(m2.ii, cos(a))
-        self.assertAlmostEqual(m2.ji, 0)
-        self.assertAlmostEqual(m2.ki, sin(a))
-        self.assertAlmostEqual(m2.ij, 0)
-        self.assertAlmostEqual(m2.jj, 1)
-        self.assertAlmostEqual(m2.kj, 0)
-        self.assertAlmostEqual(m2.ik, -sin(a))
-        self.assertAlmostEqual(m2.jk, 0)
-        self.assertAlmostEqual(m2.kk, cos(a))
-        # 1 UnwritbleMatrixIJK arg
-        a = 2
-        m = MatrixIJK(cos(a), 0, sin(a),
-                      0, 1, 0,
-                      -sin(a), 0, cos(a))
-        m2 = RotationMatrixIJK(m)
-        self.assertAlmostEqual(m2.ii, cos(a))
-        self.assertAlmostEqual(m2.ji, 0)
-        self.assertAlmostEqual(m2.ki, sin(a))
-        self.assertAlmostEqual(m2.ij, 0)
-        self.assertAlmostEqual(m2.jj, 1)
-        self.assertAlmostEqual(m2.kj, 0)
-        self.assertAlmostEqual(m2.ik, -sin(a))
-        self.assertAlmostEqual(m2.jk, 0)
-        self.assertAlmostEqual(m2.kk, cos(a))
-        # 2 args: scale and matrix
-        m = RotationMatrixIJK(cos(a), 0, sin(a),
-                              0, 1, 0,
-                              -sin(a), 0, cos(a))
-        m2 = RotationMatrixIJK(1, m)
-        self.assertAlmostEqual(m2.ii, cos(a))
-        self.assertAlmostEqual(m2.ji, 0)
-        self.assertAlmostEqual(m2.ki, sin(a))
-        self.assertAlmostEqual(m2.ij, 0)
-        self.assertAlmostEqual(m2.jj, 1)
-        self.assertAlmostEqual(m2.kj, 0)
-        self.assertAlmostEqual(m2.ik, -sin(a))
-        self.assertAlmostEqual(m2.jk, 0)
-        self.assertAlmostEqual(m2.kk, cos(a))
-        # 3 args: all vectors
-        v1 = VectorIJK(cos(a), 0, sin(a))
-        v2 = VectorIJK(0, 1, 0)
-        v3 = VectorIJK(-sin(a), 0, cos(a))
-        m = RotationMatrixIJK(v1, v2, v3)
-        self.assertAlmostEqual(m.ii, cos(a))
-        self.assertAlmostEqual(m.ji, 0)
-        self.assertAlmostEqual(m.ki, sin(a))
-        self.assertAlmostEqual(m.ij, 0)
-        self.assertAlmostEqual(m.jj, 1)
-        self.assertAlmostEqual(m.kj, 0)
-        self.assertAlmostEqual(m.ik, -sin(a))
-        self.assertAlmostEqual(m.jk, 0)
-        self.assertAlmostEqual(m.kk, cos(a))
-        # 4 args: 3 scale factors, matrix
-        m = MatrixIJK(cos(a)/2, 0, sin(a)/2,
-                      0, 1/3, 0,
-                      -sin(a)/4, 0, cos(a)/4)
-        m2 = RotationMatrixIJK(2, 3, 4, m)
-        self.assertAlmostEqual(m2.ii, cos(a))
-        self.assertAlmostEqual(m2.ji, 0)
-        self.assertAlmostEqual(m2.ki, sin(a))
-        self.assertAlmostEqual(m2.ij, 0)
-        self.assertAlmostEqual(m2.jj, 1)
-        self.assertAlmostEqual(m2.kj, 0)
-        self.assertAlmostEqual(m2.ik, -sin(a))
-        self.assertAlmostEqual(m2.jk, 0)
-        self.assertAlmostEqual(m2.kk, cos(a))
-        # 6 args: 3 x (scale, vector)
-        v1 = VectorIJK(cos(a)/2, 0, sin(a)/2)
-        v2 = VectorIJK(0, 1/3, 0)
-        v3 = VectorIJK(-sin(a)/4, 0, cos(a)/4)
-        m = RotationMatrixIJK(2, v1, 3, v2, 4, v3)
-        self.assertAlmostEqual(m.ii, cos(a))
-        self.assertAlmostEqual(m.ji, 0)
-        self.assertAlmostEqual(m.ki, sin(a))
-        self.assertAlmostEqual(m.ij, 0)
-        self.assertAlmostEqual(m.jj, 1)
-        self.assertAlmostEqual(m.kj, 0)
-        self.assertAlmostEqual(m.ik, -sin(a))
-        self.assertAlmostEqual(m.jk, 0)
-        self.assertAlmostEqual(m.kk, cos(a))
+        # 0 args - identity matrix
+        m1 = RotationMatrixIJK()
+        self.assertIsInstance(m1, RotationMatrixIJK)
+        for row in range(3):
+            for col in range(3):
+                if row == col:
+                    self.assertAlmostEqual(m1[row, col], 1)
+                else:
+                    self.assertAlmostEqual(m1[row, col], 0)
+        # 1 arg forms
+        # list of tuples, for heterogeneity
+        a = 1  # Rotate by a radians about z-axis
+        data1 = [(cos(a), sin(a), 0), (-sin(a), cos(a), 0), (0, 0, 1)]
+        m1 = RotationMatrixIJK(data1)
+        self.assertIsInstance(m1, RotationMatrixIJK)
+        for row in range(3):
+            for col in range(3):
+                self.assertAlmostEqual(m1[row, col], data1[row][col])
+        # np.ndarray
+        a = 1  # Rotate by a radians about z-axis
+        data1 = [(cos(a), sin(a), 0), (-sin(a), cos(a), 0), (0, 0, 1)]
+        a1 = np.array(data1)
+        m1 = RotationMatrixIJK(a1)
+        self.assertIsInstance(m1, RotationMatrixIJK)
+        for row in range(3):
+            for col in range(3):
+                self.assertAlmostEqual(m1[row, col], data1[row][col])
+        # RotationMatrixIJK
+        m2 = RotationMatrixIJK(m1)
+        self.assertIsInstance(m2, RotationMatrixIJK)
+        for row in range(3):
+            for col in range(3):
+                self.assertAlmostEqual(m2[row, col], data1[row][col])
         # 9 args
-        m = RotationMatrixIJK(cos(a), 0, sin(a),
-                              0, 1, 0,
-                              -sin(a), 0, cos(a))
-        self.assertAlmostEqual(m.ii, cos(a))
-        self.assertAlmostEqual(m.ji, 0)
-        self.assertAlmostEqual(m.ki, sin(a))
-        self.assertAlmostEqual(m.ij, 0)
-        self.assertAlmostEqual(m.jj, 1)
-        self.assertAlmostEqual(m.kj, 0)
-        self.assertAlmostEqual(m.ik, -sin(a))
-        self.assertAlmostEqual(m.jk, 0)
-        self.assertAlmostEqual(m.kk, cos(a))
+        # Individual matrix elements in column-major order.
+        flat_data1 = data1[0] + data1[1] + data1[2]
+        m = RotationMatrixIJK(*flat_data1)
+        for row in range(3):
+            for col in range(3):
+                self.assertAlmostEqual(m2[row, col], data1[row][col])
 
 
 if __name__ == '__main__':

3684e6ec04a724150267eeccfa6a960fc19475e0
Eric Winter <eric.winter@jhuapl.edu>
2021-08-05 09:49:53 -0400

    Changed PolarVector to inherit from Vector2D instead of AbstractVectorIJ.

--- emmpy/crucible/core/math/coords/polarvector.py
+++ emmpy/crucible/core/math/coords/polarvector.py
@@ -1,10 +1,10 @@
 """2-D vectors in a polar coordinate system."""
 
 
-from emmpy.crucible.core.math.coords.abstractvectorij import AbstractVectorIJ
+from emmpy.math.vectors.vector2d import Vector2D
 
 
-class PolarVector(AbstractVectorIJ):
+class PolarVector(Vector2D):
     """A class representing a 2-D vector in the polar coordinate system.
 
     author G.K.Stephens
@@ -12,15 +12,24 @@ class PolarVector(AbstractVectorIJ):
 
     def __init__(self, radius, angle):
         """2-D vectors in a polar coordinate system."""
-        AbstractVectorIJ.__init__(self, radius, angle)
+        self[:] = [radius, angle]
 
     def getRadius(self):
         """Return the radius."""
-        return self.getI()
+        return self[0]
 
     def getAngle(self):
         """Return the angle."""
-        return self.getJ()
+        return self[1]
+
+    def getVectorIJ(self):
+        return self
+
+    def getI(self):
+        return self[0]
+
+    def getJ(self):
+        return self[1]
 
 
 # The null vector in polar coordinates.

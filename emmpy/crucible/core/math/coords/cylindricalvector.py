ebe0f1ee2add60d1656684a566679444302ea2c7
Eric Winter <eric.winter@jhuapl.edu>
2021-08-05 10:16:25 -0400

    Removed commented code.

--- emmpy/crucible/core/math/coords/cylindricalvector.py
+++ emmpy/crucible/core/math/coords/cylindricalvector.py
@@ -12,22 +12,18 @@ class CylindricalVector(Vector3D):
 
     def __init__(self, cylindricalRadius, longInRadians, height):
         """Build a new object."""
-        # AbstractVector.__init__(self, cylindricalRadius, longInRadians, height)
         self[:] = [cylindricalRadius, longInRadians, height]
 
     def getCylindricalRadius(self):
         """Return the cylindrical radius (often denoted as r)."""
-        # return AbstractVector.getI(self)
         return self[0]
 
     def getLongitude(self):
         """Return the longitude."""
-        # return AbstractVector.getJ(self)
         return self[1]
 
     def getHeight(self):
         """Return the height (often denoted as z)."""
-        # return AbstractVector.getK(self)
         return self[2]
 
     def getI(self):

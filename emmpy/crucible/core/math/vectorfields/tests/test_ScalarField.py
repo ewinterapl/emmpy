b2cf27001e8c42f8f0e6615d3fa076afe761018b
Eric Winter <eric.winter@jhuapl.edu>
2021-08-05 13:38:38 -0400

    Modified ScalarField to raise AbstractMethodException.

--- emmpy/crucible/core/math/vectorfields/tests/test_ScalarField.py
+++ emmpy/crucible/core/math/vectorfields/tests/test_ScalarField.py
@@ -1,16 +1,23 @@
+"""Tests for the scalarfield module."""
+
+
 import unittest
 
 from emmpy.crucible.core.math.vectorfields.scalarfield import ScalarField
+from emmpy.exceptions.abstractmethodexception import AbstractMethodException
 
 
 class TestBuilder(unittest.TestCase):
+    """Tests for the scalarfield module."""
 
     def test___init__(self):
-        with self.assertRaises(Exception):
+        """Test the __init__ method."""
+        with self.assertRaises(AbstractMethodException):
             ScalarField()
 
     def test_evaluate(self):
-        with self.assertRaises(Exception):
+        """Test the evaluate method."""
+        with self.assertRaises(AbstractMethodException):
             ScalarField.evaluate(None, None)
 
 

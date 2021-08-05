ba726298b1c6f687b663b35a12f7b90eca5a6f27
Eric Winter <eric.winter@jhuapl.edu>
2021-08-05 09:54:47 -0400

    Changed CylindricalVector to inherit from Vector3D instead of AbstractVector.

--- emmpy/crucible/core/math/coords/cylindricalvector.py
+++ emmpy/crucible/core/math/coords/cylindricalvector.py
@@ -1,10 +1,10 @@
 """Represenation of a vector in cylindrical coordinates."""
 
 
-from emmpy.crucible.core.math.coords.abstractvector import AbstractVector
+from emmpy.math.vectors.vector3d import Vector3D
 
 
-class CylindricalVector(AbstractVector):
+class CylindricalVector(Vector3D):
     """A class representing a vector in the cylindrical coordinate system.
 
     @author G.K.Stephens
@@ -12,19 +12,35 @@ class CylindricalVector(AbstractVector):
 
     def __init__(self, cylindricalRadius, longInRadians, height):
         """Build a new object."""
-        AbstractVector.__init__(self, cylindricalRadius, longInRadians, height)
+        # AbstractVector.__init__(self, cylindricalRadius, longInRadians, height)
+        self[:] = [cylindricalRadius, longInRadians, height]
 
     def getCylindricalRadius(self):
         """Return the cylindrical radius (often denoted as r)."""
-        return AbstractVector.getI(self)
+        # return AbstractVector.getI(self)
+        return self[0]
 
     def getLongitude(self):
         """Return the longitude."""
-        return AbstractVector.getJ(self)
+        # return AbstractVector.getJ(self)
+        return self[1]
 
     def getHeight(self):
         """Return the height (often denoted as z)."""
-        return AbstractVector.getK(self)
+        # return AbstractVector.getK(self)
+        return self[2]
+
+    def getI(self):
+        return self[0]
+    
+    def getJ(self):
+        return self[1]
+
+    def getK(self):
+        return self[2]
+
+    def getVectorIJK(self):
+        return self
 
 
 # The ZERO vector.

d10ec71550c7ae98b04b02927368f3d5f747e387
Eric Winter <eric.winter@jhuapl.edu>
2021-08-05 10:02:29 -0400

    Rebuilt LatitudinalVector to inherit from Vector2D instead of AbstractVector.

--- emmpy/crucible/core/math/coords/latitudinalvector.py
+++ emmpy/crucible/core/math/coords/latitudinalvector.py
@@ -1,31 +1,30 @@
 """Represenation of a latitudinal vector."""
 
 
-from emmpy.crucible.core.math.coords.abstractvector import AbstractVector
+from emmpy.math.vectors.vector3d import Vector3D
 
 
-class LatitudinalVector(AbstractVector):
+class LatitudinalVector(Vector3D):
     """A class representing a vector in the latitudinal coordinate system.
 
     @author G.K.Stephens
     """
 
-    def __init__(self, *args):
+    def __init__(self, radius, latInRadians, longInRadians):
         """Build a new object."""
-        (radius, latInRadians, longInRadians) = args
-        AbstractVector.__init__(self, radius, latInRadians, longInRadians)
+        self[:] = [radius, latInRadians, longInRadians]
 
     def getRadius(self):
         """Return the radius."""
-        return AbstractVector.getI(self)
+        return self[0]
 
     def getLatitude(self):
         """Return the latitude."""
-        return AbstractVector.getJ(self)
+        return self[1]
 
     def getLongitude(self):
         """Return the longitude."""
-        return AbstractVector.getK(self)
+        return self[2]
 
 
 # The ZERO vector.

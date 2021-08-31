3820327b7f0a8ae65bfe110c6b6307b21e0e8d5d
Eric Winter <eric.winter@jhuapl.edu>
2021-08-30 20:02:12 -0400

    Fixed exception type in tests.

--- emmpy/crucible/core/math/coords/tests/test_Transformation.py
+++ emmpy/crucible/core/math/coords/tests/test_Transformation.py
@@ -4,6 +4,7 @@
 import unittest
 
 from emmpy.crucible.core.math.coords.transformation import Transformation
+from emmpy.exceptions.abstractmethodexception import AbstractMethodException
 
 
 class TestBuilder(unittest.TestCase):
@@ -11,22 +12,22 @@ class TestBuilder(unittest.TestCase):
 
     def test___init__(self):
         """Test the __init__ method."""
-        with self.assertRaises(Exception):
+        with self.assertRaises(AbstractMethodException):
             Transformation()
 
     def test_getTransformation(self):
         """Test the getTransformation method."""
-        with self.assertRaises(Exception):
+        with self.assertRaises(AbstractMethodException):
             Transformation.getTransformation(None, None, None)
 
     def test_getInverseTransformation(self):
         """Test the getInverseTransformation method."""
-        with self.assertRaises(Exception):
+        with self.assertRaises(AbstractMethodException):
             Transformation.getInverseTransformation(None, None, None)
 
     def test_mxv(self):
         """Test the mxv method."""
-        with self.assertRaises(Exception):
+        with self.assertRaises(AbstractMethodException):
             Transformation.mxv(None, None, None)
 
 

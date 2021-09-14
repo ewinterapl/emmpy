9b402dac4018071120f730aca8fac25e749e6567
Eric Winter <eric.winter@jhuapl.edu>
2021-09-14 13:10:37 -0400

    Docstrings

--- emmpy/magmodel/core/math/expansions/arraycoefficientexpansion1d.py
+++ emmpy/magmodel/core/math/expansions/arraycoefficientexpansion1d.py
@@ -1,4 +1,12 @@
-"""A 1-D arrsy for a coefficient expansion."""
+"""A 1-D array for a coefficient expansion.
+
+A 1-D array for a coefficient expansion.
+
+Authors
+-------
+G.K. Stephens
+Eric Winter (eric.winter@jhuapl.edu)
+"""
 
 
 from emmpy.magmodel.core.math.expansions.coefficientexpansion1d import (
@@ -7,25 +15,64 @@ from emmpy.magmodel.core.math.expansions.coefficientexpansion1d import (
 
 
 class ArrayCoefficientExpansion1D(CoefficientExpansion1D):
-    """A 1-D arrsy for a coefficient expansion.
+    """A 1-D array for a coefficient expansion.
 
-    author G.K.Stephens
+    A 1-D array for a coefficient expansion.
     """
 
     def __init__(self, array, firstExpansionNumber):
-        """Build a new object."""
+        """Initialize a new ArrayCoefficientExpansion1D object.
+
+        Initialize a new ArrayCoefficientExpansion1D object.
+
+        Parameters
+        ----------
+        array : list of double
+            List of expansion coefficients.
+        firstExpansionNumber : int
+            Index of first expansion coefficient.
+        """
         self.array = array
         self.firstExpansionNumber = firstExpansionNumber
 
     def getLowerBoundIndex(self):
-        """Return the lower index bound for the expansion."""
+        """Return the lower index bound for the expansion.
+        
+        Return the lower index bound for the expansion.
+
+        Returns
+        -------
+        self.firstExpansionNumber : int
+            Index of first expansion coefficient.
+        """
         return self.firstExpansionNumber
 
     def getUpperBoundIndex(self):
-        """Return the upper index bound for the expansion."""
+        """Return the upper index bound for the expansion.
+        
+        Return the upper index bound for the expansion.
+
+        Returns
+        -------
+        lastExpansionNumber : int
+            Index of last expansion coefficient.
+        """
         lastExpansionNumber = self.firstExpansionNumber + len(self.array) - 1
         return lastExpansionNumber
 
     def getCoefficient(self, index):
-        """Return a coeeficient at the index."""
+        """Return the coefficient at the specified index.
+
+        Return the coefficient at the specified index.
+
+        Parameters
+        ----------
+        index : int
+            Index of coefficient to return.
+        
+        Returns
+        -------
+        result : float
+            Desired expansion coefficient.
+        """
         return self.array[index - self.firstExpansionNumber]

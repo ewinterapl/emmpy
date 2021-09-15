0109b371f4a3fa75f745eb585c65367be15edde4
Eric Winter <eric.winter@jhuapl.edu>
2021-09-15 14:15:58 -0400

    Docstrings.

--- emmpy/magmodel/core/math/expansions/arrayexpansion1d.py
+++ emmpy/magmodel/core/math/expansions/arrayexpansion1d.py
@@ -1,14 +1,43 @@
-"""A 1-D array of expansion values."""
+"""A 1-D array of expansion values.
+
+A 1-D array of expansion values.
+
+Authors
+-------
+Eric Winter (eric.winter@jhuapl.edu)
+"""
 
 
 from emmpy.magmodel.core.math.expansions.expansion1d import Expansion1D
 
 
 class ArrayExpansion1D(Expansion1D):
-    """A 1-D array of expansion values."""
+    """A 1-D array of expansion values.
+
+    A 1-D array of expansion values.
+
+    Attributes
+    ----------
+    array : list of float
+        Expansion coefficiens.
+    firstRadialExpansionNumber : int
+        Index of first coefficient in expansion.
+    lastRadialExpansionNumber : int
+        Index of last coefficient in expansion.
+    """
 
     def __init__(self, array, firstRadialExpansionNumber):
-        """Build a new object."""
+        """Initialize a new ArrayEpansion1D object.
+
+        Initialize a new ArrayEpansion1D object.
+
+        Parameters
+        ----------
+        array : list of float
+            Expansion coefficiens.
+        firstRadialExpansionNumber : int
+            Index of first coefficient in expansion.
+        """
         self.array = array
         self.firstRadialExpansionNumber = firstRadialExpansionNumber
         self.lastRadialExpansionNumber = (
@@ -16,13 +45,50 @@ class ArrayExpansion1D(Expansion1D):
         )
 
     def getLowerBoundIndex(self):
-        """Return the lowest index."""
+        """Return the lowest expansion coefficient index.
+
+        Return the lowest expansion coefficient index.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        self.firstRadialExpansionNumber : int
+            Index of first expansion coefficient.
+        """
         return self.firstRadialExpansionNumber
 
     def getUpperBoundIndex(self):
-        """Returnn the highest index."""
+        """Return the highest expansion coefficient index.
+
+        Return the highest expansion coefficient index.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        self.lastRadialExpansionNumber : int
+            Index of last expansion coefficient.
+        """
         return self.lastRadialExpansionNumber
 
     def getExpansion(self, radialExpansion):
-        """Return the specified expansion."""
+        """Return the the specified expansion coefficient.
+        
+        Return the the specified expansion coefficient.
+
+        Parameters
+        ----------
+        radialExpansion : int
+            Index of expansion coefficient to retrieve.
+
+        Returns
+        -------
+        result : float
+            Value of expansion coefficient at specified index.
+        """
         return self.array[radialExpansion - self.firstRadialExpansionNumber]

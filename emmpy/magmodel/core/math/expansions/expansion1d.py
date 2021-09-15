c6734e456588d458efd44255edd82c6f49a47456
Eric Winter <eric.winter@jhuapl.edu>
2021-09-15 15:17:40 -0400

    Docstrings.

--- emmpy/magmodel/core/math/expansions/expansion1d.py
+++ emmpy/magmodel/core/math/expansions/expansion1d.py
@@ -1,39 +1,22 @@
-"""Interface for a 1-D expansion."""
+"""Interface for a 1-D expansion.
+
+Interface for a 1-D expansion.
+
+Authors
+-------
+G.K. Stephens
+Eric Winter (eric.winter@jhuapl.edu)
+"""
 
 
 class Expansion1D:
     """Interface for a 1-D expansion.
 
-    An interface representing an arbitrary series expansion, that starts at
-    a lower bound index (L) and ends at an upper bound index (U), where T is
-    any Object.
+    An interface representing an arbitrary series expansion, that starts
+    at a lower bound index (L) and ends at an upper bound index (U), where
+    T is any Object.
 
-    This is similar to an array or List of T, but with a non-zero
-    starting index. If the object is a Double, the interface
-    CoefficientExpansion1D can be used instead to avoid autoboxing.
-
-    author G.K.Stephens
-    param <T> some arbitrary object that is represented in the expansion
-    (common examples would include math objects like Vectors VectorIJK
+    This is similar to an array or List of T, but with a non-zero starting
+    index. If the object is a Double, the interface CoefficientExpansion1D
+    can be used instead to avoid autoboxing.
     """
-
-    # def size(self):
-    #     """@return the number of elements of the expansion"""
-    #     size = self.getUpperBoundIndex() - self.getLowerBoundIndex() + 1
-    #     return size
-
-    # def getLowerBoundIndex(self):
-    #     """@return the lower bound index of the expansion (L)"""
-    #     raise Exception
-
-    # def getUpperBoundIndex(self):
-    #     """@return the upper bound index of the expansion (U)"""
-    #     raise Exception
-
-    # def getExpansion(self):
-    #     """gets ith expansion T<sub>i</sub> in &#8721; T<sub>i</sub>
-
-    #     @param index the index i
-    #     @return the ith expansion corresponding to the index T<sub>i</sub>
-    #     """
-    #     raise Exception

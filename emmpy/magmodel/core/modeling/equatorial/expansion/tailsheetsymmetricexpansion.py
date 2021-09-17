bc14d4189f68cfb79ca589ef328e7ce80fb80239
Eric Winter <eric.winter@jhuapl.edu>
2021-09-17 16:25:23 -0400

    Docstrings

--- emmpy/magmodel/core/modeling/equatorial/expansion/tailsheetsymmetricexpansion.py
+++ emmpy/magmodel/core/modeling/equatorial/expansion/tailsheetsymmetricexpansion.py
@@ -1,13 +1,21 @@
-"""Symmetric tail current sheet expansion."""
+"""Symmetric tail current sheet expansion.
+
+Symmetric tail current sheet expansion.
+
+Authors
+-------
+G.K. Stephens
+Eric Winter (eric.winter@jhuapl.edu)
+"""
 
 
 from math import exp, sqrt
 
 import scipy.special as sps
 
+from emmpy.crucible.core.math.vectorfields.vectorfield import VectorField
 from emmpy.crucible.core.math.vectorspace.vectorij import VectorIJ
 from emmpy.crucible.core.math.vectorspace.vectorijk import VectorIJK
-from emmpy.crucible.core.math.vectorfields.vectorfield import VectorField
 
 
 class TailSheetSymmetricExpansion(VectorField):
@@ -17,29 +25,62 @@ class TailSheetSymmetricExpansion(VectorField):
     solution of the magnetic field from a thin current sheet along the z=0
     plane, from Tsyganenko and Sitnov 2007, eq. 15.
 
-    SUBROUTINE TAILSHT_S (M,X,Y,Z,BX,BY,BZ)
-
-    author G.K.Stephens
+    Attributes
+    ----------
+    waveNumber : float
+        The wave number of this expansion.
+    currentSheetHalfThickness : DifferentiableScalarFieldIJ
+        A 2D scalar field representing the current sheet half thickness
+        throughout the equatorial current system.
+    bessel : BesselFunctionEvaluator, ignored
+        The Bessel function evaluator.
     """
 
     def __init__(self, waveNumber, currentSheetHalfThickness, bessel):
-        """Build a new object.
+        """Initialize a new TailSheetSymmetricExpansion object.
 
-        param waveNumber the wave number of this expansion
-        param currentSheetHalfThickness a 2D scalar field representing the
-        current sheet half thickness throughout the equatorial current system
-        param bessel the Bessel function evaluator
+        Initialize a new TailSheetSymmetricExpansion object.
+
+        Parameters
+        ----------
+        waveNumber : float
+            The wave number of this expansion.
+        currentSheetHalfThickness : DifferentiableScalarFieldIJ
+            A 2D scalar field representing the current sheet half thickness
+            throughout the equatorial current system.
+        bessel : BesselFunctionEvaluator, ignored
+            The Bessel function evaluator.
         """
         self.waveNumber = waveNumber
         self.currentSheetHalfThickness = currentSheetHalfThickness
         self.bessel = bessel
 
     def evaluate(self, *args):
-        """Evaluate the expansion."""
+        """Evaluate the expansion.
+        
+        Evaluate the expansion.
+
+        Parameters
+        ----------
+        location : VectorIJK
+            Cartesian location to evaluate the field.
+        buffer : VectorIJK, optional
+            Buffer to hold the result.
+        
+        Returns
+        -------
+        buffer : VectorIJK
+            Result of expansion at location.
+        
+        Raises
+        ------
+        TypeError
+            If incorrect parameters are supplied.
+        """
         if len(args) == 1:
             (location,) = args
             buffer = VectorIJK()
-            return self.evaluate(location, buffer)
+            self.evaluate(location, buffer)
         elif len(args) == 2:
             (location, buffer) = args
             x = location.i
@@ -47,30 +88,28 @@ class TailSheetSymmetricExpansion(VectorField):
             z = location.k
             locationIJ = VectorIJ(x, y)
 
-            # get the current sheet half thickness
+            # Get the current sheet half thickness.
             thick = self.currentSheetHalfThickness.evaluate(locationIJ)
 
-            # now get the current sheet half thickness derivatives
+            # Now get the current sheet half thickness derivatives.
             dThickdx = self.currentSheetHalfThickness.differentiateFDi(
                 locationIJ)
             dThickdy = self.currentSheetHalfThickness.differentiateFDj(
                 locationIJ)
 
-            # convert to polar
+            # Convert to polar.
             rho = sqrt(x*x + y*y)
-
-            # convert derivatives to polar
             dThickdRho = (x*dThickdx + y*dThickdy)/rho
             cosPhi = x/rho
             sinPhi = y/rho
 
-            # introduce a finite thickness in z by replacing z with this value
+            # Introduce a finite thickness in z by replacing z with this value.
             zDist = sqrt(z*z + thick*thick)
 
-            # kn is the wave number
+            # kn is the wave number.
             kn = self.waveNumber
 
-            # evaluate the Bessel function
+            # Evaluate the Bessel function.
             j0 = sps.jv(0, kn*rho)
             j1 = sps.jv(1, kn*rho)
 
@@ -80,6 +119,6 @@ class TailSheetSymmetricExpansion(VectorField):
             bz = kn*ex*(j0 - thick*dThickdRho*j1/zDist)
 
             buffer[:] = [bx, by, bz]
-            return buffer
         else:
-            raise Exception
+            raise TypeError
+        return buffer

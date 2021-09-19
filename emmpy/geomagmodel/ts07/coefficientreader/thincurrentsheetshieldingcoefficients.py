f38e2c61e142414afefb3dd1618a601e4fe4b256
Eric Winter <eric.winter@jhuapl.edu>
2021-09-19 15:23:59 -0400

    Docstrings

--- emmpy/geomagmodel/ts07/coefficientreader/thincurrentsheetshieldingcoefficients.py
+++ emmpy/geomagmodel/ts07/coefficientreader/thincurrentsheetshieldingcoefficients.py
@@ -1,4 +1,13 @@
-"""Store the static coefficients for the TS07D model."""
+"""Store the static coefficients for the TS07D model.
+
+Store the static coefficients for the TS07D model.
+
+Authors
+-------
+Nicholas Sharp
+G.K. Stephens
+Eric Winter (eric.winter@jhuapl.edu)
+"""
 
 
 class ThinCurrentSheetShieldingCoefficients:
@@ -8,11 +17,26 @@ class ThinCurrentSheetShieldingCoefficients:
     are available on the model webpage at
     http://geomag_field.jhuapl.edu/model/TAIL_PAR.zip.
 
-    To construct this class, use the {@link TS07DStaticCoefficientsFactory}
-    class.
+    To construct this class, use the TS07DStaticCoefficientsFactor class.
 
-    author Nicholas Sharp
-    author G.K.Stephens
+    Attributes
+    ----------
+    numRadialExpansions : int
+        Number of radial expansions.
+    numAzimuthalExpansions : int
+        Number of azimuthal expansions.
+    symmetricTailExpansion : CoefficientExpansion2D
+        Coefficients for symmetric tail expansion.
+    symmetricTailWaveExpansion : CoefficientExpansion1D
+        Coefficients for symmetric tail wave expansion.
+    oddTailExpansion : CoefficientExpansion2D
+        Coefficients for odd tail expansion.
+    oddTailWaveExpansion : CoefficientExpansion1D
+        Coefficients for odd tail wave expansion.
+    evenTailExpansion : CoefficientExpansion2D
+        Coefficients for even tail expansion.
+    evenTailWaveExpansion : CoefficientExpansion1D
+        Coefficients for even tail wave expansion.
     """
 
     def __init__(
@@ -20,10 +44,30 @@ class ThinCurrentSheetShieldingCoefficients:
         symmetricTailExpansion, symmetricTailWaveExpansion,
         oddTailExpansion, oddTailWaveExpansion,
         evenTailExpansion, evenTailWaveExpansion):
-        """Build a new object.
+        """Initialize a new ThinCurrentSheetShieldingCoefficients object.
 
         Constructor is package private, should be constructed using the
         TS07DStaticCoefficientsFactory class.
+
+        Parameters
+        ----------
+        numRadialExpansions : int
+            Number of radial expansions.
+        numAzimuthalExpansions : int
+            Number of azimuthal expansions.
+        symmetricTailExpansion : CoefficientExpansion2D
+            Coefficients for symmetric tail expansion.
+        symmetricTailWaveExpansion : CoefficientExpansion1D
+            Coefficients for symmetric tail wave expansion.
+        oddTailExpansion : CoefficientExpansion2D
+            Coefficients for odd tail expansion.
+        oddTailWaveExpansion : CoefficientExpansion1D
+            Coefficients for odd tail wave expansion.
+        evenTailExpansion : CoefficientExpansion2D
+            Coefficients for even tail expansion.
+        evenTailWaveExpansion : CoefficientExpansion1D
+            Coefficients for even tail wave expansion.
+
         """
         self.numRadialExpansions = numRadialExpansions
         self.numAzimuthalExpansions = numAzimuthalExpansions
@@ -35,33 +79,129 @@ class ThinCurrentSheetShieldingCoefficients:
         self.evenTailWaveExpansion = evenTailWaveExpansion
 
     def getSymmetricTailExpansion(self):
-        """Return the symmetric tail expansion."""
+        """Return the symmetric tail expansion.
+        
+        Return the symmetric tail expansion.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        result : CoefficientExpansion2D
+            Coefficients for symmetric tail expansion.
+        """
         return self.symmetricTailExpansion
 
     def getSymmetricTailWaveExpansion(self):
-        """Return the symmetric tail wave expansion."""
+        """Return the symmetric tail wave expansion.
+        
+        Return the symmetric tail wave expansion.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        result : CoefficientExpansion1D
+            Coefficients for symmetric tail wave expansion.
+        """
         return self.symmetricTailWaveExpansion
 
     def getOddTailExpansion(self):
-        """Return the odd tail expansion."""
+        """Return the odd tail expansion.
+        
+        Return the odd tail expansion.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        result : CoefficientExpansion2D
+            Coefficients for odd tail expansion.
+        """
         return self.oddTailExpansion
 
     def getOddTailWaveExpansion(self):
-        """Return the odd tail wve expansion."""
+        """Return the odd tail wave expansion.
+        
+        Return the odd tail wave expansion.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        result : CoefficientExpansion1D
+            Coefficients for odd tail wave expansion.
+        """
         return self.oddTailWaveExpansion
 
     def getEvenTailExpansion(self):
-        """Return the even tail expansion."""
+        """Return the even tail expansion.
+        
+        Return the even tail expansion.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        result : CoefficientExpansion2D
+            Coefficients for even tail expansion.
+        """
         return self.evenTailExpansion
 
     def getEvenTailWaveExpansion(self):
-        """Return the even tail wave expansion."""
+        """Return the even tail wave expansion.
+        
+        Return the even tail wave expansion.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        result : CoefficientExpansion1D
+            Coefficients for even tail wave expansion.
+        """
         return self.evenTailWaveExpansion
 
     def getNumRadialExpansions(self):
-        """Return the number of radial expanions."""
+        """Return the number of radial expanions.
+        
+        Return the number of radial expanions.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        result : int
+            Number of radial expansions.
+        """
         return self.numRadialExpansions
 
     def getNumAzimuthalExpansions(self):
-        """Return the number of azimuthal expansions."""
+        """Return the number of azimuthal expanions.
+        
+        Return the number of azimuthal expanions.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        result : int
+            Number of azimuthal expansions.
+        """
         return self.numAzimuthalExpansions

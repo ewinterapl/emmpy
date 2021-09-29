993d22f5f6b767f35ff6dc2097df4bbea8c255c3
Eric Winter <eric.winter@jhuapl.edu>
2021-09-29 17:41:55 -0400

    Docstrings

--- emmpy/geomagmodel/ts07/modeling/fieldaligned/fieldalignedcurrentbuilder.py
+++ emmpy/geomagmodel/ts07/modeling/fieldaligned/fieldalignedcurrentbuilder.py
@@ -1,5 +1,12 @@
-"""emmpy.geomagmodel.ts07.modeling.fieldaligned.fieldalignedcurrentbuilder"""
+"""Build a field-aligned current.
 
+Build a field-aligned current.
+
+Authors
+-------
+G.K. Stephens
+Eric Winter (eric.winter@jhuapl.edu)
+"""
 
 import emmpy.crucible.core.math.vectorfields.vectorfields as vectorfields
 from emmpy.geomagmodel.ts07.modeling.fieldaligned.birkelanddeformationfunction import (
@@ -8,11 +15,11 @@ from emmpy.geomagmodel.ts07.modeling.fieldaligned.birkelanddeformationfunction i
 from emmpy.geomagmodel.ts07.modeling.fieldaligned.scaledfield import (
     ScaledField
 )
-from emmpy.geomagmodel.ts07.modeling.fieldaligned.stretchedfield import (
-    StretchedField
-)
 from emmpy.geomagmodel.ts07.modeling.fieldaligned.smoothedconicalcurrentmagneticfield import (
     SmoothedConicalCurrentMagneticField
+)
+from emmpy.geomagmodel.ts07.modeling.fieldaligned.stretchedfield import (
+    StretchedField
 )
 from emmpy.magmodel.core.math.deformation.sphericalfielddeformation import (
     SphericalFieldDeformation
@@ -24,14 +31,14 @@ from emmpy.magmodel.core.modeling.fac.twoconicalfields import TwoConicalFields
 
 
 # Below are the TS07 values, if you don't overrride these, this is what will
-# be used
+# be used.
 D_THETA = [0.06, 0.09]
 D_PHI = [0.055, 0.030]
 BM = [[0.1618068350, 0.7058026940], [0.1278764024, 0.4036015198]]
 THETA0 = [[0.7113544659, 0.5567714182], [0.8867880020, 0.7247997430]]
 
 # Static coefficients used in the coordinate deformations of r and theta.
-# Given in Tsyganenko 2002-1, Table 2, eq. 18 and 19
+# Given in Tsyganenko 2002-1, Table 2, eq. 18 and 19.
 A = [
     [[-0.1797957553, 2.999642482, -0.9322708978, -0.6811059760,
       0.2099057262, -8.358815746, -14.86033550, 0.3838362986, -16.30945494],
@@ -78,31 +85,82 @@ D = [
 
 
 class FieldAlignedCurrentBuilder:
-    """A builder that builds Field Aligned current modules.
+    """Build a field-aligned current.
 
-    Note: this only includes the internal field and NOT the shielding field.
+    Note: this only includes the internal field and NOT the shielding
+    field.
 
-    @author G.K.Stephens
+    Attributes
+    ----------
+    mode : 
+    smoothed :
+    theta0 :
+    deltaTheta :
+    trigParity :
+    a :
+    b :
+    c :
+    d :
+    bmScaleFactor :
+    dipoleTilt :
+    dynamicPressure :
+    kappa :
+    dPhi :
+    scalingCoefficient :
     """
 
     def __init__(self, *args):
-        """Constructor"""
+        """Initialize a new FieldAlignedCurrentBuilder object.
+
+        Initialize a new FieldAlignedCurrentBuilder object.
+
+        Parameters
+        ----------
+        options : FacConfigurationOptions
+            FAC configuration options.
+        dipoleTilt : float
+            Dipole tilt angle.
+        dynamicPressure : float
+            Dynamic pressure.
+        kappa : float
+            Kappa scaling factor.
+        OR
+        region : int
+            FAC region code.
+        mode : int
+            Which mode (only 1 and 2 are currently available).
+        trigParity : TrigParity
+            Sine function is odd cosine is even.
+        dipoleTilt : float
+            Dipole tilt angle.
+        dynamicPressure : float
+            Dynamic pressure.
+        kappa : float
+            Kappa scaling factor.
+        scalingCoefficient : float
+            Scaling coefficient.
+        
+        Raises
+        ------
+        TypeError
+            If invalid parameters are provided.
+        """
         if len(args) == 4:
             (options, dipoleTilt, dynamicPressure, kappa) = args
             region = options.getRegion().getAsInt()
-            # needed to construct the conical field, eq. 14 & 15 of T91
+            # Needed to construct the conical field, eq. 14 & 15 of T91.
             self.mode = options.getMode()
             self.smoothed = options.isSmoothed()
             self.theta0 = options.getTheta0()
             self.deltaTheta = options.getDeltaTheta()
             self.trigParity = options.getTrigParity()
-            # needed to deform the conical field, eq. 18 & 19 T02
+            # Needed to deform the conical field, eq. 18 & 19 T02.
             self.a = A[region - 1][self.mode - 1]
             self.b = B[region - 1][self.mode - 1]
             self.c = C[region - 1][self.mode - 1]
             self.d = D[region - 1][self.mode - 1]
             self.bmScaleFactor = BM[region - 1][self.mode - 1]
-            # needed for stretching, eq. 21, 22, 23, 24, &25 T02
+            # Needed for stretching, eq. 21, 22, 23, 24, &25 T02.
             self.dipoleTilt = dipoleTilt
             self.dynamicPressure = dynamicPressure
             self.kappa = kappa
@@ -111,13 +169,6 @@ class FieldAlignedCurrentBuilder:
         elif len(args) == 7:
             (region, mode, trigParity, dipoleTilt, dynamicPressure, kappa,
              scalingCoefficient) = args
-            # @param region region 1 or 2
-            # @param mode which mode (only 1 and 2 are currently available)
-            # @param trigParity sine function is odd cosine is even
-            # @param dipoleTilt the dipole tilt angle
-            # @param dynamicPressure the dynamic pressure
-            # @param kappa the global scaling coefficient
-            # @param scalingCoefficient the
             self.mode = mode
             self.smoothed = False
             self.theta0 = THETA0[region - 1][mode - 1]
@@ -134,33 +185,100 @@ class FieldAlignedCurrentBuilder:
             self.dPhi = D_PHI[region - 1]
             self.scalingCoefficient = scalingCoefficient
         else:
-            raise Exception
+            raise TypeError
 
     def withSmoothing(self):
-        """Smooth the field aligned current."""
+        """Set the FAC smoothing flag.
+
+        Set the FAC smoothing flag.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        self : FieldAlignedCurrentBuilder
+            The current object.
+        """
         self.smoothed = True
         return self
 
     def setSmoothing(self, smoothing):
+        """Set the FAC smoothing flag to the specified value.
+
+        Set the FAC smoothing flag to the specified value.
+
+        Parameters
+        ----------
+        smoothing : bool
+            True for smoothing, False if not.
+
+        Returns
+        -------
+        self : FieldAlignedCurrentBuilder
+            The current object.
+        """
         self.smoothed = smoothing
         return self
 
     def withTheta0(self, theta0):
+        """Set the FAC central angle.
+
+        Set the FAC central angle.
+
+        Parameters
+        ----------
+        theta0 : float
+            Central angle for FAC.
+
+        Returns
+        -------
+        self : FieldAlignedCurrentBuilder
+            The current object.
+        """
         self.theta0 = theta0
         return self
 
     def withDeltaTheta(self, deltaTheta):
+        """Set the FAC angular half-width.
+
+        Set the FAC angular half-width.
+
+        Parameters
+        ----------
+        deltaTheta : float
+            FAC angular half-width.
+
+        Returns
+        -------
+        self : FieldAlignedCurrentBuilder
+            The current object.
+        """
         self.deltaTheta = deltaTheta
         return self
 
     def build(self):
-        # Note: this is a point of inconsistency with the Fortran code, in the
+        """Build the field-aligned current.
+        
+        Build the field-aligned current.
+        
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        pDynScaledField : VectorField
+            The field.
+        """
+        # Note: this is a point of inconsistency with the Fortran code. In the
         # Fortran this calculation is done in single precision. When the field
         # values are very large, this can result in tenths of differences
-        # between the Java and the Fortran version of the model
+        # between the Java and the Fortran version of the model.
         dynamicPressureScalingFactor = pow(self.dynamicPressure/2, 0.155)
 
-        # construct the conical field, eq. 14 & 15 of T91
+        # Construct the conical field, eq. 14 & 15 of T91.
         undeformedConicalField = None
         if self.smoothed:
             undeformedConicalField = (
@@ -175,7 +293,7 @@ class FieldAlignedCurrentBuilder:
                 )
             )
 
-        # deform the conical field,
+        # Deform the conical field.
         deformation = BirkelandDeformationFunction(
             self.a, self.b, self.c, self.d
         )
@@ -185,10 +303,10 @@ class FieldAlignedCurrentBuilder:
             self.bmScaleFactor
         )
 
-        # make it two cones
+        # Make it two cones.
         twoCones = TwoConicalFields(deformedField)
 
-        # scale the FIELD output by the coefficient
+        # Scale the field output by the coefficient.
         scaledTwoCones = vectorfields.scale(twoCones, self.scalingCoefficient)
 
         stretchedField = ScaledField(
@@ -197,7 +315,7 @@ class FieldAlignedCurrentBuilder:
             ), self.kappa
         )
 
-        # Scale position vector for solar wind (see Tsy 2002-1 2.4)
+        # Scale position vector for solar wind (see Tsy 2002-1 2.4).
         pDynScaledField = vectorfields.scaleLocation(
             stretchedField, dynamicPressureScalingFactor
         )

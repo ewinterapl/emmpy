ef133cc1769ba18fa2e40c58a85b3c8fb7f86a46
Eric Winter <eric.winter@jhuapl.edu>
2021-09-29 17:58:53 -0400

    Docstrings

--- emmpy/geomagmodel/ts07/modeling/fieldaligned/scaledfield.py
+++ emmpy/geomagmodel/ts07/modeling/fieldaligned/scaledfield.py
@@ -1,54 +1,73 @@
-"""emmpy.geomagmodel.ts07.modeling.fieldaligned.scaledfield"""
+"""Scale the position and value of a vector field.
 
+Scale the position and value of a vector field.
 
-# import crucible.core.math.vectorspace.UnwritableVectorIJK;
-# import crucible.core.math.vectorspace.VectorIJK;
+Authors
+-------
+G.K. Stephens
+Eric Winter (eric.winter@jhuapl.edu)
+"""
+
 
 from emmpy.crucible.core.math.vectorfields.vectorfield import VectorField
 
 
 class ScaledField(VectorField):
-    """Scales a VectorField by scaling both the position and the output vector
-    by a scalar value.
+    """Scale the position and value of a vector field.
 
-    where B' is the resulting scaled field
-    B and r are the original unscaled
-    and a is the scale factor
+    Scale a VectorField by scaling both the position and the output
+    vector by a scalar value.
 
-    author G.K.Stephens
+    Attributes
+    ----------
+    unscaledField : VectorField
+        The unscaled field.
+    kappaScale : float
+        The scale factor.
     """
 
     def __init__(self, unscaledField, kappaScale):
-        """Constructor
-        
-        Constructs a scaled VectorField by scaling both the position and the
-        output vector by a scalar value.
+        """Initialize a new ScaledField object.
+
+        Construct a scaled VectorField by scaling both the position and
+        the output vector by a scalar value.
 
         B'(r) = a B(a r)
 
-        where B' is the resulting scaled field
-        B and r are the original unscaled and a is the scale factor
+        where B' is the resulting scaled field.
+        B and r are the original unscaled and a is the scale factor.
 
-        param VectorField unscaledField
-        param double kappaScale
+        Parameters
+        ----------
+        unscaledField : VectorField
+            The unscaled field.
+        kappaScale : float
+            The scale factor.
         """
-        # VectorField unscaledField
         self.unscaledField = unscaledField
-        # double kappaScale
         self.kappaScale = kappaScale
 
     def evaluate(self, location, buffer):
-        """evaluate
+        """Evaluate the scaled field at a given location.
 
-        param UnwritableVectorIJK location
-        param VectorIJK buffer
-        return VectorIJK
+        Evaluate the scaled field at a given location.
+
+        Parameters
+        ----------
+        location : VectorIJK
+            Location for evaluation.
+        buffer : VectorIJK
+            Buffer to hold the evaluation result.
+        
+        Returns
+        -------
+        buffer : VectorIJK
+            Field evaluated at location.
         """
-
-        # perform spatial scaling, eq. (25)
+        # Perform spatial scaling, eq. (25).
         scaledLocation = location*self.kappaScale
         self.unscaledField.evaluate(scaledLocation, buffer)
 
-        # Apply the kappa scaling factor eq. (25)
+        # Apply the kappa scaling factor eq. (25).
         buffer *= self.kappaScale
         return buffer

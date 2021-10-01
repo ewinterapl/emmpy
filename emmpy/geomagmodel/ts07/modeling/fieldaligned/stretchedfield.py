da0ae45c83b83c36e1edecf7f3992f32a1c8e25f
Eric Winter <eric.winter@jhuapl.edu>
2021-10-01 13:10:53 -0400

    Docstrings

--- emmpy/geomagmodel/ts07/modeling/fieldaligned/stretchedfield.py
+++ emmpy/geomagmodel/ts07/modeling/fieldaligned/stretchedfield.py
@@ -1,37 +1,53 @@
-"""emmpy.geomagmodel.ts07.modeling.fieldaligned.stretchedfield"""
+"""Perform a stretching deformation on a vector field.
+
+Perform a stretching deformation on a vector field.
+
+Authors
+-------
+Nicholas Sharp
+G.K. Stephens
+Eric Winter (eric.winter@jhuapl.edu)
+"""
 
 
 from math import atan2, cos, sin, sqrt
 
-from emmpy.math.coordinates.cylindricalvector import CylindricalVector
 from emmpy.crucible.core.math.vectorfields.vectorfield import VectorField
 from emmpy.crucible.core.math.vectorspace.vectorijk import VectorIJK
-from emmpy.geomagmodel.ts07.modeling.fieldaligned.ffunction import (
-    FDerivatives, Ffunction
-)
+from emmpy.geomagmodel.ts07.modeling.fieldaligned.ffunction import Ffunction
+from emmpy.math.coordinates.cylindricalvector import CylindricalVector
 
 
 class StretchedField(VectorField):
-    """Performs a stretching deformation on the input VectorField.
+    """Perform a stretching deformation on a vector field.
     
-    This stretching is accounting for 3 effects as described in
-    "A model of the near magnetosphere with a dawn-dusk asymmetry 1.
-    Mathematical structure" by N. A. Tsyganenko.
-    See eq. (21, 22, 23, 24, and 25) in section 2.3.3.
+    This stretching is accounting for 3 effects as described in "A model
+    of the near magnetosphere with a dawn-dusk asymmetry 1. Mathematical
+    structure" by N. A. Tsyganenko. See eq. (21, 22, 23, 24, and 25) in
+    section 2.3.3.
 
-    1: the noon-midnight asymmetry in the R1 and R2 field aligned currents.
+    1: the noon-midnight asymmetry in the R1 and R2 field aligned
+       currents.
     2: dipole tilt angle
-    3: a spatial scaling that can account for the equatorward expansion of the
-    Birkeland current ovals.
+    3: a spatial scaling that can account for the equatorward expansion
+       of the Birkeland current ovals.
 
-    see http://onlinelibrary.wiley.com/doi/10.1029/2001JA000219/abstract
-    (Tsyganenko, 2002)
+    See http://onlinelibrary.wiley.com/doi/10.1029/2001JA000219/abstract
+    (Tsyganenko, 2002).
 
-    author Nicholas Sharp
-    author G.K.Stephens
+    Attributes
+    ----------
+    unstretchedField : VectorField
+        The input unstretched field that will be stretched.
+    dipoleTilt : float
+        The dipole tilt angle.
+    deltaPhi : float
+        Determines the amount of shift of the center of the FAC region to
+        the nightside.
+    fFunction : Ffunction
+        Stretching function.
     """
 
-    # float rho02, beta, hingeDistance, epsilon, bConst
     rho02 = 49.0
     beta = 0.9
     hingeDistance = 10.0
@@ -39,36 +55,47 @@ class StretchedField(VectorField):
     bConst = 0.5  # These parameters adjust the deformation
 
     def __init__(self, unstretchedField, dipoleTilt, deltaPhi):
-        """Constructor
+        """Initialize a new StretchedField object.
 
-        param VectorField unstretchedField the input unstretched field that
-        will be stretched
-        param double dipoleTilt the dipole tilt angle
-        param deltaPhi deltaPhi determines the amount of shift of the center
-        of the FAC region to the nightside
+        Initialize a new StretchedField object.
+
+        Parameters
+        ----------
+        unstretchedField : VectorField
+            The input unstretched field that will be stretched.
+        dipoleTilt : float
+            The dipole tilt angle.
+        deltaPhi : float
+            Determines the amount of shift of the center of the FAC region
+            to the nightside.
         """
-        # VectorField unstretchedField
         self.unstretchedField = unstretchedField
-        # double deltaPhi
         self.deltaPhi = deltaPhi
-        # double dipoleTilt
         self.dipoleTilt = dipoleTilt
-        # Ffunction fFunction
         self.fFunction = Ffunction(deltaPhi, dipoleTilt)
 
     def evaluate(self, location, buffer):
-        """evaluate
+        """Evaluate the field.
 
-        param UnwritableVectorIJK location
-        param VectorIJK buffer
-        return VectorIJK
+        Evaluate the field.
+
+        Parameters
+        ----------
+        location : VectorIJK
+            Location for evaluation.
+        buffer : VectorIJK
+            Buffer to hold the result.
+
+        Returns
+        -------
+        buffer : VectorIJK
+            Field evaluated at location.
         """
-
-        # float x, y, z, rho2, rho, phi, sinPhi, cosPhi
         x = location.i
         y = location.j
         z = location.k
-        # convert to spherical and cylindrical
+
+        # Convert to spherical and cylindrical.
         rho2 = x*x + z*z
         rho = sqrt(rho2)
         phi = 0.0
@@ -81,12 +108,10 @@ class StretchedField(VectorField):
 
         # Tsy 2002-1 eqn 21, evaluates the stretch transformation function that
         # is used for the FAC field along with the partial derivatives with
-        # respect to phi, rho, and y
-        # FDerivatives fValues
+        # respect to phi, rho, and y.
         fValues = self.fFunction.evaluate(CylindricalVector(rho, phi, y))
 
-        # the derivatives
-        # float dF_dPhi, dF_dRho, dF_dy, sinF, cosF, xStar, yStar, zStar
+        # The derivatives.
         dF_dPhi = fValues.getdF_dPhi()
         dF_dRho = fValues.getdF_dRho()
         dF_dy = fValues.getdF_dy()
@@ -96,14 +121,12 @@ class StretchedField(VectorField):
         yStar = y
         zStar = -rho*sinF
 
-        # UnwritableVectorIJK unstretchedVector
         unstretchedVector = self.unstretchedField.evaluate(
             VectorIJK(xStar, yStar, zStar)
         )
 
         # Tsy 2002-1 eqn 22, B* the original undeformed field evaluated with
-        # the deformed coordinates
-        # float bRhoStar, bPhiStar
+        # the deformed coordinates.
         bRhoStar = (
             unstretchedVector.i*cosF - unstretchedVector.k*sinF
         )
@@ -111,37 +134,41 @@ class StretchedField(VectorField):
             -unstretchedVector.i*sinF - unstretchedVector.k*cosF
         )
 
-        # Tsy 2002-1 eqn 23, B' apply the transformation matrix
-        # float bRho, bPhi, by
+        # Tsy 2002-1 eqn 23, B' apply the transformation matrix.
         bRho = bRhoStar*dF_dPhi
         bPhi = bPhiStar - rho*(unstretchedVector.j*dF_dy +
                                bRhoStar*dF_dRho)
         by = unstretchedVector.j*dF_dPhi
 
-        # Tsy 2002-1 eqn 24, and now convert B' from cylindrial to Cartesian
-        # float bx, bz
+        # Tsy 2002-1 eqn 24, and now convert B' from cylindrial to Cartesian.
         bx = bRho*cosPhi - bPhi*sinPhi
         bz = -bRho*sinPhi - bPhi*cosPhi
 
-        # return buffer.setTo(bx, by, bz)
         buffer[:] = [bx, by, bz]
         return buffer
 
     def evaluate2(self, location, buffer):
-        """evaluate2
+        """An alternative evaluation of the field.
 
-        param UnwritableVectorIJK location
-        param VectorIJK buffer
-        return VectorIJK
+        An alternative evaluation of the field.
+
+        Parameters
+        ----------
+        location : VectorIJK
+            Location for evaluation.
+        buffer : VectorIJK
+            Buffer to hold the result.
+
+        Returns
+        -------
+        buffer : VectorIJK
+            Field evaluated at location.
         """
-
-        # float x, y, z
         x = location.getI()
         y = location.getJ()
         z = location.getK()
 
-        # convert to spherical and cylindrical
-        # float rho2, rho, rSph, phi, sinPhi, cosPhi
+        # Convert to spherical and cylindrical.
         rho2 = x*x + z*z
         rho = sqrt(rho2)
         rSph = sqrt(x*x + y*y + z*z)
@@ -154,9 +181,8 @@ class StretchedField(VectorField):
         cosPhi = cos(phi)
 
         # Implementation of Tsy 2002-1 eq. (21) (with some extra normalization
-        # term applied to b?!)
-        # the constant has been scaled by the constant rho02 / (rho02 + 1)
-        # float sinPhiCoef, r1Rh, f, fm1, psiTerm
+        # term applied to b?!).
+        # The constant has been scaled by the constant rho02 / (rho02 + 1).
         sinPhiCoef = (
             self.deltaPhi + StretchedField.bConst*StretchedField.rho02 /
             (StretchedField.rho02 + 1)*(rho2 - 1)/(StretchedField.rho02 + rho2)
@@ -169,12 +195,10 @@ class StretchedField(VectorField):
             pow(f, 1/StretchedField.epsilon)
         )
 
-        # "F" in literature
-        # float phiStretched
+        # "F" in literature.
         phiStretched = phi - sinPhiCoef*sinPhi - psiTerm
 
-        # compute derivatives of F with respect to phi, rho, and y
-        # float dF_dPhi, dF_dRho, dF_dy, sinF, cosF, xStar, yStar, zStar
+        # Compute derivatives of F with respect to phi, rho, and y.
         dF_dPhi = 1 - sinPhiCoef*cosPhi
         dF_dRho = (
             -2*StretchedField.bConst*StretchedField.rho02*rho /
@@ -192,14 +216,12 @@ class StretchedField(VectorField):
         yStar = y
         zStar = -rho*sinF
 
-        # UnwritableVectorIJK unstretchedVector
         unstretchedVector = (
             self.unstretchedField.evaluate(VectorIJK(xStar, yStar, zStar))
         )
 
         # Tsy 2002-1 eqn 22, B* the original undeformed field evaluated with
-        # the deformed coordinates
-        # float bRhoStar, bPhiStar
+        # the deformed coordinates.
         bRhoStar = (
             unstretchedVector.getI()*cosF - unstretchedVector.getK()*sinF
         )
@@ -207,15 +229,13 @@ class StretchedField(VectorField):
             -unstretchedVector.getI()*sinF - unstretchedVector.getK()*cosF
         )
 
-        # Tsy 2002-1 eqn 23, B' apply the transformation matrix
-        # float bRho, bPhi, by
+        # Tsy 2002-1 eqn 23, B' apply the transformation matrix.
         bRho = bRhoStar*dF_dPhi
         bPhi = bPhiStar - rho*(unstretchedVector.getJ()*dF_dy +
                                bRhoStar*dF_dRho)
         by = unstretchedVector.getJ()*dF_dPhi
 
-        # Tsy 2002-1 eqn 24, and now convert B' from cylindrial to Cartesian
-        # float bx, bz
+        # Tsy 2002-1 eqn 24, and now convert B' from cylindrial to Cartesian.
         bx = bRho*cosPhi - bPhi*sinPhi
         bz = -bRho*sinPhi - bPhi*cosPhi
 

bdc3146b49e113eb65b86862f6d0de7631108169
Eric Winter <eric.winter@jhuapl.edu>
2021-10-12 17:57:56 -0400

    Docstrings

--- emmpy/crucible/core/math/coords/cartesianvectorfieldvalue.py
+++ emmpy/crucible/core/math/coords/cartesianvectorfieldvalue.py
@@ -19,23 +19,4 @@ class CartesianVectorFieldValue(VectorFieldValue):
     A Cartesian vector field value contains a position in the vector
     field, and a value for the vector field, both in Cartesian
     coordinates.
-
-    Attributes
-    ----------
-    None
     """
-
-    # def __init__(self, position, value):
-    #     """Initialize a new CartesianVectorFieldValue object.
-
-    #     Initialize a new CartesianVectorFieldValue object.
-
-    #     Parameters
-    #     ----------
-    #     position : VectorIJK
-    #         Position in the vector field.
-    #     value : VectorIJK
-    #         Value of the vector field at the position.
-    #     """
-    #     self.position = position
-    #     self.value = value

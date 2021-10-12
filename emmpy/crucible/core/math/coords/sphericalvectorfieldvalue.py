9329540ee8b14b8722227e295a87787a3d31a8d8
Eric Winter <eric.winter@jhuapl.edu>
2021-10-12 18:04:13 -0400

    SphericalVectorFieldValue now inherits from VectorFieldValue.

--- emmpy/crucible/core/math/coords/sphericalvectorfieldvalue.py
+++ emmpy/crucible/core/math/coords/sphericalvectorfieldvalue.py
@@ -10,24 +10,12 @@ Eric Winter (eric.winter@jhuapl.edu)
 """
 
 
-class SphericalVectorFieldValue:
+from .vectorfieldvalue import VectorFieldValue
+
+
+class SphericalVectorFieldValue(VectorFieldValue):
     """Vector field in a 3-D spherical space.
 
     A spherical vector field value contains a position in the vector field,
     and a value for the vector field, both in spherical coordinates.
     """
-
-    def __init__(self, position, value):
-        """Initialize a new SphericalVectorFieldValue object.
-
-        Initialize a new SphericalVectorFieldValue object.
-
-        Parameters
-        ----------
-        position : SpheicalVector
-            Position in the vector field.
-        value : SphericalVector
-            Value of the vector field at the position.
-        """
-        self.position = position
-        self.value = value

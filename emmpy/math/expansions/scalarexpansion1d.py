7a97090e48d286b18a174c7d7185f5e25c1cf5b3
Eric Winter <eric.winter@jhuapl.edu>
2021-10-14 12:52:11 -0400

    ScalarExpansion1D now uses padding elements to allow direct indexing.

--- emmpy/math/expansions/scalarexpansion1d.py
+++ emmpy/math/expansions/scalarexpansion1d.py
@@ -1,8 +1,9 @@
 """A 1-D array of scalar expansion components.
 
 A 1-D array of scalar expansion components. The index of the first valid
-component (firstExpansionNumber) is not required to be 0. Component access
-by index is adjusted from the logical index to the physical index.
+component (firstExpansionNumber) is not required to be 0. The stored array
+is padded with unused elements at the head of the array, to allow for the
+desired indexing to work properly.
 
 Authors
 -------
@@ -19,56 +20,64 @@ class ScalarExpansion1D(np.ndarray):
     This class represents a series expansion of scalars that starts at
     a lower bound index (L) and ends at an upper bound index (U).
     The index of the first valid component (firstExpansionNumber) is not
-    required to be 0. Component access by index is adjusted from the
-    logical index to the physical index.
+    required to be 0. To permit this, the stored array is padded with
+    unused elements at the head of the array. For example, if
+    firstExpansionNumber is 2, then elements 0 and 1 in the array are
+    allocated, but ignored. This is more efficient than recomputing
+    the index on every access, since firstExpansionNumber is usually a
+    small integer. It also allows the expansion to be used directly as a
+    numpy array, by using slicing of the form:
+
+    valid_components = expansion[firstExpansionNumber:]
 
     Attributes
     ----------
     firstExpansionNumber : int
-        Logical index of first expansion coefficient. Maps to physical
-        index 0 in the stored array.
+        Index of first expansion component.
     lastExpansionNumber : int
-        Logical index of last expansion coefficient. Maps to last physical
-        index in the stored array.
+        Index of last expansion component.
     """
 
     def __new__(cls, data, firstExpansionNumber):
         """Allocate a new ScalarExpansion1D object.
 
         Allocate a new ScalarExpansion1D object by allocating a new
-        np.ndarray object on which this class will expand.
+        np.ndarray object on which this class will expand. Extra, unused
+        elements are padded at the head of the array to allow for non-0-
+        based indexing.
 
         Parameters
         ----------
         data : array-like of float
             1-D array of scalar expansion components.
         firstExpansionNumber : int
-            Logical index of first expansion coefficient.
+            Logical index of first expansion component.
 
         Returns
         -------
         se1d : ScalarExpansion1D
             The newly-allocated object.
         """
-        size = len(data)
+        size = len(data) + firstExpansionNumber
         se1d = super().__new__(cls, shape=(size,))
         return se1d
 
     def __init__(self, data, firstExpansionNumber):
         """Initialize a new ScalarExpansion1D object.
 
-        Initialize a new ScalarExpansion1D object.
+        Initialize a new ScalarExpansion1D object. Note that the padding
+        elements (equal to firstExpansionNumber elements) are ignored.
 
         Parameters
         ----------
         data : array-like of float
             1-D array of scalar expansion components.
         firstExpansionNumber : int
-            Index of first expansion coefficient.
+            Index of first valid expansion component.
         """
-        self[:] = np.array(data)
+        self[firstExpansionNumber:] = np.array(data)
         self.firstExpansionNumber = firstExpansionNumber
-        self.lastExpansionNumber = self.firstExpansionNumber + len(self) - 1
+        self.lastExpansionNumber = self.firstExpansionNumber + len(data) - 1
 
     def getComponent(self, index):
         """Return the component at the specified index.
@@ -85,4 +94,4 @@ class ScalarExpansion1D(np.ndarray):
         result : float
             Desired expansion component.
         """
-        return self[index - self.firstExpansionNumber]
+        return self[index]
--- emmpy/math/expansions/tests/test_scalarexpansion1d.py
+++ emmpy/math/expansions/tests/test_scalarexpansion1d.py
@@ -11,6 +11,7 @@ from emmpy.math.expansions.scalarexpansion1d import ScalarExpansion1D
 # Test data.
 data = np.array([1.1, 2.2, 3.3, 4.4, 5.5])
 firstExpansionNumber = 1
+lastExpansionNumber = firstExpansionNumber + len(data) - 1
 
 class TestBuilder(unittest.TestCase):
     """Tests for the scalarexpansion1d module."""
@@ -20,35 +21,20 @@ class TestBuilder(unittest.TestCase):
         se1d = ScalarExpansion1D.__new__(
             ScalarExpansion1D, data, firstExpansionNumber
         )
-        self.assertEqual(len(se1d), len(data))
+        self.assertEqual(len(se1d), len(data) + firstExpansionNumber)
 
     def test___init__(self):
         """Test the __init__ method."""
         se1d = ScalarExpansion1D(data, firstExpansionNumber)
-        for i in range(len(data)):
-            self.assertAlmostEqual(se1d[i], data[i])
-        for i in range(firstExpansionNumber,
-                       firstExpansionNumber + len(data)):
-            self.assertAlmostEqual(se1d[i - firstExpansionNumber],
-                                   data[i - firstExpansionNumber])
+        for i in range(firstExpansionNumber, lastExpansionNumber + 1):
+            self.assertAlmostEqual(se1d[i], data[i - firstExpansionNumber])
         self.assertEqual(se1d.firstExpansionNumber, firstExpansionNumber)
-
-    def test_getLowerBoundIndex(self):
-        """Test the getLowerBoundIndex method."""
-        se1d = ScalarExpansion1D(data, firstExpansionNumber)
-        self.assertEqual(se1d.getLowerBoundIndex(), firstExpansionNumber)
-
-    def test_getUpperBoundIndex(self):
-        """Test the getUpperBoundIndex method."""
-        se1d = ScalarExpansion1D(data, firstExpansionNumber)
-        self.assertEqual(se1d.getUpperBoundIndex(),
-                         firstExpansionNumber + len(data) - 1)
+        self.assertEqual(se1d.lastExpansionNumber, lastExpansionNumber)
 
     def test_getComponent(self):
         """Test the getCoefficient method."""
         se1d = ScalarExpansion1D(data, firstExpansionNumber)
-        for i in range(firstExpansionNumber,
-                       firstExpansionNumber + len(data)):
+        for i in range(firstExpansionNumber, lastExpansionNumber + 1):
             self.assertAlmostEqual(se1d.getComponent(i),
                                    data[i - firstExpansionNumber])
 

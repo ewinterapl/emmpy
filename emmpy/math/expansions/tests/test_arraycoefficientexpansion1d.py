5f6cfa284bef46b0b94b3a6d6390e85772af1e0c
Eric Winter <eric.winter@jhuapl.edu>
2021-10-15 15:00:33 -0400

    Docstrings

--- emmpy/math/expansions/tests/test_arraycoefficientexpansion1d.py
+++ emmpy/math/expansions/tests/test_arraycoefficientexpansion1d.py
@@ -1,3 +1,6 @@
+"""Tests for the arraycoefficientexpansion1d module."""
+
+
 import unittest
 
 from emmpy.math.expansions.arraycoefficientexpansion1d import (
@@ -6,11 +9,14 @@ from emmpy.math.expansions.arraycoefficientexpansion1d import (
 
 
 class TestBuilder(unittest.TestCase):
+    """Tests for the arraycoefficientexpansion1d module."""
 
     def test___init__(self):
+        """Test the __init__ method."""
         ArrayCoefficientExpansion1D([0], 0)
 
     def test_getCoefficient(self):
+        """Test the getCoefficient method."""
         c = ArrayCoefficientExpansion1D([0.0, 1.1, 2.2, 3.3], 1)
         self.assertAlmostEqual(c.getCoefficient(2), 1.1)
 

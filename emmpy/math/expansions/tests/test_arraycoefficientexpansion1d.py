55d881fc8df28066afad30a541251a9a4a24a569
Eric Winter <eric.winter@jhuapl.edu>
2021-10-15 15:06:46 -0400

    Completed current test suite.

--- emmpy/math/expansions/tests/test_arraycoefficientexpansion1d.py
+++ emmpy/math/expansions/tests/test_arraycoefficientexpansion1d.py
@@ -1,25 +1,51 @@
-"""Tests for the arraycoefficientexpansion1d module."""
+"""Tests for the arraycoefficientexpansion1d module.
+
+NOTE: These tests are for the unpadded, np.ndarray-based version of the
+module.
+"""
 
 
 import unittest
+
+import numpy as np
 
 from emmpy.math.expansions.arraycoefficientexpansion1d import (
     ArrayCoefficientExpansion1D
 )
 
 
+# Test data.
+data = np.array([1.1, 2.2, 3.3, 4.4, 5.5])
+firstExpansionNumber = 1
+lastExpansionNumber = firstExpansionNumber + len(data) - 1
+
+
 class TestBuilder(unittest.TestCase):
     """Tests for the arraycoefficientexpansion1d module."""
 
+    def test___new__(self):
+        """Test the __new__ method."""
+        se1d = ArrayCoefficientExpansion1D.__new__(
+            ArrayCoefficientExpansion1D, data, firstExpansionNumber
+        )
+        self.assertEqual(len(se1d), len(data))
+
     def test___init__(self):
         """Test the __init__ method."""
-        ArrayCoefficientExpansion1D([0], 0)
+        ace1d = ArrayCoefficientExpansion1D(data, firstExpansionNumber)
+        for i in range(firstExpansionNumber, lastExpansionNumber + 1):
+            self.assertAlmostEqual(ace1d[i - firstExpansionNumber],
+                                   data[i - firstExpansionNumber])
+        self.assertEqual(ace1d.firstExpansionNumber, firstExpansionNumber)
+        self.assertEqual(ace1d.lastExpansionNumber, lastExpansionNumber)
 
     def test_getCoefficient(self):
         """Test the getCoefficient method."""
-        c = ArrayCoefficientExpansion1D([0.0, 1.1, 2.2, 3.3], 1)
-        self.assertAlmostEqual(c.getCoefficient(2), 1.1)
+        ace1d = ArrayCoefficientExpansion1D(data, firstExpansionNumber)
+        for i in range(firstExpansionNumber, lastExpansionNumber + 1):
+            self.assertAlmostEqual(ace1d.getCoefficient(i),
+                                   data[i - firstExpansionNumber])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()

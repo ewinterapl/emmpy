10edb33a4bf2446b6bd289886208a67d0a629719
Eric Winter <eric.winter@jhuapl.edu>
2021-10-28 10:39:56 -0400

    Remove crucible.core.math.functions.

--- emmpy/crucible/core/math/__init__.py
+++ emmpy/crucible/core/math/__init__.py
@@ -13,7 +13,6 @@ Modules
 
 Packages
 --------
-functions
 vectorfields
 vectorspace
 """
--- emmpy/crucible/core/math/functions/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Tests for Python port of Java classes from the functions hierarchy.
-
-Modules
--------
-test_univariatefunction.py
-
-Packages
---------
-"""

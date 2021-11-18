6db82ccd581cf1bad07eaf718b89b2c8fc9f8d45
Eric Winter <eric.winter@jhuapl.edu>
2021-11-18 11:09:47 -0500

    Removed recursion from create().

--- emmpy/geomagmodel/ts07/coefficientreader/ts07dvariablecoefficientsutils.py
+++ emmpy/geomagmodel/ts07/coefficientreader/ts07dvariablecoefficientsutils.py
@@ -103,61 +103,54 @@ class TS07DVariableCoefficientsUtils:
                     variableCoefficientsFile
                 )
             )
-            return TS07DVariableCoefficientsUtils.create(
-                variableCoefficientsFile, numCurrentSheets,
-                numAzimuthalExpansions, numRadialExpansions, facConfiguration
-            )
-        elif len(args) == 4:
-            raise TypeError
         elif len(args) == 5:
             (variableCoefficientsFile, numCurrentSheets,
              numAzimuthalExpansions, numRadialExpansions,
              facConfiguration) = args
-            # Constructs the TS07D set of coefficients from the ASCII file WITH
-            # a customized resolution and with MANY current sheets. This set of
-            # coefficient can be used to construct the TS07D model.
-            # This is package private because (at least for now) we want to
-            # limit the public API to only support 1 or 2 current sheets, not
-            # many current sheets.
-
-            # The number of asymmetric expansions is simply n*m.
-            numAsymmetricExpansions = (
-                numRadialExpansions*numAzimuthalExpansions
-            )
-
-            # Half the number of equatorial expansion coefficients, r+2*(r*a).
-            # This is half the expansions as the dynamic pressure terms double
-            # this number.
-            numHalfExpansions = numRadialExpansions + 2*numAsymmetricExpansions
-            numExpansions = numHalfExpansions*2*numCurrentSheets
-
-            totalNumberOfCoefficients = (
-                1 + numExpansions + numCurrentSheets + 5 +
-                facConfiguration.numberOfFields
-            )
-
-            coeffs = nones((totalNumberOfCoefficients,))
-            lineNumber = 0
-            for line in open(variableCoefficientsFile):
-                try:
-                    coeffs[lineNumber] = float(line.split()[0])
-                    lineNumber += 1
-                except ValueError:
-                    break
-
-            # NOTE, this is a deviation from the FORTRAN code, the FORTRAN
-            # code handles this in the WARPED subroutine.
-            twist = coeffs[-1]/10
-            coeffs[-1] = twist
-
-            return TS07DVariableCoefficientsUtils.createFromArray(
-                coeffs, numCurrentSheets, numAzimuthalExpansions,
-                numRadialExpansions, facConfiguration
-            )
-        elif len(args) == 6:
-            raise TypeError
         else:
             raise TypeError
+
+        # Constructs the TS07D set of coefficients from the ASCII file WITH
+        # a customized resolution and with MANY current sheets. This set of
+        # coefficient can be used to construct the TS07D model.
+        # This is package private because (at least for now) we want to
+        # limit the public API to only support 1 or 2 current sheets, not
+        # many current sheets.
+
+        # The number of asymmetric expansions is simply n*m.
+        numAsymmetricExpansions = (
+            numRadialExpansions*numAzimuthalExpansions
+        )
+
+        # Half the number of equatorial expansion coefficients, r+2*(r*a).
+        # This is half the expansions as the dynamic pressure terms double
+        # this number.
+        numHalfExpansions = numRadialExpansions + 2*numAsymmetricExpansions
+        numExpansions = numHalfExpansions*2*numCurrentSheets
+
+        totalNumberOfCoefficients = (
+            1 + numExpansions + numCurrentSheets + 5 +
+            facConfiguration.numberOfFields
+        )
+
+        coeffs = nones((totalNumberOfCoefficients,))
+        lineNumber = 0
+        for line in open(variableCoefficientsFile):
+            try:
+                coeffs[lineNumber] = float(line.split()[0])
+                lineNumber += 1
+            except ValueError:
+                break
+
+        # NOTE, this is a deviation from the FORTRAN code, the FORTRAN
+        # code handles this in the WARPED subroutine.
+        twist = coeffs[-1]/10
+        coeffs[-1] = twist
+
+        return TS07DVariableCoefficientsUtils.createFromArray(
+            coeffs, numCurrentSheets, numAzimuthalExpansions,
+            numRadialExpansions, facConfiguration
+        )
 
     @staticmethod
     def readDynamicPressure(variableCoefficientsFile):

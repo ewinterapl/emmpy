6c59ef406ee1d6ddb1ca3ad9186fa154657c1ee9
Eric Winter <eric.winter@jhuapl.edu>
2022-01-27 15:00:50 -0500

    Docstrings

--- emmpy/crucible/crust/vectorfieldsij/differentiablescalarfieldij.py
+++ emmpy/crucible/crust/vectorfieldsij/differentiablescalarfieldij.py
@@ -8,6 +8,19 @@ class DifferentiableScalarFieldIJ:
     """The value and derivatives of a 2-D scalar field.
 
     This class defines a differentiable scalar field in 2 dimensions.
+
+    Attributes
+    ----------
+    evaluate: function(Vector2D)->float
+        Function to evaluate the scalar field at the specified position.
+
+    differentiateFDi: function(Vector2D)->float
+        Function to evaluate the first derivative with respect to the first
+        coordinate of the scalar field at the specified position.
+
+    differentiateFDi: function(Vector2D)->float
+        Function to evaluate the first derivative with respect to the
+        second coordinate of the scalar field at the specified position.
     """
 
     def __init__(self, evaluateFunction, iDerivativeFunction,

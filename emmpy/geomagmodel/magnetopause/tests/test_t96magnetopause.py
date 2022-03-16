b97881f2b160dda021d1c20cb847ecd17fc70cff
Eric Winter <eric.winter@jhuapl.edu>
2022-03-16 13:15:59 -0400

    Added stub for test___init__().

--- emmpy/geomagmodel/magnetopause/tests/test_t96magnetopause.py
+++ emmpy/geomagmodel/magnetopause/tests/test_t96magnetopause.py
@@ -11,6 +11,9 @@ from emmpy.geomagmodel.magnetopause.t96magnetopause import (
 class TestBuilder(unittest.TestCase):
     """Tests for the t96magnetopause module."""
 
+    def test___init__(self):
+        """Test the __init__ method."""
+
 
 if __name__ == "__main__":
     unittest.main()

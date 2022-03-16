fcb065c784ced8f918d660b700b6cd411900726c
Eric Winter <eric.winter@jhuapl.edu>
2022-03-16 16:49:15 -0400

    Implemented constructor.

--- emmpy/geomagmodel/magnetopause/magnetopauseoutput.py
+++ emmpy/geomagmodel/magnetopause/magnetopauseoutput.py
@@ -1,4 +1,11 @@
-"""XXX.
+"""A container class for the output of the magnetopause methods.
+
+A container class for the output of the magnetopause methods mirroring the
+output of the magnetopause subroutines in the Geopack FORTRAN code
+(T96_MGNP_08 and SHUETAL_MGNP_08). This contains a boolean indicating if the
+query point is inside or outside the magnetopause along with a point located
+on the magnetopause boundary that is approximately the closest point located
+on the magnetopause to the query point.
 
 Authors
 -------
@@ -8,63 +15,38 @@ Eric Winter (eric.winter@jhuapl.edu)
 
 
 class MagnetopauseOutput:
-    """XXX.
+    """A container class for the output of the magnetopause methods.
 
     Attributes
     ----------
     None
     """
 
-    def __init__(self):
+    def __init__(self, magnetopauseLocation, distanceToMagnetopause,
+                 withinMagnetosphere):
         """Initialize a new MagnetopauseOutput object.
 
         Initialize a new MagnetopauseOutput object.
 
         Parameters
         ----------
-        None
+        magnetopauseLocation : VectorIJK
+            XXX
+        distanceToMagnetopause : float
+            XXX
+        withinMagnetosphere : bool
+            XXX
 
         Returns
         -------
         None
         """
+        self.magnetopauseLocation = magnetopauseLocation
+        self.distanceToMagnetopause = distanceToMagnetopause
+        self.withinMagnetosphere = withinMagnetosphere
 
 
 ###############################################################################
-# package magnetopause;
-
-# import crucible.core.math.vectorspace.UnwritableVectorIJK;
-
-# /**
-#  * A container class for the output of the magnetopause methods mirroring the output of the
-#  * magnetopause subroutines in the Geopack FORTRAN code (T96_MGNP_08 and SHUETAL_MGNP_08). This
-#  * contains a boolean indicating if the query point is inside or outside the magnetopause along with
-#  * a point located on the magnetopause boundary that is approximately the closest point located on
-#  * the magnetopause to the query point.
-#  * 
-#  * @author G.K.Stephens
-#  *
-#  */
-# public class MagnetopauseOutput {
-
-#   private final UnwritableVectorIJK magnetopauseLocation;
-#   private final double distanceToMagnetopause;
-#   private final boolean withinMagnetosphere;
-
-#   /**
-#    * Constructor
-#    * 
-#    * @param magnetopauseLocation
-#    * @param distanceToMagnetopause
-#    * @param withinMagnetosphere
-#    */
-#   MagnetopauseOutput(UnwritableVectorIJK magnetopauseLocation, double distanceToMagnetopause,
-#       boolean withinMagnetosphere) {
-#     super();
-#     this.magnetopauseLocation = magnetopauseLocation;
-#     this.distanceToMagnetopause = distanceToMagnetopause;
-#     this.withinMagnetosphere = withinMagnetosphere;
-#   }
 
 #   /**
 #    * The distance between the query point and the point located on the magnetopause boundary that is
--- emmpy/geomagmodel/magnetopause/tests/test_magnetopauseoutput.py
+++ emmpy/geomagmodel/magnetopause/tests/test_magnetopauseoutput.py
@@ -3,9 +3,12 @@
 
 import unittest
 
+from numpy import isin
+
 from emmpy.geomagmodel.magnetopause.magnetopauseoutput import (
     MagnetopauseOutput
 )
+from emmpy.math.coordinates.vectorijk import VectorIJK
 
 
 class TestBuilder(unittest.TestCase):
@@ -13,8 +16,17 @@ class TestBuilder(unittest.TestCase):
 
     def test___init__(self):
         """Test the __init__ method."""
-        mo = MagnetopauseOutput()
+        (x, y, z) = (1, 2, 3)
+        location = VectorIJK(x, y, z)
+        d = 10.0
+        isInside = False
+        mo = MagnetopauseOutput(location, d, isInside)
         self.assertIsInstance(mo, MagnetopauseOutput)
+        self.assertAlmostEqual(mo.magnetopauseLocation.i, x)
+        self.assertAlmostEqual(mo.magnetopauseLocation.j, y)
+        self.assertAlmostEqual(mo.magnetopauseLocation.k, z)
+        self.assertAlmostEqual(mo.distanceToMagnetopause, d)
+        self.assertEqual(mo.withinMagnetosphere, isInside)
 
 
 if __name__ == "__main__":

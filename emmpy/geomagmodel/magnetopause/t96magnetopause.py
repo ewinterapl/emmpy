a35e896f5b9ef28578b1177f857065df45040d8c
Eric Winter <eric.winter@jhuapl.edu>
2022-03-16 13:12:50 -0400

    Added T96Magnetopause.

---
+++ emmpy/geomagmodel/magnetopause/t96magnetopause.py
@@ -0,0 +1,30 @@
+"""Compute the magnetopause according to the T96 model.
+
+Authors
+-------
+G.K. Stephens
+Eric Winter (eric.winter@jhuapl.edu)
+"""
+
+
+class T96Magnetopause:
+    """Compute the magnetopause according to the T96 model.
+
+    Attributes
+    ----------
+    None
+    """
+
+    def __init__(self):
+        """Initialize a new T96Magnetopause object.
+
+        Initialize a new T96Magnetopause object.
+
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        """
---
+++ emmpy/geomagmodel/magnetopause/tests/test_t96magnetopause.py
@@ -0,0 +1,16 @@
+"""Tests for the t96magnetopause module."""
+
+
+import unittest
+
+from emmpy.geomagmodel.magnetopause.t96magnetopause import (
+    T96Magnetopause
+)
+
+
+class TestBuilder(unittest.TestCase):
+    """Tests for the t96magnetopause module."""
+
+
+if __name__ == "__main__":
+    unittest.main()

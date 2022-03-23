b41b7190d8265d0a52e7c52214ceec1ac82736e3
Eric Winter <eric.winter@jhuapl.edu>
2022-03-23 16:28:23 -0400

    Added original Java code as comments.

---
+++ emmpy/geomagmodel/earthcurrentdensityfield.py
@@ -0,0 +1,47 @@
+# package geomagmodel;
+
+# import static crucible.core.math.vectorfields.VectorFields.curl;
+# import static crucible.core.math.vectorfields.VectorFields.quadraticApproximation;
+# import static crucible.core.math.vectorfields.VectorFields.scale;
+
+# import crucible.core.math.vectorfields.VectorField;
+# import crucible.core.math.vectorspace.UnwritableVectorIJK;
+# import crucible.core.math.vectorspace.VectorIJK;
+# import crucible.core.units.FundamentalPhysicalConstants;
+
+# /**
+#  * Constructs the current density from the magnetic field via Ampere's Law. This is computed
+#  * numerically using the finite difference method.
+#  * 
+#  * @author G.K.Stephens
+#  *
+#  */
+# public class EarthCurrentDensityField implements VectorField {
+
+#   // RE = 6371.2 KM is the standard radius of the Earth in geomagnetism
+#   private final static double radiusOfEarthKM = 6371.2;
+
+#   private final VectorField jField;
+
+#   /**
+#    * Constructor for the current density in units of nA/m^2.
+#    * 
+#    * @param magneticField a {@link VectorField} represents the magnetic field at Earth, where the
+#    *        inputs are location in Earth radii and the output is in nT
+#    * @param dr the step size
+#    */
+#   public EarthCurrentDensityField(VectorField magneticField, double dr) {
+
+#     double mu0 = FundamentalPhysicalConstants.MAGNETIC_CONSTANT;
+
+#     // convert units
+#     this.jField = scale(curl(quadraticApproximation(magneticField, dr, dr, dr)),
+#         1.0 / (radiusOfEarthKM * 1000.0 * mu0));
+#   }
+
+#   @Override
+#   public VectorIJK evaluate(UnwritableVectorIJK location, VectorIJK buffer) {
+#     return jField.evaluate(location, buffer);
+#   }
+
+# }

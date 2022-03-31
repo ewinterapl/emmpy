5243833dddb6b2f8d7f542ccded8208ec7330f66
Eric Winter <eric.winter@jhuapl.edu>
2022-03-31 11:01:21 -0400

    Moved differentiablevectorfield to math/vectorfields.

--- emmpy/math/vectorfields/__init__.py
+++ emmpy/math/vectorfields/__init__.py
@@ -8,6 +8,7 @@ cartesianvectorfield.py
 cartesianvectorfieldvalue.py
 cylindricalvectorfield.py
 cylindricalvectorfieldvalue.py
+differentiablevectorfield.py
 quadraticapproximationdifferentiablevectorfield.py
 sphericalvectorfield.py
 sphericalvectorfieldvalue.py

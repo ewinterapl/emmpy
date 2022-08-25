11972f700394ff0b72d3b4020880fbc00ec151ab
Eric Winter <eric.winter@jhuapl.edu>
2022-08-25 17:58:17 -0400

    Renaming.

--- emmpy/magmodel/math/deformation/tests/__init__.py
+++ emmpy/magmodel/math/deformation/tests/__init__.py
@@ -2,11 +2,11 @@
 
 Modules
 -------
-test_BasisVectorFieldDeformation.py
-test_CylindricalBasisVectorFieldDeformation.py
-test_CylindricalFieldDeformation.py
-test_SphericalFieldDeformation.py
-test_VectorFieldDeformation.py
+test_basisvectorfielddeformation.py
+test_cylindricalbasisvectorfielddeformation.py
+test_cylindricalfielddeformation.py
+test_sphericalfielddeformation.py
+test_vectorfielddeformation.py
 
 Packages
 --------

d04fd13234e0363e9e28853dc202c24d5ba3a4e3
Eric Winter <eric.winter@jhuapl.edu>
2022-09-05 14:34:30 -0400

    Bumped version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="emmpy",
-    version="0.0.1",
+    version="0.0.2",
     author="Eric Winter",
     author_email="eric.winter@jhuapl.edu",
     description="Empirical Magnetic Modeling for Python",
